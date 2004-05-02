6b754d1e2f2e68ce47eff7c45965b509a0d2c4ef
Clément Stenac <zorglub@videolan.org>
2004-05-02 14:32:45 +0000

    Cosmetic fix + extend

--- www.videolan.org/include/package.php
+++ www.videolan.org/include/package.php
@@ -2,7 +2,7 @@
 
 function pkgitem($version,$name,$top)
 { ?>
-  <li><a href="http://download.videolan.org/pub/videolan/<? echo $top."/".$version."/".$name; ?>"><code><? echo $name; ?></code></a>
+  <li><a href="http://download.videolan.org/pub/videolan/<? echo $top."/".$version."/".$name; ?>"><code><? echo $name; ?></code></a> Download from mirrors: 
 <? 
     $file = $_SERVER["DOCUMENT_ROOT"]."/include/mirrors";
     $file_id = fopen( $file , "r" );
@@ -17,7 +17,7 @@ function pkgitem($version,$name,$top)
 	$esp = strpos( $mirror, " " );
 	$url = substr( $mirror, 0, $esp );
 	$mirror_name = substr( $mirror, $esp, strlen( $mirror ) - $esp );
-	echo " [<a href=\"http://www.videolan.org/mirror.html?mirror=$url&file=$top/$version/$name\">Download from $mirror_name</a>]\n "; 
+	echo " [<a href=\"http://www.videolan.org/mirror.html?mirror=$url&file=$top/$version/$name\">$mirror_name</a>]\n "; 
     }
 ?>
 </li><?

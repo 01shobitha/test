7f6dcc05ebd6105179fbef788026c4228eefda58
Antoine Cellerier <dionoea@videolan.org>
2005-01-19 15:15:12 +0000

    * first press release of 2005

---
+++ www.videolan.org/pr2005-1.php
@@ -0,0 +1,78 @@
+<?php
+   $title = "VideoLAN - Press Release";
+   $lang = "en";
+   $date = "19 January 2005";
+   $menu = array( "home" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php3");
+?>
+
+<h1>
+         The VideoLAN project investigates alleged violation
+                  of their intellectual property
+</h1>
+
+<p>
+VLC media player (VLC) is an open-source cross-platform media player and
+streaming solution.
+</p>
+
+<p>
+Following announcement by Cinema On the Web (COW) and DivXNetworks
+of the new COW video on demand service, the VideoLAN project team
+members were brought knowledge of a possible violation of VLC media
+player's licence. The VideoLAN project which owns copyrights to the VLC,
+distributes the VLC as free software exclusively under the terms of the
+GNU General Public License (GPL).
+</p>
+
+<p>
+The Microsoft installer (MSI) file which can be downloaded off the
+<a href="http://www.cinemaonweb.com/ww1/index.php?ab=cow-sw">http://www.cinemaonweb.com/ww1/index.php?ab=cow-sw</a> web page appears
+to contain several binary VLC and Xvid files covered by the GPL. The
+End-User License Agreement (EULA) which the user accepts when installing
+states that:
+</p>
+
+<blockquote>
+« [...] Cinema on Web. (the "Licensor") grants to you this personal,
+limited, non-exclusive, non-transferable, non-assignable license solely
+to use in a single copy of the Licensed Works on a single computer for
+use by a single concurrent user only, and solely provided that you
+adhere to all of the terms and conditions of this Agreement. [...] »
+</blockquote>
+
+<p>
+The VideoLAN project is a strong supporter of open source and free
+software. It is our belief that the licensing terms above are
+incompatible with several provisions of the GPL. If that is the case, we
+are afraid COW might be infringing our protected intellectual property
+and denying our users' rights. 
+</p>
+
+<p>
+The VideoLAN project will do every thing it can to ensure that its
+rights and our users' rights are not denied.
+</p>
+
+<p>
+Xvid.org has been notified of a possible violation of their own Xvid
+software's GPL license.
+</p>
+
+
+<p>
+Antoine Cellerier,<br/>
+Public Relations, the VideoLAN project, VIA Centrale Réseaux<br/>
+videolan@videolan.org
+</p>
+
+<p>
+Related documents :
+</p>
+<ul>
+<li>
+GPL : <a href="http://www.gnu.org/copyleft/gpl.html">http://www.gnu.org/copyleft/gpl.html</a>
+</li>
+</ul>
+
+<?php footer('$Id: news.php 2907 2004-11-23 14:41:24Z courmisch $'); ?>

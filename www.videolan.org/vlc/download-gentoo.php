7555272efc0eac7a3daa2300d9835e4211516007
Antoine Cellerier <dionoea@videolan.org>
2005-02-10 16:52:46 +0000

    * add the stream and streamout USE variables

--- www.videolan.org/vlc/download-gentoo.php
+++ www.videolan.org/vlc/download-gentoo.php
@@ -15,7 +15,7 @@
 href="http://www.gentoo.org/">Gentoo Linux</a> distribution.
 So you can simple type <i>emerge vlc</i> and you will have VLC installed. I advice to always use the latest (and 'unstable') version of VLC by emerging like: <i>ACCEPT_KEYWORDS="~x86" emerge vlc</i>.</p>
 <p>
-When you don't set the correct USE variables, you will get a very bare bones version of VLC. I advice to emerge with the following USE variables: dvd nls xv bidi truetype wxwindows imlib matroska faad png"
+When you don't set the correct USE variables, you will get a very bare bones version of VLC. I advice to emerge with the following USE variables: dvd nls xv bidi truetype wxwindows imlib matroska faad png. To add streaming support, add the stream and streamout USE variables.
 </p>
 
 <h2>Older versions</h2>

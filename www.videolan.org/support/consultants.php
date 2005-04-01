f96a4f08cd4e4fff8fccc6c0a47b88cb3b5c00b5
Jean-Paul Saman <jpsaman@videolan.org>
2005-04-01 09:25:32 +0000

    Added a consultant

--- www.videolan.org/support/consultants.php
+++ www.videolan.org/support/consultants.php
@@ -15,20 +15,27 @@ for professional services to order.</p>
 <table>
 
 <tr>
-
 <td>
-<a href="http://www.anevia.com/">
-<img src="/images/anevia.png" alt="Anevia" width="200" height="57" />
-</a>
+  <a href="http://www.anevia.com/">
+  <img src="/images/anevia.png" alt="Anevia" width="200" height="57" />
+  </a>
 </td>
-
 <td>French company founded by four members of the VideoLAN team (Brieuc
 Jeunhomme, Tristan Leteurtre, Damien Lucas and Alexis de Lattre - to
 know more about them, see the <a href="/team/">VideoLAN team page</a>).
 Anevia sells professional video servers and provides development and
 integration of high quality video streaming solutions.</td>
-
 </tr>
+
+<tr>
+<td>
+  <a href="http://www.m2x.nl/">
+  <img src="http://www.m2x.nl/mambo/images/m2x.png" alt="M2X" width="250" height="87" />
+  </a>
+</td>
+<td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page </a> for more information).</td>
+</tr>
+
 </table>
 
 <?php footer('$Id$'); ?>

43a61604d9d180fe693faebd474d7d190c699bf9
Jean-Paul Saman <jpsaman@videolan.org>
2005-04-01 09:30:40 +0000

    How is this supposed to work?

--- www.videolan.org/support/consultants.php
+++ www.videolan.org/support/consultants.php
@@ -30,7 +30,7 @@ integration of high quality video streaming solutions.</td>
 <tr>
 <td>
   <a href="http://www.m2x.nl/">
-  <img src="http://www.m2x.nl/mambo/images/m2x.png" alt="M2X"/>
+  <img src="http://www.m2x.nl/mambo/images/m2x.png" alt="M2X" width="200" height="57" />
   </a>
 </td>
 <td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page </a> for more information).</td>

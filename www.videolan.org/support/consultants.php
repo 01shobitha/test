0e44721bb7a13c9f2d6cf547029a8809fda0cb34
Jean-Paul Saman <jpsaman@videolan.org>
2005-04-03 13:20:49 +0000

    Finish the sentence.

--- www.videolan.org/support/consultants.php
+++ www.videolan.org/support/consultants.php
@@ -33,7 +33,7 @@ integration of high quality video streaming solutions.</td>
   <img src="/images/m2x.png" alt="M2X" width="200" height="57" />
   </a>
 </td>
-<td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page </a> for more information).</td>
+<td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page</a> for more information on his contributions).</td>
 </tr>
 
 </table>

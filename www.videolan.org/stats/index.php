613b7685e5f7d878eec0c4702a261e4d50a0fe5b
Clément Stenac <zorglub@videolan.org>
2005-06-25 18:31:29 +0000

    Fix stats page

---
+++ www.videolan.org/stats/index.php
@@ -0,0 +1,37 @@
+<?
+   $title = "VideoLAN - Statistics";
+   $lang = "en";
+   $date = "06 April 2002";
+   $menu = array( "home", "overview" );
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php3");
+
+   include($_SERVER["DOCUMENT_ROOT"]."/include/news.php3");
+?>
+
+<h1>Statistics</h1>
+
+<p>
+
+<a href="200309">septembre 2003</a><br />
+<a href="200310">octobre 2003</a><br />
+<a href="200311">novembre 2003</a><br />
+<a href="200312">décembre 2003</a><br />
+<a href="200401">janvier 2004</a><br />
+<a href="200402">février 2004</a><br />
+<a href="200403">mars 2004</a><br />
+<a href="200404">avril 2004</a><br />
+<a href="200405">mai 2004</a><br />
+<a href="200406">juin 2004</a><br />
+<a href="200407">juillet 2004</a><br />
+<a href="200408">août 2004</a><br />
+<a href="200409">septembre 2004</a><br />
+<a href="200410">octobre 2004</a><br />
+<a href="200411">novembre 2004</a><br />
+<a href="200412">décembre 2004</a><br />
+<a href="200501">janvier 2005</a><br />
+<a href="200502">février 2005</a><br />
+<a href="200503">mars 2005</a><br />
+<a href="200504">avril 2005</a><br />
+<a href="200505">mai 2005</a><br />
+<a href="200506">juin 2005</a><br />
+</p>

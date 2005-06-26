56b94b2f8fab4b2614ab3bba20eb2c8ab42048de
Clément Stenac <zorglub@videolan.org>
2005-06-26 08:05:55 +0000

    Fix stats

--- www.videolan.org/stats/index.php
+++ www.videolan.org/stats/index.php
@@ -8,30 +8,9 @@
    include($_SERVER["DOCUMENT_ROOT"]."/include/news.php3");
 ?>
 
-<h1>Statistics</h1>
+<h1>Web Statistics</h1>
 
-<p>
+<?php include( $_SERVER["DOCUMENT_ROOT"]."/stats/web.inc" ); ?>
 
-<a href="200309">septembre 2003</a><br />
-<a href="200310">octobre 2003</a><br />
-<a href="200311">novembre 2003</a><br />
-<a href="200312">décembre 2003</a><br />
-<a href="200401">janvier 2004</a><br />
-<a href="200402">février 2004</a><br />
-<a href="200403">mars 2004</a><br />
-<a href="200404">avril 2004</a><br />
-<a href="200405">mai 2004</a><br />
-<a href="200406">juin 2004</a><br />
-<a href="200407">juillet 2004</a><br />
-<a href="200408">août 2004</a><br />
-<a href="200409">septembre 2004</a><br />
-<a href="200410">octobre 2004</a><br />
-<a href="200411">novembre 2004</a><br />
-<a href="200412">décembre 2004</a><br />
-<a href="200501">janvier 2005</a><br />
-<a href="200502">février 2005</a><br />
-<a href="200503">mars 2005</a><br />
-<a href="200504">avril 2005</a><br />
-<a href="200505">mai 2005</a><br />
-<a href="200506">juin 2005</a><br />
-</p>
+<?php footer('$Id: index.php 3321 2005-06-25 13:53:44Z zorglub $'); ?>
+

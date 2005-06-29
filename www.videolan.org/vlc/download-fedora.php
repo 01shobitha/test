156050b3e8092d5cdc1541efa0e536bc9f9612f4
Antoine Cellerier <dionoea@videolan.org>
2005-06-29 17:07:45 +0000

    add links to freshrpms

--- www.videolan.org/vlc/download-fedora.php
+++ www.videolan.org/vlc/download-fedora.php
@@ -11,6 +11,10 @@
 
 <h1>VLC media player for Fedora Core 3</h1>
 
+<h2>Unofficial Fedora Core packages</h1>
+
+<p>Matthias Saou from freshrpms.net also maintains some Fedora Core Packages : <a href="http://freshrmps.net">get them here</a>.</p>
+
 <h2>Install VLC from the RPM packages</h2>
 
 <p>Download the RPM packages tarballs listed in the section below and
@@ -52,19 +56,5 @@ uncompress them in the same directory :</p>
 <p> Older versions can be found by browsing our FTP site :</p>
 <ul> <?php mirrordir("vlc"); ?> </ul>
 
-<h1>Unofficial Fedora Core packages</h1>
-
-<p>Matthias Saou from freshrpms.net also maintains some Fedora Core Packages.</p>
-
-<ul>
-<?php pkgitem("vlc/0.8.1/rpm/fedora/fc3","videolan-client-0.8.1-1.1.fc3.rf.i386.rpm","(VLC i386 Fedora Core 3 Binary package)");
-pkgitem("vlc/0.8.1/rpm/fedora/fc3","videolan-client-0.8.1-1.1.fc3.rf.x86_64.rpm","(VLC x86_64 Fedora Core 3 Binary package)");
-pkgitem("vlc/0.8.1/rpm/fedora/fc2","videolan-client-0.8.1-1.1.fc2.rf.i386.rpm","(VLC i386 Fedora Core 2 Binary package)");
-pkgitem("vlc/0.8.1/rpm/fedora/fc2","videolan-client-0.8.1-1.1.fc2.rf.x86_64.rpm","(VLC x86_64 Fedora Core 2 Binary package)");?>
-</ul>
-
-<ul>
-<?php pkgitem("vlc/0.8.1/rpm/fedora/fc1","videolan-client-0.8.1-1.1.fc1.rf.i386.rpm","(VLC i386 Fedora Core 1 Binary package)");?>
-</ul>
 
 <?php footer('$Id$'); ?>

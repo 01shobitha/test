9a5c372e2b92dbc7286c85e34e504e684493dccc
Antoine Cellerier <dionoea@videolan.org>
2005-07-22 20:53:43 +0000

    G5 +100euros. Many thanks to Carlo (murray) (commit that didn't work last time ... weird)

--- www.videolan.org/contribute.php
+++ www.videolan.org/contribute.php
@@ -47,14 +47,14 @@ tell you if we are interested and give you our snail-mail address. </p>
 
 <p>We desperately need a G5 to continue developement on Mac hardware.
 <a href="http://forum.videolan.org/viewtopic.php?p=31286">More info
-here</a>. Current status of donnations is: 138,49&euro; donated ... much
-more needed (lets say 661,51&euro;). Remember to mention that you are
+here</a>. Current status of donnations is: 238,49&euro; donated ... much
+more needed (lets say 561,51&euro;). Remember to mention that you are
 donating money for the G5 in the donation comments.<br />
 
 Status bar : <img src="/images/green-pix.png" style="border: 0px;
-padding: 0px; margin:0px" height="10px" width="27px" alt="ok" /><img
+padding: 0px; margin:0px" height="10px" width="47px" alt="ok" /><img
 src="/images/red-pix.png" style="border: 0px; padding: 0px; margin:0px"
-height="10px" width="133px" alt="not ok" /> (green is what is already
+height="10px" width="113px" alt="not ok" /> (green is what is already
 available, red is what we still need in order to buy the G5)</p>
 
 

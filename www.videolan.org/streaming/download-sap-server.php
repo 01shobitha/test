66a1eb855dd79fb33ce813cb820dab4a4b0257f8
Rémi Denis-Courmont <rem@videolan.org>
2005-10-14 12:58:20 +0000

    miniSAPserver version 0.3.2

--- www.videolan.org/streaming/download-sap-server.php
+++ www.videolan.org/streaming/download-sap-server.php
@@ -1,7 +1,7 @@
 <?php
    $title = "miniSAPserver - download";
    $lang = "en";
-   $date = "12 december 2003";
+   $date = "14 october 2003";
    $menu = array( "streaming", "" );
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php3");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
@@ -10,10 +10,10 @@
 
 <h1> MiniSAPserver for Linux, BSD and Mac OS X</h1>
 
-<p>Latest miniSAPserver tarball (0.3.1) (suggests VLC 0.8.2 or higher):</p>
+<p>Latest miniSAPserver tarball (0.3.2) (suggests VLC 0.8.2 or higher):</p>
 
 <ul>
-  <?php pkgitem("0.3.1","minisapserver-0.3.1.tar.bz2","miniSAPserver"); ?>
+  <?php pkgitem("0.3.2","minisapserver-0.3.2.tar.bz2","miniSAPserver"); ?>
 </ul>
 
 <?php

6b8c324cbb699d129abc4702949b6b52d4920a86
Clément Stenac <zorglub@videolan.org>
2005-11-30 17:42:12 +0000

    First draft for an EUCD/DADVSI warning page

---
+++ www.videolan.org/eucd.php
@@ -0,0 +1,75 @@
+<?php
+   $title = "VideoLAN - And the issue of EUCD / DADVSI ";
+   $lang = "en";
+   $date = "16 November 2005";
+   $menu = array( "home" );
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php3");
+?>
+
+<h1> The battle is not over...</h1>
+
+
+<p>
+<strong>
+	During the night of 22nd to 23rd December 2005, while everybody is preparing for Christmas, the French Parliament will rule about the "DADVSI" law. This vote will be made with minimal discussion, as an "emergency" has been declared on this law.<br />
+</strong>
+</p>
+
+<p>
+
+	This law is the french transcription of the european EUCD (European Union Copyright Directive) text, which itself comes from the american DMCA (Digital Millenium Copyright Act).<br />
+
+The main goal of this law is to restrict the rights of digital content purchasers. It most notably forbids them from working around technical content protection measures. <br />
+
+Doing so, writing or publishing software allowing to do so, or even merely talking about ways to do so becomes an offence that can be punished with three years in jail.</p>
+
+<p>
+<strong>
+VideoLAN is directly impacted, most notably for its DVD reading capability (all Linux DVD reading software have the same problem). Should this law be passed, this would seriously hinder VLC's development.
+</strong>
+</p>
+
+<p>The french website <a href="http://eucd.info">eucd.info</a> collects
+a large number of related articles and documentation</p>
+
+
+<h1> Français, ceci vous concerne directement.</h1>
+
+<p>
+<strong>
+       Pendant la nuit du 22 au 23 décembre 2005, alors que tout le
+monde prépare Noël, l'Assemblée Nationale devra se prononcer sur le
+texte DADVSI (Droits d'Auteurs et Droits Voisins dans la Société de
+l'Information). Ce vote se fera sans discussion, une mesure d'urgence
+ayant été décrétée sur ce texte.
+</strong>
+</p>
+
+<p>
+Cette loi est la transcription en droit français de la directive EUCD   
+(European Union Copyright Directive), qui est elle même inspirée du     
+DMCA (Digital Millenium Copyright Act) américain.<br />                 
+ L'effet principal de ce texte est de restreindre fortement les droits
+des particuliers achetant des produits culturels. DADVSI s'oppose ainsi
+directement à l'exception de copie privée en autorisant les producteurs
+de contenu à mettre en place des mesures techniques l'interdisant, et
+surtout en interdisant aux acquéreurs de contourner ces mesures.<br />
+
+Contourner une mesure de protection, écrire ou diffuser un logiciel
+permettant de la contourner, ou faire connaître des moyens de le faire
+sont assimilés à des délits de contrefaçon, passibles de trois ans de
+prison et 300 000 euros d'amende.</p>
+
+<p> <strong> VideoLAN est directement concerné, notamment pour sa       
+capacité à lire les DVD (tous les logiciels de lecture de DVD sous      
+Linux se retrouverait illégaux). L'acceptation de cette loi aurait un   
+effet négatif très fort sur le développement de VLC. </strong> </p>     
+
+<p> <strong>Vous</strong> pouvez aider les nombreuses associations      
+(d'artistes, de développeurs, d'enseignants, de consommateurs,          
+...) luttant contre cette proposition "liberticide", de <a               
+href="http://eucd.info/index.php?2005/11/16/182-appel-defendons-nos-dro 
+its-et-libertes">nombreuses façons différentes</a>. </p>                
+
+<p>Le site <a href="http://eucd.info">eucd.info</a> rassemble des
+informations détaillées sur ce problème et des dossiers complets.</p>

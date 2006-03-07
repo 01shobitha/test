91c96dee27f6a9c3b94912f228bfc1177587a5a8
Rémi Denis-Courmont <rem@videolan.org>
2006-03-07 22:18:26 +0000

    Unfinished press release about ISP change

---
+++ www.videolan.org/pr2006-1.php
@@ -0,0 +1,54 @@
+<?php
+   $title = "VideoLAN - Press Release";
+   $lang = "en";
+   $date = "8 March 2006";
+   $menu = array( "home" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php3");
+?>
+
+<h1>The VideoLAN website gets a new home</h1>
+
+<p>
+VideoLAN is the software development project behind the open-source
+VLC&nbsp;media&nbsp;player (VLC), a cross-platform media player and
+streaming solution, some widely used audio/video encoding/decoding
+and decryption open-source libraries, and IP network streaming tools.
+</p>
+<p>
+Ever since its inception in 1996 by visionary students, the project has
+been hosted on the campus of the &Eacute;cole Centrale Paris,
+a famous French engineering university (&quot;Grande &Eacute;cole&quot;).
+The project eventually went open-source in early 2001.
+From then on, contributors from all over the world started contributing,
+and VLC was ported to Windows within about 3 months.
+</p>
+<p>
+As VLC popularity rose, so did the bandwidth usage of our main web server.
+We did upgrade our web server since then (thanks to HP and IBM), and the
+campus network connection from 10 to 100&nbsp;Mbps.
+</p>
+<p>
+Unfortunately, while the hardware is now fairly sufficient, the network
+bandwidth turned out to be insufficient, and keeps being saturated within
+the days following a new release of our masterpiece.
+</p>
+<p>We hoped that with a 3&nbsp;gigabits/s (and possibly times more) capable
+backbone, and recently upgraded gigabit connection between the campus and
+the university would ultimately put an end to these issues...
+<br />
+We were obviously too gullible.
+
+<p>
+R&eacute;mi Denis-Courmont,<br/>
+Public Relations, the VideoLAN project<br/>
+<a href="mailto:videolan@videolan.org">videolan@videolan.org</a>
+</p>
+
+
+<p>References&nbsp;:</p>
+<dl>
+<dt>&Eacute;cole Centrale Paris&nbsp;:</dt>
+<dd><a href="http://www.ecp.fr/">http://www.ecp.fr/</a></dd>
+</dl>
+
+<?php footer('$Id$'); ?>

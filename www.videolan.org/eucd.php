230a61ff577a2a0d480d3e9585a4375c5f842290
Clément Stenac <zorglub@videolan.org>
2006-03-16 09:51:20 +0000

    DADVSI

--- www.videolan.org/eucd.php
+++ www.videolan.org/eucd.php
@@ -1,5 +1,5 @@
 <?php
-   $title = "VideoLAN - And the issue of EUCD / DADVSI ";
+   $title = "VideoLAN - DADVSI";
    $lang = "en";
    $date = "16 November 2005";
    $menu = array( "home" );
@@ -12,49 +12,66 @@
 
 <p>The DADVSI (roughly "Author Rights and other Rights in the
 Information Society") is the French transcription of the european EUCD
-(European Union Copyright Directive) text, which itself comes from the
-american DMCA (Digital Millenium Copyright Act).<br />
+(European Union Copyright Directive) text, which itself is very similar
+to the american DMCA (Digital Millenium Copyright Act).<br />
 
 The main goal of this law is to restrict the rights of digital content
 purchasers. It most notably forbids them from working around technical
-content protection measures. <br />
+content protection measures (oh, by the way, it does not precisely
+describe was a technical content protection measure is, neither what is
+considered as working around). <br />
 
 Doing so, writing or publishing software allowing to do so, or even
-merely talking about ways to do so becomes an offence that can be
-punished with three years in jail.</p>
+merely talking about ways to do so is illegal with this law.</p>
 
-<p> <strong> VideoLAN is directly impacted, most notably for its        
-DVD reading capability (all Linux DVD reading software has the same     
-problem). Should this law be passed as it is written, this would        
-seriously hinder VLC's development. </strong> </p>                      
+<p>On March 14th and 15th, this law was passed at the French "Assemblée
+Nationale" (House of Representatives).<br />
+
+Despite the promises made by the French Ministry of Culture, this law
+does not protect Free Software, or working around DRM for "legal"
+purposes.</p>
+
+<p>Moreover, an amendment was adopted, which punishes with 3 years in
+jail any person that would write, distribute or promote a software that
+is "obviously" aimed at illegal file sharing (except if it is "only"
+aimed at legal sharing). This amendment is nicknamed the
+<em>Vivendi-Universal Amendment</em>. Guess why...</p>
+
+<p><strong> VideoLAN is directly impacted, most notably for its        
+DVD reading capability</strong></p>
+
+<p>This means that VLC will be considered as illegal in France as soon
+as this law is officially promulgated. The penalty for distributing or
+promoting software that works around DRM is <strong>6 months in jail and
+a 30 000 euros fine</strong>.</p>
+
+<p>Here is a non-exhaustive list of free software that are impacted:</p>
+
+<ul>
+<li>VLC (reads DVD, works around iTunes DRM)</li>
+<li>Mplayer (reads DVD)</li>
+<li>Totem (reads DVD)</li>
+<li>More generally, any free software DVD player</li>
+<li>Bittorent (P2P, could be used for illegal sharing)</li>
+<li>eMule and all its variants (P2P, could be used for illegal sharing)</li>
+<li>Sharpmusique (works around iTunes DRM)</li>
+<li>FairTunes (works around iTunes DRM)</li>
+<li>Any software that works around any DRM, either on music files or
+others</li>
+<li>...</li>
+</ul>
 
 <p>The french website <a                                                
 href="http://eucd.info/index.php?English-readers">eucd.info</a>         
 collects a large number of related articles and documentation, and has a 
 dedicated english-speaking section.</p> 
 
-<p>Discussion about this law has started in the french National
-Assembly, and will resume on January, 17th.<br />
+<p>We would like to thank all the people who are working on spreading
+information about DADVSI, most notably EUCD.info.<br />
 
-A nice fact is that many MPs, both from the Government's political party
-and opposition were very aware of the possible impact of this law on
-Free Software, and there was a real debate on the first part of the
-law.<br />
-
-<p>
-We would like to thank all the people involved in miscellaneous
-information actions, and the MPs that raised concerns concerns about
-this text, thus allowing a general public awareness about these issues.
-</p>
-
-<p>
-The most important parts (mostly about DRM and Peer-To-Peer) will take
-place in February, so the battle is not over. <strong>It is thus very
-important to keep spreading the word about the dangers of this law.</strong>
-</p>
-
-
-<p><strong>Update</strong> (January 18th) : The french Ministry of Culture has started presenting a new text for this Feburary session. Good news ! With the old version, users and developers of VideoLAN could be threatened with 3 years in jail and a 300 000 euros fine. Now, users could only get a small fine (something like 40 euros), and developers would <em>only</em> get one year in jail and a 100 000 euros fine.</p>
+We also thank the MPs who have tried to raise attention on the dangers
+of this text and who have tried (but failed) to limit its nefarious
+effects.</p>
 
 <h1> Français, ceci vous concerne directement.</h1>
 
@@ -65,21 +82,60 @@ l'Information) est la transcription en droit français de la directive
 EUCD (European Union Copyright Directive), qui est elle même inspirée   
 du DMCA (Digital Millenium Copyright Act) américain.<br />              
 
- L'effet principal de ce texte est de restreindre fortement les droits
+L'effet principal de ce texte est de restreindre fortement les droits
 des particuliers achetant des produits culturels. DADVSI s'oppose ainsi
 directement à l'exception de copie privée en autorisant les producteurs
-de contenu à mettre en place des mesures techniques l'interdisant, et
-surtout en interdisant aux acquéreurs de contourner ces mesures.<br />
+de contenu à mettre en place des mesures techniques (DRM) l'interdisant, et
+surtout en interdisant aux acquéreurs de contourner ces mesures. (La loi
+ne définit toutefois pas précisément ni la notion de mesure technique de
+protection, ni la notion de contournement).<br />
 
 Contourner une mesure de protection, écrire ou diffuser un logiciel
 permettant de la contourner, ou faire connaître des moyens de le faire
-sont assimilés à des délits de contrefaçon, passibles de trois ans de
-prison et 300 000 euros d'amende.</p>
+sont considérés comme illégaux dans le cadre de cette loi.</p>
 
-<p> <strong> VideoLAN est directement concerné, notamment pour sa       
-capacité à lire les DVD (tous les logiciels de lecture de DVD sous      
-Linux se retrouveraient illégaux). L'adoption de cette loi aurait un   
-effet négatif très fort sur le développement de VLC. </strong> </p>     
+<p>Les éléments principaux de cette loi ont été adoptés les 14 et 15
+mars 2006 à l'Assemblée Nationale par une grande partie de la majorité
+UMP.<br />
+
+Malgré les promesses et les dénégations du Ministre de la Culture, M.
+Renaud Donnedieu de Vabres, cette loi ne protège en aucun cas le
+Logiciel Libre ni le contournement des DRM à des fins d'usage licite
+d'une oeuvre légalement acquise.</p>
+
+<p>De plus, un amendement, surnommé <em>Amendement
+Vivendi-Universal</em> a été adopté. Celui-ci punit de 3 ans de prison
+et 300 000 euros d'amende toute personne développant, distribuant,
+faisant la promotion, ou parlant d'un logiciel "manifestement" destiné
+au partage illicite de fichier (à l'exception des logiciels uniquement
+destinés au partage légal).</p>
+
+<p><strong>Cette loi concerne directement VLC, notamment pour ses
+capacités à lire les DVD</strong></p>
+
+<p>Dès la promulgation de cette loi en France, VLC sera considéré comme
+illégal, et les développeurs et distributeurs passibles de <strong>6
+mois de prison et 30 000 euros d'amende</strong>.</p>
+
+<p>Voici une liste non-exhaustive de logiciels libres touchés par ces
+mesures:</p>
+
+<ul>
+<li>VLC (lecture de DVD, contournement des DRM iTunes)</li>
+<li>Mplayer (lecture de DVD)</li>
+<li>Totem (lecture de DVD)</li>
+<li>Plus généralement, tous les lecteurs de DVD libres</li>
+<li>Bittorent (logiciel de P2P, pouvant être utilisé pour du partage
+illégal)</li>
+<li>eMule et toutes ses variantes (logiciels de P2P, pouvant être
+utilisés pour du partage illégal)</li>
+<li>Sharpmusique (contournement des DRM iTunes)</li>
+<li>FairTunes (contournement des DRM iTunes)</li>
+<li>Tout logiciel qui contournerait les DRM, que ce soit sur des
+fichiers musicaux ou autres</li>
+<li>...</li>
+</ul>
+
 
 <p> <strong>Vous</strong> pouvez aider les nombreuses associations      
 (d'artistes, de développeurs, d'enseignants, de consommateurs,          
@@ -90,30 +146,13 @@ its-et-libertes">nombreuses façons différentes</a>. </p>
 <p>L'initiative <a href="http://eucd.info">eucd.info</a> rassemble des
 informations détaillées sur ce problème et des dossiers complets.</p>
 
-<p>Une première partie du débat a eu lieu du 21 au 23 décembre 2005 à
-l'Assemblée Nationale. Au cours de ce débat, de nombreux députés de
-tous partis ont mis en avant les dangers de ce texte, notamment pour le
-Logiciel Libre.<br />
 
 <p>Nous tenons à remercier toutes les personnes participant à
-l'information à propos de DADVSI, notamment l'initiative eucd.info.<br
-/>
-
+l'information à propos de DADVSI, notamment l'initiative eucd.info.<br />
 Nous remercions également les nombreux députés ayant attiré l'attention
-sur les dangers de ce texte.</p>
+sur les dangers de ce texte et ayant tenté, sans succès, d'en limiter
+les effets néfastes. </p>
 
-
-
-
-La suite du débat, notamment les parties concernant directement         
-les logiciels libres (articles contre le contournement des mesures      
-techniques de protection, pour l'interopérabilité, et sur la            
-responsabilité des auteurs de logiciels de transfert), aura lieu à      
-partir du 17 Janvier 2006 à l'Assemblée Nationale. <strong>Il importe   
-donc de rester mobilisés et de continuer les actions d'information à    
-propos des dangers de ce texte.</strong></p>                            
-
-<p><strong>Mise à jour</strong> (18 janvier) : Le Ministre de la Culture a commencé à présenter son projet de loi amélioré. Il y a des améliorations : alors que dans la première version, tant les utilisateurs que les développeurs pouvaient être reconnus coupables de contrefaçon et condamnés à 300 000 euros d'amende et 3 ans de prison, dans la nouvelle version, les utilisateurs ne s'exposeraient plus qu'à une amende de première classe (38 euros) et les développeurs ne pourraient plus risquer qu'un an de prison et 100 000 euros d'amende.</p>
 
 <h1>Was ist DADVSI?</h1>
 

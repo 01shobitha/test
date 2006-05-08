eeda38819981d6cd1b4bb7423833629505efaf46
Antoine Cellerier <dionoea@videolan.org>
2006-05-08 16:58:04 +0000

    Host local copy of slackware tgz. Thanks to pallansson

--- www.videolan.org/vlc/download-slackware.php
+++ www.videolan.org/vlc/download-slackware.php
@@ -4,13 +4,19 @@
    $date = "8 May 2006";
    $menu = array( "vlc", "download" );
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php3");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
 <?php function pkgitem($version,$name,$comment) { ?><li><a href="http://download.videolan.org/pub/videolan/<?php echo $version."/".$name; ?>"><code><?php echo $name; ?></code></a> <?php echo $comment; ?> </li><?php } ?>
 
 <h1>VLC media player for Linux Slackware</h1>
 
-<p>Unofficial tgz are available on <a href="http://www.slacky.it/index.php?option=com_remository&Itemid=29&func=fileinfo&parent=folder&filecatid=1120">Italian Slackware Community</a>.</p>
-<p>Pallanson also build a tgz for VLC 0.8.5. Get it <a href="http://pallansson.homeip.net/vlc-0.8.5-i686-1.tgz">here</a>.</p>
+<h2>Unofficial packages</h2>
+<p>Pallansson build a tgz for VLC 0.8.5:</p>
+<ul>
+  <?php pkgitem("0.8.5/slackware","vlc-0.8.5-i686-1.tgz","vlc"); ?>
+</ul>
+
+<p>Another VLC tgz are also available on <a href="http://www.slacky.it/index.php?option=com_remository&Itemid=29&func=fileinfo&parent=folder&filecatid=1120">Italian Slackware Community</a>.</p>
 
 <?php footer('$Id$'); ?>

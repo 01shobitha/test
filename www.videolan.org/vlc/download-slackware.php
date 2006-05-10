33635065797efbc94f8452d25efd50f0faa5a8b6
Antoine Cellerier <dionoea@videolan.org>
2006-05-10 21:04:01 +0000

    Updated slackware package (now with V4L support).

--- www.videolan.org/vlc/download-slackware.php
+++ www.videolan.org/vlc/download-slackware.php
@@ -10,9 +10,9 @@
 <h1>VLC media player for Linux Slackware</h1>
 
 <h2>Unofficial packages</h2>
-<p><a href="http://pallansson.homeip.net/vlc/index.html">Per Allansson</a> build a tgz for VLC 0.8.5:</p>
+<p><a href="http://pallansson.homeip.net/vlc/index.html">Per Allansson (click here for packages changelog)</a> build a tgz for VLC 0.8.5:</p>
 <ul>
-  <?php pkgitem("0.8.5/slackware","vlc-0.8.5-i686-1.tgz","vlc"); ?>
+  <?php pkgitem("0.8.5/slackware","vlc-0.8.5-i686-2.tgz","vlc"); ?>
 </ul>
 
 <p>Another VLC tgz are also available on <a href="http://www.slacky.it/index.php?option=com_remository&Itemid=29&func=fileinfo&parent=folder&filecatid=1120">Italian Slackware Community</a>.</p>

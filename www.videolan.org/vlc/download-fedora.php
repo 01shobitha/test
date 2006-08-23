bf5551c500633ac4759c547f9aa603fe73f68754
Jean-Paul Saman <jpsaman@videolan.org>
2006-08-23 11:29:43 +0000

    Update Fedora download page for vlc-0.8.5 rpm packages and yum repositories.

--- www.videolan.org/vlc/download-fedora.php
+++ www.videolan.org/vlc/download-fedora.php
@@ -9,51 +9,82 @@
 
 <div id="fullwidth">
 
-<h1>VLC media player for <a href="http://fedora.redhat.com/">Fedora Core</a> 3</h1>
+<h1>VLC media player for <a href="http://fedora.redhat.com/">Fedora Core</a> 4</h1>
+
+<h2>Yum repository</h2>
+<p>VLC 0.8.5 is available for Fedora Core 4 from a YUM repository hosted at VideoLAN or one of its mirrors. All packages have been GPG signed by Jean-Paul Saman. Download the publickey <a href=http://www.m2x.nl/mambo/packages/gpg_pubkey_jean-paul.saman.asc>here</a>.</p>
+
+<p>Add the <a href=http://www.m2x.nl/mambo/packages/gpg_pubkey_jean-paul.saman.asc>publickey</a> to the RPM database with:</p>
+
+<pre>
+   $> rpm --import gpg_pubkey_jean-paul.saman.asc
+</pre>
+
+<p>Add the following files to your /etc/yum.repos.d directory for updating and installing VLC from a YUM repository. Change the baseurl to a mirror closest to you (see below for a list of mirrors). You will need at minimum <b>vlc.repo</b> and <b>vlc-contrib.repo</b> :</p>
+
+<p>file: vlc.repo</p>
+<pre>
+[base]
+name=VLC 0.8.5 - $basearch - Base
+baseurl=http://download.videolan.org/pub/videolan/vlc/0.8.5/fedora/4/i386/vlc/0.8.5/base/
+enabled=1
+gpgcheck=1
+</pre>
+
+<p>file: vlc-contrib.repo</p>
+<pre>
+[contrib]
+name=VLC 0.8.5 - $basearch - Contrib
+baseurl=http://download.videolan.org/pub/videolan/vlc/0.8.5/fedora/4/i386/vlc/0.8.5/contrib/
+enabled=1
+gpgcheck=1
+</pre>
+
+<p>file: vlc-devel.repo</p>
+<pre>
+[devel]
+name=VLC 0.8.5 - $basearch - Devel
+baseurl=http://download.videolan.org/pub/videolan/vlc/0.8.5/fedora/4/i386/vlc/0.8.5/devel/
+enabled=1
+gpgcheck=1
+</pre>
+
+<p>file: vlc-source.repo</p>
+<pre>
+[source]
+name=VLC 0.8.5 - $basearch - Source
+baseurl=http://download.videolan.org/pub/videolan/vlc/0.8.5/fedora/4/i386/vlc/0.8.5/source/
+enabled=1
+gpgcheck=1
+</pre>
+
+<p>The yum repository is mirrored to :</p>
+
+<?php  pkgitem( "VLC Fedora Core Yum repository","vlc/0.8.5/fedora/4/i386/vlc/0.8.5/","","");?>
+
+<h2>Install VLC from RPM packages</h2>
+
+<p>Download the RPM packages tarball listed below. It includes all RPM's for vlc, 3rd party libraries not included in the distribution, devel RPM's, debug RPM's and source RPM's.</p>
+
+<?php  pkgitem( "VLC RPM packages","vlc/0.8.5/fc4","vlc-fc4.tar.gz","");?>
+
+<p>Uncompress vlc-fc4.tar.gz in the same directory :</p>
+
+<pre>
+   $ tar zxf &lt;package name&gt;
+</pre>
+
+<p>Then, as root, install the packages (use -ivh for installation and -Uvh for upgrade) :</p>
+
+<pre>
+    # rpm -ivh fedora/4/i386/vlc/0.8.5/base/*.rpm fedora/4/i386/vlc/0.8.5/contrib/*.rpm
+</pre>
 
 <h1>Unofficial Fedora Core packages</h1>
 
 <p>rpm.livna.org maintains some Fedora Core Packages for VLC: <a href="http://rpm.livna.org/">get them here</a>.</p>
 
 <p>Matthias Saou from freshrpms.net also maintains some Fedora Core Packages: <a href="http://freshrpms.net">get them here</a>.</p>
-
-<h2>Install VLC from the RPM packages</h2>
-
-<p style="color: red;font-size:larger;">Warning, these are VLC media player 0.8.1 RPM
-packages. To get the latest VLC media player version, please use one of
-the Unofficial Fedora Core packages.</p>
-
-<p>Download the RPM packages tarballs listed in the section below and
-uncompress them in the same directory :</p>
-
-<pre>
-   $ tar zxf &lt;package name&gt;
-</pre>
-
-<p>Then, as root, install the packages:</p>
-
-<pre>
-    # rpm -U vlc/* --force
-</pre>
-
-
-<h2>Fedora Core 3 packages for i386</h2>
-
-<p>Latest RPM x86 packages tarballs for Fedora Core 3 : </p>
-
-<ul><li>
-<?php pkgitem( "VLC Binary packages","vlc/0.8.1/rpm/fedora/fc3","vlc-binary.tar.gz","");?></li>
-</ul>
-
-<p>Optional components:</p>
-
-<ul><li><?php  pkgitem_nomirr( "VLC Development headers","vlc/0.8.1/rpm/fedora/fc3","vlc-devel.tar.gz","");?></li>
-<li><?php pkgitem_nomirr("VLC Source Code", "vlc/0.8.1/rpm/fedora/fc3","vlc-source.tar.gz","") ?></li>
-</ul>
-
-<p>You can also browse the <a href="http://download.videolan.org/pub/videolan/vlc/0.8.1/rpm/fedora/fc3/vlc">packages directory of our FTP site</a>.</p>
-
-<p>Thank to Jason Luka who maintains these packages</p>
 
 <h2>Older versions</h2>
 <?php browse_old("vlc") ; ?>

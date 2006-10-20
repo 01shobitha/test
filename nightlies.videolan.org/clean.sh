3cc20a6968fdd83018a90c96c0a37aeb5bc0be24
Jean-Baptiste Kempf <jb@videolan.org>
2006-10-20 17:03:38 +0000

    Poor script made to clean nighlty builds that keeps all the files from the last 2 months and backup the older ones that are made on mondays.
    Could be MUCH better. But works.

---
+++ nightlies.videolan.org/clean.sh
@@ -0,0 +1,69 @@
+#! /bin/sh
+
+
+DATEMIN=`date -d '2 months ago' +%Y%m%d`;
+
+
+echo "Two months ago, we were on : $DATEMIN";
+
+cd build/;
+
+#those have folder in it
+for i in win32 sid-i386 sid-amd64
+do 
+	echo "Entering folder $i...";
+	cd  $i;
+	if [ ! -d backup ]; then
+		echo "Making a backup dir in $i"
+		mkdir backup
+	fi
+	for j in `ls -1 -b  | xargs`
+	do 
+		day=`echo $j |cut -d- -f2`;
+
+		if [[ $day < $DATEMIN ]]; then
+			echo "Older than it should be  $i/$j";
+			DATETEST=`date -d $day +%u`
+			if [[ $DATETEST = 1 ]];
+			then
+				mv $j backup/
+				echo "Backing it up"
+			else	
+				rm -rf $j/
+				echo "GO TO /dev/null"
+			fi
+		fi
+	done
+	cd ..;
+done
+
+#those have just files in it
+for i in macosx macosx
+do 
+	echo "Entering folder $i...";
+	cd  $i;
+	if [ ! -d backup ]; then
+		echo "Making a backup dir in $i"
+		mkdir backup
+	fi
+	for j in `ls -1 -b  | xargs`
+	do 
+		day=`echo $j |cut -d- -f2`;
+
+		if [[ $day < $DATEMIN ]]; then
+			echo "Older than it should be  $i/$j";
+			DATETEST=`date -d $day +%u`
+			if [[ $DATETEST = 1 ]];
+			then
+				mv $j backup/
+				echo "Backing it up"
+			else	
+				rm -rf $j
+				echo "GO TO /dev/null"
+			fi
+		fi
+	done
+	cd ..;
+done
+
+cd ..; # quitting build/

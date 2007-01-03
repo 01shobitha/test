d9917b3ed6f66d85d7ca9c965c678fff965087b7
Rémi Denis-Courmont <rem@videolan.org>
2007-01-03 15:51:40 +0000

    Proposed advisory

---
+++ www.videolan.org/sa2007-1.php
@@ -0,0 +1,117 @@
+<?php
+   $title = "VideoLAN security advisory 0701";
+   $lang = "en";
+   $date = "3 January 2007";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<h1>Security Advisory 0701</h1>
+<pre>
+Summary           : URL format string injection in CDDA and VCDX plugins
+Date              : 01 January 2007
+Affected versions : VLC media player 0.8.6 down to 0.7.0
+ID                : VideoLAN-SA-0701, MOAB-02-01-2007
+</pre>
+<h2>Details</h2>
+<p>VLC media player CDDA (CD Digital Audio) and VCDX (Video CD) plugins
+are prone to a C-style format string vulnerability
+when trying to open a media resource location.
+The bug occurs when handling error and debug messages
+from underlying library <code>libcdio</code>.
+</p>
+<p>Because the VCDX plugins probes every media resource location unless
+another plugin succesfully opened the resource,
+almost any invalid location can trigger the bug.
+</p>
+<h2>Impact</h2>
+<p>If successful, a malicious third party could use this vulnerability
+to execute arbitrary code within the context of VLC media player (i.e.
+acquire local user privileges on the vulnerable system),
+or crash the player instance.
+</p>
+<h2>Threat mitigation</h2>
+<p>Exploitation of this bug requires passing of a maliciously crafted
+media location to VLC media player.
+While this normally comes from trustworthy user input,
+it can also come from a playlist file (M3U and such),
+from the network through a service discovery plugin (if enabled),
+or embedded in a web page if VLC is installed as a browser plugin.
+</p>
+<h2>Workarounds</h2>
+<p>If support for Audio and Video CDs is not used,
+one can remove the affected plugins manually from the VLC plugin
+&quot;<code>access</code>&quot; directory.
+Relevant filenames are as follow:
+</p>
+<ul>
+ <li><code>access/libcdda_plugin.dll</code> and
+     <code>access/libvcdx_plugin.dll</code> on Windows,
+ </li>
+ <li><code>access/libcdda_plugin.dylib</code> and
+     <code>access/libvcdx_plugin.dylib</code> on MacOS X,
+ </li>
+ <li><code>access/libcdda_plugin.so</code> and
+     <code>access/libvcdx_plugin.so</code> on other operating systems
+     (typically found in <code>/usr/lib</code>
+      or <code>/usr/local/lib</code>).
+ </li>
+</ul>
+<p>Otherwise,
+browser plugins and network-based service discovery mechanisms
+(SAP, UPnP, Bonjour...) should be disabled,
+and untrusted playlist files should not be used.
+</p>
+<h2>Solution</h2>
+<p>Source code patch for VLC 0.8.6 is available
+ <a href="patches/vlc-0.8.6-MOAB-02-01-2007.patch">here</a>
+ (<a href="patches/vlc-0.8.6-MOAB-02-01-2007.patch.asc">GPG signature</a>).
+</p>
+<p>Linux and BSD users should get relevant software upgrade
+from their respective distribution/OS vendor.
+</p>
+<p>Updated binaries for Windows and MacOS X are not available yet.
+The VideoLAN project apologizes for any user inconvenience;
+as a volunteer activity,
+we cannot keep up with a zero-day security vulnerability disclosure.
+</p>
+<h2>Credits</h2>
+<p>This bug was reported by Kevin Finisterre
+as part of the <cite>Month Of Apple Bugs</cite> activity.
+</p>
+<h2>References</h2>
+<ul>
+<li>Public announce:
+ <a href="http://applefun.blogspot.com/2007/01/moab-02-01-2007-vlc-media-player-udp.html"
+ >http://applefun.blogspot.com/2007/01/moab-02-01-2007-vlc-media-player-udp.html</a>
+</li>
+<li>Initial bug report:
+ <a href="http://www.via.ecp.fr/via/ml/vlc-devel/2007-01/msg00005.html"
+ >http://www.via.ecp.fr/via/ml/vlc-devel/2007-01/msg00005.html</a>
+</li>
+<li>Initial patch: <a href="http://trac.videolan.org/vlc/changeset/18481"
+ >http://trac.videolan.org/vlc/changeset/18481</a>
+</li>
+<li>Month of Apple Bugs: <a href="http://applefun.blogspot.com/"
+ >http://applefun.blogspot.com/</a>
+</li>
+<li>The VideoLAN project: <a href="http://www.videolan.org/"
+ >http://www.videolan.org/</a>
+</li>
+</ul>
+<h2>History</h2>
+<dl>
+<dt>03 January 2006</dt>
+<dd>Initial advisory</dd>
+<dd>Patch provided against VLC 0.8.6 source code</dd>
+<dt>02 January 2006</dt>
+<dd>Patch applied to VLC development tree</dd>
+<dd>Bug reported and publicized by Kevin Finisterre</dd>
+</dl>
+
+<address>R&eacute;mi Denis-Courmont,<br />
+on behalf of the VideoLAN project</address>
+
+<?php footer('$Id$'); ?>

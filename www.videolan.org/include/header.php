45d16477d53617e21fc86a9fa42b8e9832feaa12
Jean-Baptiste Kempf <jb@videolan.org>
2007-03-28 22:58:10 +0000

    +the

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -109,7 +109,7 @@ function footer($tag) {
      &nbsp;-&nbsp; <?php echo $tag; ?>
   </p>
   <p>
-  All rights reserved to VideoLAN team. -&nbsp;
+  All rights reserved to the VideoLAN team. -&nbsp;
     <?php if($language=="fr") { } else { echo 'valid'; } ?>
     <a href="http://validator.w3.org/check/referer">XHTML 1.1</a>
     and 

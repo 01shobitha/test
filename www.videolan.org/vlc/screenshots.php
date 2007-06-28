70fefe818aed1962ebc3d3df016ea872a8517edd
Jean-Baptiste Kempf <jb@videolan.org>
2007-06-28 22:13:43 +0000

    Double / corrections to make xtophe pleased.

--- www.videolan.org/vlc/screenshots.php
+++ www.videolan.org/vlc/screenshots.php
@@ -19,16 +19,16 @@ echo"    <div class=\"screenshot\"><a href='http://download.videolan.org/$img'><
 
 <h2>Latest screenshots 0.8.6</h2>
 <?php
-screenshot( "/vlc/screenshots/0.8.6/VLC-0.8.6-beryl.jpg",
-	    "/vlc/screenshots/0.8.6/tm_VLC-0.8.6-beryl.jpg",
+screenshot( "vlc/screenshots/0.8.6/VLC-0.8.6-beryl.jpg",
+	    "vlc/screenshots/0.8.6/tm_VLC-0.8.6-beryl.jpg",
 	    "GNU/Linux Debian - Beryl", "1680x1050" );
 
-screenshot( "/vlc/screenshots/0.8.6/VLC-0.8.6-beryl-ubuntu.jpg",
-	    "/vlc/screenshots/0.8.6/tm_VLC-0.8.6-beryl-ubuntu.jpg",
+screenshot( "vlc/screenshots/0.8.6/VLC-0.8.6-beryl-ubuntu.jpg",
+	    "vlc/screenshots/0.8.6/tm_VLC-0.8.6-beryl-ubuntu.jpg",
 	    "GNU/Linux Ubuntu - Beryl", "1024x768" );
 
-screenshot( "/vlc/screenshots/0.8.6/VLC-mac-Fullscreen.jpg",
-	    "/vlc/screenshots/0.8.6/tm_VLC-mac-Fullscreen.jpg",
+screenshot( "vlc/screenshots/0.8.6/VLC-mac-Fullscreen.jpg",
+	    "vlc/screenshots/0.8.6/tm_VLC-mac-Fullscreen.jpg",
 	    "Mac OS X - FullScreen Controller", "1280x800" );
 
 ?>

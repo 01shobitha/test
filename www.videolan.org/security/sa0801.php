0e5948d15627b62d20489a75560b847b21e6e2e1
Felix Paul Kühne <fkuehne@videolan.org>
2007-12-30 20:26:10 +0000

    * drafts for VideoLAN-SA-0801 and VideoLAN-SA-0802. Feedback is welcome!

---
+++ www.videolan.org/security/sa0801.php
@@ -0,0 +1,70 @@
+<?php
+   $title = "VideoLAN Security Advisory 0801";
+   $lang = "en";
+   $date = "5 January 2008";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<h1>Security Advisory 0801</h1>
+<pre>
+Summary           : Format string vulnerability in the Web interface
+                    Buffer overflow in the Subtitles demuxer
+Date              : 5 January 2008
+Affected versions : VLC media player 0.8.6d and earlier
+ID                : VideoLAN-SA-0801
+CVE reference     : 
+</pre>
+<h2>Details</h2>
+<p>VLC media player's web interface suffers from a format string vulnerability when using specifically crafted clients.
+</p>
+<p>Its subtitle demuxer is prone to multiple buffer-overflows triggered by parsing maliciously crafted files.
+</p>
+<h2>Impact</h2>
+<p>If successful, a malicious third party could remotely use the Web interface or locally use the subtitles parser to execute arbitrary code within the context of VLC media player or crash the current player instance.</p>
+<h2>Threat mitigation</h2>
+<p>Exploitation of the Web interface problem requires the user to actively enable this interface. It is disabled by default.</p>
+<p>The subtitle parsing issue could be exploited by specifically crafted files  both automatically opened because of a similar name to movies being played by the user and by actively opened files.
+</p>
+<h2>Workarounds</h2>
+<p>The user may refrain from using the Web interface until an update is installed. VLC media player can also be controlled remotely through the RC and telnet interfaces.
+</p>
+<p>Automatic detection of subtitle files can be disabled in the Subtitle category inside VLC's Video preferences or by providing <code>--no-sub-autodetect-file</code> when using VLC media player through the command-line. The user is asked to use subtitle files authored by trusted sources only.</p>
+<h2>Solution</h2>
+<p>VLC media player 0.8.6e adresses this issue and introduces further
+usability fixes.
+</p>
+<p>Pre-compiled packages will be available at the usual download locations shortly.
+</p>
+<h2>Credits</h2>
+<p>The Subtitle Parser vulnerability was discovered by Michal Luczaj.
+</p> 
+<p>The Web interface vulnerability was reported by Luigi Auriemma.
+</p>
+<h2>References</h2>
+<dl>
+<dt>Luigi Auriemma</dt>
+<dd><a href="http://aluigi.altervista.org/">Luigi Auriemma</a>, 
+<a href="http://aluigi.altervista.org/adv/vlcboffs-adv.txt">Buffer-overflow and format string in VideoLAN VLC 0.8.6d</a>
+</dd>
+<dt>The VideoLAN Project</dt>
+<dd><a href="http://www.videolan.org/">http://www.videolan.org/</a>
+</dd>
+</dl>
+<h2>History</h2>
+<dl>
+<dt>5 January 2008</dt>
+<dd>VLC 0.8.6e bugfix release</dd>
+<dt>24 December 2007</dt>
+<dd>Bug reported by Luigi Auriemma</dd>
+<dd>Source code fixes for VLC 0.8.6d and development tree</dd>
+</dl>
+
+<address>R&eacute;mi Denis-Courmont, Felix Paul K&uuml;hne,<br />
+on behalf of the VideoLAN project</address>
+
+</div>
+
+<?php footer('$Id$'); ?>
---
+++ www.videolan.org/security/sa0802.php
@@ -0,0 +1,66 @@
+<?php
+   $title = "VideoLAN Security Advisory 0802";
+   $lang = "en";
+   $date = "5 January 2008";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<h1>Security Advisory 0703</h1>
+<pre>
+Summary           : Misusable Stream Output features of the webbrowser plugins
+                    and the M3U Playlist Parser 
+Date              : 5 January 2008
+Affected versions : VLC media player 0.8.6d and earlier
+ID                : VideoLAN-SA-0802
+CVE reference     : 
+</pre>
+<h2>Details</h2>
+<p>Using VLC media player's webbrowser plugins or its M3U Playlist Parser could lead to unwanted file system action within the user's local priviledges.
+</p>
+<h2>Impact</h2>
+<p>If successful, a malicious third party could missuse the Stream Output features of VLC media player's webbrowser plugins and the M3U Playlist Parser to write arbitrary data to all accessible file system locations within the user's priviledges without notifying the user about its actions.
+</p>
+<h2>Threat mitigation</h2>
+<p>Exploitation of these design issues requires the user to visit a malicious
+crafted website using VLC media player's webbrowser plugins or by opening a specifically crafted M3U playlist file.
+</p>
+<h2>Workarounds</h2>
+<p>The user may use stop using VLC media player's <code>--m3u-extvlcopt</code> option, which enables the parsing of exploitable playlist item options. Note the the authors of some M3U files require these options for correct functionality.
+<p>
+VLC media player's webbrowser plugins should be disabled until further action or websites from untrusted sources should not be opened, as there is no work-around available.
+</p>
+<h2>Solution</h2>
+<p>VLC media player 0.8.6e adresses this issue and introduces further
+usability fixes.
+</p>
+<p>Pre-compiled packages will be available shortly at the usual download locations.
+</p>
+<h2>Credits</h2>
+<p>These vulnerabilities were discovered internally by multiple members of the VideoLAN Team, notably Damien Fouilleul and R&eacute;mi Denis-Courmont. 
+</p>
+<h2>References</h2>
+<dl>
+<dt>The VideoLAN project</dt>
+<dd><a href="http://trac.videolan.org/vlc/ticket/1371">trac ticket #1371</a></dd>
+<dd><a href="http://www.videolan.org/">http://www.videolan.org/</a>
+</dd>
+</dl>
+<h2>History</h2>
+<dl>
+<dt>xx January 2008</dt>
+<dd>VLC 0.8.6e bugfix release</dd>
+<dt>xx January 2008</dt>
+<dd>Source code fixes for VLC 0.8.6d and development tree</dd>
+<dt>20 November 2007</dt>
+<dd>Ticket opened</dd>
+</dl>
+
+<address>R&eacute;mi Denis-Courmont, Damien Fouilleul, Felix Paul K&uuml;hne,<br />
+on behalf of the VideoLAN project</address>
+
+</div>
+
+<?php footer('$Id$'); ?>

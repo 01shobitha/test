278fdc1b17fda0e1c9b59d76d00a68921722f15e
Antoine Cellerier <dionoea@videolan.org>
2008-04-06 19:20:17 +0000

    Add correct encoding headers.

--- www.videolan.org/mirror-geo.php
+++ www.videolan.org/mirror-geo.php
@@ -1,3 +1,5 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <!-- Location based randomisation -->
 <!--
 <?php
@@ -106,8 +108,9 @@ echo "$code : $name\n";
 
 <!-- Web page -->
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>VideoLAN - Download from mirror</title>
   <meta http-equiv="refresh" content="0; url=<?php echo $mirror_url.$file ?>" />
  </head>

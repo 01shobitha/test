e862b4568d5157a0cd8b302d91306b9a4c5a778a
Felix Paul Kühne <fkuehne@videolan.org>
2008-04-13 18:43:26 +0000

    Security Advisory for VLC 0.8.6f. Please review

--- www.videolan.org/security/sa0803.php
+++ www.videolan.org/security/sa0803.php
@@ -1,7 +1,7 @@
 <?php
    $title = "VideoLAN Security Advisory 0803";
    $lang = "en";
-   $date = "8 February 2008";
+   $date = "4 April 2008";
    $menu = array( "vlc" );
    require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
 ?>
@@ -20,57 +20,55 @@ an inappropriate misrepresentation of the VideoLAN project position
 </span>
 </p>
 
-<h1>Security Advisory 0803</h1>
+<h1>Security Advisory 0804</h1>
 <pre>
-Summary           : Arbitrary file overwrite and other abuses
-                    through M3U parser and browsers plugins
-Date              : February 2008
-Affected versions : VLC media player 0.8.6d and earlier
-ID                : VideoLAN-SA-0803
-CVE reference     : 
+Summary           : Buffer overflows in multiple modules
+Date              : April 2008
+Affected versions : VLC media player 0.8.6e and earlier
+ID                : VideoLAN-SA-0803, <a href="sa0801.php">VideoLAN-SA-0801</a>
+CVE reference     : CVE-2007-6681, CVE-2008-0073, CVE-2008-1489
 </pre>
 <h2>Details</h2>
-<p>Using VLC media player's webbrowser plugins or its M3U Playlist Parser could lead to unwanted file system action within the user's local privileges.
+<p>VLC media player's following modules suffer from arbitrary memory overwrite vulnerabilities when using specially crafted (invalid) input streams / files:
+Subtitle demuxer, Real RTSP demuxer, MP4 demuxer, Cinepak decoder.
 </p>
 <h2>Impact</h2>
-<p>If successful, a malicious third party could misuse the Stream Output features of VLC media player's webbrowser plugins and the M3U Playlist Parser to write arbitrary data to all accessible file system locations within the user's privileges without notifying the user about its actions.
+<p>If successful, a malicious third part could trigger the execution of arbitrary code within the context of the running instance or terminate the application unexpectedly.
 </p>
 <h2>Threat mitigation</h2>
-<p>Exploitation of these design issues requires the user to visit a malicious
-crafted website using VLC media player's webbrowser plugins or to open a specially crafted M3U playlist file.
+<p>Exploitation of the MP4 demuxer or the Cinepak decoder issues requires the user to explicitly open specially crafted files.
 </p>
+<p>Subtitle files can be opened manually by the user or automatically based on the filename of the movie. Both ways may lead to exploitation of the Subtitle Parser's buffer-overflow.
+</p>
+<p>Exploitation of the Real RTSP problems requires the user to explicitly open streams provided by maliciously third parties.</p>
 <h2>Workarounds</h2>
-<p>The user may stop using VLC media player's <code>--m3u-extvlcopt</code> setting, which enables parsing of exploitable playlist item options. Note that authors of some M3U files require these options for correct functionality.</p>
-<p>
-VLC media player's webbrowser plugins should be disabled until further action or websites from untrusted sources should not be opened, as there is no work-around available.
-</p>
+<p>The user is asked to open Real RTSP streams and MP4 files as well as files containing Cinepak video streams from trusted content providers only. In case of uncertainess, it is recommended not to open this kind of streams or files. RTSP streams can easily be identified by the <code>rtsp</code> prefix of their URL/MRL, while the MP4 container file type is recognizable by the <code>mp4</code> suffix. Cinepak encoded video streams are usually found in MOV and MP4 container files only, which may be perceived by their <code>mp4</code> and <code>mov</code> suffixes.</p>
+<p>Automatic detection of subtitle files can be disabled by unchecking the "Autodetect subtitle files" option in the Subtitle category inside the Video preferences. Note that you need to restart VLC media player for this change to take effect. In case that you use VLC media player through the command-line, provide <code>--no-sub-autodetect-file</code> to override its default behavior.</p><p>The user is asked to use subtitle files authored by trusted sources only.</p>
 <h2>Solution</h2>
-<p>VLC media player 0.8.6e addresses these design issues and introduces further
+<p>VLC media player 0.8.6f addresses these issues and introduces further
 usability fixes.
 </p>
-<p>Pre-compiled packages will be available shortly at the usual download locations.
+<p>Pre-compiled packages are available at the usual download locations.
 </p>
 <h2>Credits</h2>
-<p>These vulnerabilities were discovered internally by multiple members of the VideoLAN Team, notably Damien Fouilleul and R&eacute;mi Denis-Courmont. 
+<p>The Real RTSP demuxer, MP4 demuxer and Cinepak codec vulnerabilities were discovered by Drew Yao of Apple Product Security. Luigi Ariemma reported the Subtitle issue.
 </p>
 <h2>References</h2>
 <dl>
 <dt>The VideoLAN project</dt>
-<dd><a href="http://trac.videolan.org/vlc/ticket/1371">trac ticket #1371</a></dd>
 <dd><a href="http://www.videolan.org/">http://www.videolan.org/</a>
 </dd>
 </dl>
 <h2>History</h2>
 <dl>
-<dt>February 2008</dt>
-<dd>VLC 0.8.6e bugfix release</dd>
-<dt>February 2008</dt>
-<dd>Source code fixes for VLC 0.8.6d and development tree</dd>
-<dt>20 November 2007</dt>
-<dd>Ticket opened</dd>
+<dt>2 April 2008</dt>
+<dd>VLC 0.8.6f bugfix release</dd>
+<dt>March 2008</dt>
+<dd>Source code fixes for VLC 0.8.6f and development tree</dd>
+<dd>Vulnerity reports</dd>
 </dl>
 
-<address>R&eacute;mi Denis-Courmont, Damien Fouilleul, Felix Paul K&uuml;hne,<br />
+<address>Felix Paul K&uuml;hne,<br />
 on behalf of the VideoLAN project</address>
 
 </div>
---
+++ www.videolan.org/security/sa0804.php
@@ -0,0 +1,78 @@
+<?php
+   $title = "VideoLAN Security Advisory 0803";
+   $lang = "en";
+   $date = "8 February 2008";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<p><span style="font-weight: bold;">DISCLAIMER:
+This document is work-in-progress security advisory
+of the VideoLAN project.
+It has not been approved as a security advisory.
+It does not represent the position of the VideoLAN project
+or any of its contributors.
+Citing or quoting this document otherwise may constitute
+an inappropriate misrepresentation of the VideoLAN project position
+(or lack thereof) on the covered subject.
+</span>
+</p>
+
+<h1>Security Advisory 0803</h1>
+<pre>
+Summary           : Arbitrary file overwrite and other abuses
+                    through M3U parser and browsers plugins
+Date              : February 2008
+Affected versions : VLC media player 0.8.6d and earlier
+ID                : VideoLAN-SA-0803
+CVE reference     : 
+</pre>
+<h2>Details</h2>
+<p>Using VLC media player's webbrowser plugins or its M3U Playlist Parser could lead to unwanted file system action within the user's local privileges.
+</p>
+<h2>Impact</h2>
+<p>If successful, a malicious third party could misuse the Stream Output features of VLC media player's webbrowser plugins and the M3U Playlist Parser to write arbitrary data to all accessible file system locations within the user's privileges without notifying the user about its actions.
+</p>
+<h2>Threat mitigation</h2>
+<p>Exploitation of these design issues requires the user to visit a malicious
+crafted website using VLC media player's webbrowser plugins or to open a specially crafted M3U playlist file.
+</p>
+<h2>Workarounds</h2>
+<p>The user may stop using VLC media player's <code>--m3u-extvlcopt</code> setting, which enables parsing of exploitable playlist item options. Note that authors of some M3U files require these options for correct functionality.</p>
+<p>
+VLC media player's webbrowser plugins should be disabled until further action or websites from untrusted sources should not be opened, as there is no work-around available.
+</p>
+<h2>Solution</h2>
+<p>VLC media player 0.8.6e addresses these design issues and introduces further
+usability fixes.
+</p>
+<p>Pre-compiled packages will be available shortly at the usual download locations.
+</p>
+<h2>Credits</h2>
+<p>These vulnerabilities were discovered internally by multiple members of the VideoLAN Team, notably Damien Fouilleul and R&eacute;mi Denis-Courmont. 
+</p>
+<h2>References</h2>
+<dl>
+<dt>The VideoLAN project</dt>
+<dd><a href="http://trac.videolan.org/vlc/ticket/1371">trac ticket #1371</a></dd>
+<dd><a href="http://www.videolan.org/">http://www.videolan.org/</a>
+</dd>
+</dl>
+<h2>History</h2>
+<dl>
+<dt>February 2008</dt>
+<dd>VLC 0.8.6e bugfix release</dd>
+<dt>February 2008</dt>
+<dd>Source code fixes for VLC 0.8.6d and development tree</dd>
+<dt>20 November 2007</dt>
+<dd>Ticket opened</dd>
+</dl>
+
+<address>R&eacute;mi Denis-Courmont, Damien Fouilleul, Felix Paul K&uuml;hne,<br />
+on behalf of the VideoLAN project</address>
+
+</div>
+
+<?php footer('$Id$'); ?>

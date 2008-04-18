69ca744d7e24e5abd7d02f06ed65af5704a3d933
Antoine Cellerier <dionoea@videolan.org>
2008-04-18 20:54:33 +0000

    Again.

--- www.videolan.org/mirror-geo.php
+++ www.videolan.org/mirror-geo.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="utf-8" ?>\n'; ?>
+<?php echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <!-- Location based randomisation -->
 <!--

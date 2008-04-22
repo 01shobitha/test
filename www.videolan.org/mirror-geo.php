8951fd8b8a1f05b6fd07bcee726fdd02e5bc9156
Antoine Cellerier <dionoea@videolan.org>
2008-04-22 12:19:45 +0000

    Fix mirrors page for IE6

--- www.videolan.org/mirror-geo.php
+++ www.videolan.org/mirror-geo.php
@@ -1,5 +1,6 @@
 <?php echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <!-- Location based randomisation -->
 <!--
 <?php
@@ -122,7 +123,6 @@ else
 
 <!-- Web page -->
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>VideoLAN - Download from mirror</title>

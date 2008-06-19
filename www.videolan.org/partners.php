f33aafa9dc70be149511b6044aeeeb3637d1437f
Rémi Denis-Courmont <rem@videolan.org>
2008-06-19 20:12:58 +0000

    Revert [4584] at this point

--- www.videolan.org/partners.php
+++ www.videolan.org/partners.php
@@ -30,6 +30,16 @@ Anevia sells professional video servers and provides development and
 integration of high quality video streaming solutions.</td>
 </tr>
 
+<tr><td colspan="2"><h2>M2X</h2></td></tr>
+<tr>
+<td><p class="center">
+  <a href="http://www.m2x.nl/">
+  <img src="/images/partners/m2x.png" alt="M2X" width="224" height="103" />
+  </a></p>
+</td>
+<td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page</a> for more information on his contributions).</td>
+</tr>
+
 </table>
 
 

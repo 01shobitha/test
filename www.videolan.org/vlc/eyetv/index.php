a6ac15a167870fc3048a0d5a55483733e8db8716
Derk-Jan Hartman <hartman@videolan.org>
2008-08-22 19:53:20 +0000

    preliminary page for EyeTV plugin download.

---
+++ www.videolan.org/vlc/eyetv/index.php
@@ -0,0 +1,19 @@
+<?php
+   $title = "VLC media player - EyeTV capture plugin";
+   $lang = "en";
+   $menu = array( "vlc", "download" );
+   $date = "14 February 2001";
+   $enable_live = true;
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<h1> EyeTV capture plugin </h1>
+
+<div id="description">
+  <h2>Capturing from your EyeTV with VLC media player</h2>
+
+  <p>Here you will soon be able to download a plugin for EyeTV. With this plugin you can open with VLC, mpeg streams produced by the EyeTV application.</p>
+
+</div>
+
+<?php footer('$Id$'); ?>

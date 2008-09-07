c5f2ccf777d9a73bbb828369a8864922de8f07d9
Antoine Cellerier <dionoea@videolan.org>
2008-09-07 22:48:41 +0000

    Add some XP screenshots by nonickname2.

--- www.videolan.org/vlc/screenshots.php
+++ www.videolan.org/vlc/screenshots.php
@@ -26,6 +26,12 @@ screenshot( "0.9.2/qt-albumart.jpg",
 screenshot( "0.9.2/qt-fullscreen.jpg",
             "0.9.2/tm_qt-fullscreen.jpg",
             "GNU/Linux Debian - Qt fullscreen interface", "1042x794" );
+screenshot( "0.9.2/fullscreen_1.jpg",
+            "0.9.2/tm_fullscreen_1.jpg",
+            "Windows XP - Qt F11 fullscreen interface", "1280x800" );
+screenshot( "0.9.2/fullscreen_osd_4.jpg",
+            "0.9.2/tm_fullscreen_osd_4.jpg",
+            "Windows XP - Qt fullscreen interface", "1280x800" );
 ?>
 
 <h2 class="clearme">0.8.6</h2>

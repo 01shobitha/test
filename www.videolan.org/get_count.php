3efad419c4723737c41f4ad6be8df5dd490f9037
Christophe Mutricy <xtophe@videolan.org>
2008-09-14 23:02:26 +0000

    count only 0.9.2

--- www.videolan.org/get_count.php
+++ www.videolan.org/get_count.php
@@ -20,7 +20,7 @@
 	die;
     }
     
-    $request = pg_query($connect, "select sum(number) from (select * from mirrors where file like '%0.8.6%' union select * from mirrors_archive where file like '%0.8.6%') as allmirrors;" );
+    $request = pg_query($connect, "select sum(number) from (select * from mirrors where file like '%0.9.2%' union select * from mirrors_archive where file like '%0.9.2%') as allmirrors;" );
     if( $row = pg_fetch_array($request) )
     {
         $new = date( "U" )." ".$row[0]."\n";

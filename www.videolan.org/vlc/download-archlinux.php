440ac332d61c0ba226fc3cae0042cb1bd8ba5940
Jean-Baptiste Kempf <jb@videolan.org>
2008-09-15 04:10:34 +0000

    ARch Linux updates.

--- www.videolan.org/vlc/download-archlinux.php
+++ www.videolan.org/vlc/download-archlinux.php
@@ -1,7 +1,7 @@
 <?php
    $title = "VLC media player for ALT Linux";
    $lang = "en";
-   $date = "8 May 2006";
+   $date = "15 Sept 2008";
    $menu = array( "vlc", "download" );
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
@@ -10,7 +10,7 @@
 
 <h1>VLC media player for <a href="http://www.archlinux.org/">Arch Linux</a></h1>
 
-Arch Linux  has a VLC package in its 'Extra' repository (see <a href='http://archlinux.org/packages/583/'>http://archlinux.org/packages/583/</a>).
+Arch Linux  has a VLC package in its 'Extra' repository (see <a href='http://www.archlinux.org/packages/extra/i686/vlc/'>http://www.archlinux.org/packages/extra/i686/vlc/</a>).
 
 Issue this command to install:
 <pre>

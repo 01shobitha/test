99e3177a5e63f42c669af0e63b8c0bbcc79e6f80
Christophe Mutricy <xtophe@videolan.org>
2008-10-07 19:34:20 +0000

    Upgrade on the front page also

--- www.videolan.org/include/os-specific.php
+++ www.videolan.org/include/os-specific.php
@@ -43,9 +43,9 @@
     function DoDL( $os, $with_js=1 )
     {
 	$dl = array();
-	$dl["Win32"] = array("/mirror-geo.php?file=vlc/0.9.2/win32/vlc-0.9.2-win32.exe", "Windows (14 MB). This is version 0.9.2, we do not have a 0.9.3 windows release." );
-	$dl["OSX-PPC"] = array("/mirror-geo.php?file=vlc/0.9.3/macosx/vlc-0.9.3-powerpc.dmg", "Mac OS X PowerPC (18 MB)" );
-	$dl["OSX-Intel"] = array( "/mirror-geo.php?file=vlc/0.9.3/macosx/vlc-0.9.3-intel.dmg","Mac OS X Intel (18 MB)" );
+	$dl["Win32"] = array("/mirror-geo.php?file=vlc/0.9.4/win32/vlc-0.9.4-win32.exe", "Windows (14 MB)" );
+	$dl["OSX-PPC"] = array("/mirror-geo.php?file=vlc/0.9.4/macosx/vlc-0.9.4-powerpc.dmg", "Mac OS X PowerPC (18 MB)" );
+	$dl["OSX-Intel"] = array( "/mirror-geo.php?file=vlc/0.9.4/macosx/vlc-0.9.4-intel.dmg","Mac OS X Intel (18 MB)" );
 	$dl["Linux"] =array("/vlc/", "Linux and other OSes" );
 	$dl["Debian"] = array( "/vlc/download-debian.html", "Debian GNU/Linux" );
 	$dl["Ubuntu"] = array( "/vlc/download-ubuntu.html", "Ubuntu Linux");

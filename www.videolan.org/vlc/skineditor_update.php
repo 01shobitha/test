2ac0167980fb97926a653e95e5397dc3de531cf8
Daniel Dreibrodt <altglass@videolan.org>
2008-10-14 16:18:10 +0000

    critical skineditor update system fix

--- www.videolan.org/vlc/skineditor_update.php
+++ www.videolan.org/vlc/skineditor_update.php
@@ -3,7 +3,7 @@ $latestv = "0.7";
 $cdl = "http://d-gfx.kognetwork.ch/VLC/skineditor/vlcse_update_0_7_1.zip";
 
 echo "SKINEDITORUPDATEPAGE\n";
-echo $latestver."\n";
+echo $latestv."\n";
 echo $cdl."\n";
 
 ?>

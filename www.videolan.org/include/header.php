3a9405e3918cacd1f9a69ba182a191d01ebc567d
Jean-Baptiste Kempf <jb@videolan.org>
2008-11-26 15:41:54 +0000

    Add a missing /

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -140,7 +140,7 @@ function footer($tag) {
 	</div> <!-- MAINCONTENT -->
 </div> <!-- PAGECONTAINER -->
 <div id="footer">
-  <p> <a href="/">VideoLAN</a> &nbsp;-&nbsp;<a href="/team">Contact</a>&nbsp;-&nbsp;
+  <p> <a href="/">VideoLAN</a> &nbsp;-&nbsp;<a href="/team/">Contact</a>&nbsp;-&nbsp;
       See the
      <a href="http://www.videolan.org/stats/downloads.html">statistics</a>
      &nbsp;-&nbsp; <?php echo $tag; ?>

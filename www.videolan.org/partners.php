8acab0b419636bc36df72cb4a5a643954fd3ca83
Jean-Baptiste Kempf <jb@videolan.org>
2009-01-07 11:17:57 +0000

    Update partners page.

--- www.videolan.org/partners.php
+++ www.videolan.org/partners.php
@@ -36,7 +36,20 @@ integration of high quality video streaming solutions.</td>
   <img src="http://images.videolan.org/images/partners/m2x.png" alt="M2X" width="224" height="103" />
   </a></p>
 </td>
-<td>M2X is an ICT company that is specialized in using Open Source software for providing solutions to IT problem areas of Networking (wired or wireless), Embedded and Multimedia. The founder of the company is a VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page</a> for more information on his contributions).</td>
+<td>
+<p>M2X is an ICT company that is specialized in using Open Source software
+for providing solutions to IT problem areas of Networking (wired or
+ wireless), Embedded and Multimedia. The founder of the company is a
+VideoLAN developer Jean-Paul Saman (see the <a href="/team/">VideoLAN team page</a>
+for more information on his contributions).</p>
+
+<p>In typical modern ICT solutions the three expertise areas Networking,
+   Embedded and Multimedia are present. Either being targeted at consumer
+   electronics (eg: settop boxes) for IPTV solutions or big servers
+   machines. M2X provides innovative solutions for these use cases through
+   its experience in developing and integrating Open Source software like
+   VLC and embedded linux in customer products.</p>
+   </td>
 </tr>
 
 </table>

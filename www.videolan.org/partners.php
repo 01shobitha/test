b85adf1e61903ac51614b0abe6476f67ff3f634d
Jean-Baptiste Kempf <jb@videolan.org>
2009-01-20 08:35:04 +0000

    Fixes and space

--- www.videolan.org/partners.php
+++ www.videolan.org/partners.php
@@ -57,8 +57,8 @@ integration of high quality video streaming solutions.</td>
 
 <h1> Partners </h1>
 
-<p> Such an experience would never have been born without the invaluable      
-cooperation of our partners, on both technical and financial sides: </p>      
+<p> Such an experience would never have been born without the invaluable
+cooperation of our partners, on both technical and financial sides: </p>
 
 <table border="0" cellspacing="0">
 
@@ -128,12 +128,12 @@ cooperation of our partners, on both technical and financial sides: </p>
     </td>
     <td>
 
-        <p>The <a href="http://www.ecp.fr/">École Centrale Paris</a> and      
-        it's <a href="http://www.msio.ecp.fr/">Master in Open Information     
-        System</a> are historic partners of the project. The Master in Open   
-        Information System aims to enable the student to acquire a real       
-        competence in the field of Open Computing Systems and supports        
-        VideoLAN as a key developement project. </p>                          
+        <p>The <a href="http://www.ecp.fr/">École Centrale Paris</a> and
+        it's <a href="http://www.msio.ecp.fr/">Master in Open Information
+        System</a> are historic partners of the project. The Master in Open
+        Information System aims to enable the student to acquire a real
+        competence in the field of Open Computing Systems and supports
+        VideoLAN as a key developement project. </p>
 
     </td>
   </tr>
@@ -151,26 +151,27 @@ cooperation of our partners, on both technical and financial sides: </p>
     </td>
     <td>
 
-         <p>HP gave a DL 385 server which is now running                
-         most of the VideoLAN web sites and developement                
-         tools : <a href="http://www.videolan.org">WWW</a>,             
-         <a href="http://forum.videolan.org">Forum</a>, <a              
-	 href="http://download.videolan.org/pub/videolan/">Downloads</a>, 
-<a href="http://wiki.videolan.org/Subversion">SVN</a> and     
-         <a href="https://trac.videolan.org">Trac</a>. This web         
-         server was shipped with two AMD opteron 2.4 GHz CPUs, 8 GB     
-         RAM and 4 36 GB SCSI hard drives. VideoLAN also won a <a       
-         href="http://images.videolan.org/images/sirius1.png">nice    
-         HP laptop</a> at <a                                            
+         <p>HP gave a DL 385 server which is now running
+         most of the VideoLAN web sites and developement
+         tools : <a href="http://www.videolan.org">WWW</a>,
+         <a href="http://forum.videolan.org">Forum</a>, <a
+     href="http://download.videolan.org/pub/videolan/">Downloads</a>,
+<a href="http://wiki.videolan.org/Subversion">SVN</a> and
+         <a href="https://trac.videolan.org">Trac</a>. This web
+         server was shipped with two AMD opteron 2.4 GHz CPUs, 8 GB
+         RAM and 4 36 GB SCSI hard drives. VideoLAN also won a <a
+         href="http://images.videolan.org/images/sirius1.png">nice
+         HP laptop</a> at <a
          href="http://www.tropheesdulibre.org/article.php3?id_article=47"
-         >Journées du Libre</a>.</p>                                 
+         >Journées du Libre</a>.</p>
 
     </td>
   </tr>
-
+</table>
 
 <h1> Past Partners </h1>
 <p> We would like to thank other companies that helped us in the past, on both technical and financial sides: </p>
+<table border="0" cellspacing="0">
   <tr>
     <td colspan="2">
         <h2> IBM </h2>
@@ -183,39 +184,39 @@ cooperation of our partners, on both technical and financial sides: </p>
     </td>
     <td>
 
-        <p>IBM gave two Netfinity servers (<a                           
-        href="http://www.via.ecp.fr/inauguration.html">see picture</a>  
-        - Nov 2000) and an xSeries server (<a href="http://www.via.ecp.fr/membres/vie/montage-ibm-2005/">April 2005</a>) to the <a        
-        href="http://www.via.ecp.fr/via/index.html?lang=en">VIA         
-        Centrale Réseaux</a> association which manages the network of   
-        the student campus of the <a href="http://www.ecp.fr/">École    
-        Centrale Paris</a>. These servers now run the <a                
-        href="/developers/lists.html">mailing        
-        lists</a>, <a                                                   
+        <p>IBM gave two Netfinity servers (<a
+        href="http://www.via.ecp.fr/inauguration.html">see picture</a>
+        - Nov 2000) and an xSeries server (<a href="http://www.via.ecp.fr/membres/vie/montage-ibm-2005/">April 2005</a>) to the <a
+        href="http://www.via.ecp.fr/via/index.html?lang=en">VIA
+        Centrale Réseaux</a> association which manages the network of
+        the student campus of the <a href="http://www.ecp.fr/">École
+        Centrale Paris</a>. These servers now run the <a
+        href="/developers/lists.html">mailing
+        lists</a>, <a
         href="ftp://ftp.videolan.org/pub/videolan/">FTP</a>, <a href="http://wiki.videolan.org">Wiki</a>
-        and DNS services. IBM also awarded VideoLAN in the <a           
-        href="http://www-1.ibm.com/linux/news/ilsc.shtml">Linux         
-        Challenge</a>, an international challenge for opensource        
-        projects developed by university students.</p>                  
+        and DNS services. IBM also awarded VideoLAN in the <a
+        href="http://www-1.ibm.com/linux/news/ilsc.shtml">Linux
+        Challenge</a>, an international challenge for opensource
+        projects developed by university students.</p>
 
     </td>
   </tr>
 
   <tr>
     <td colspan="2">
-	<h2> British Telecom Exact Technologies </h2>
+    <h2> British Telecom Exact Technologies </h2>
     </td>
   </tr>
   <tr>
     <td>
-	<p class="center"><a href="http://btplc.com/Innovation/"><img
-	src="http://images.videolan.org/images/partners/BTexact.png" width="160" height="56" alt="BTexact
-	Technologies" /></a></p>
+    <p class="center"><a href="http://btplc.com/Innovation/"><img
+    src="http://images.videolan.org/images/partners/BTexact.png" width="160" height="56" alt="BTexact
+    Technologies" /></a></p>
     </td>
     <td>
-	<p>British Telecom Exact Technologies developed an IPv6 port of
-	the VideoLAN solution, and contributed it back to the VideoLAN project
-	under the GPL license.</p>
+    <p>British Telecom Exact Technologies developed an IPv6 port of
+    the VideoLAN solution, and contributed it back to the VideoLAN project
+    under the GPL license.</p>
     </td>
   </tr>
 
@@ -231,82 +232,82 @@ cooperation of our partners, on both technical and financial sides: </p>
     </td>
     <td>
 
-        <p>AT&amp;T developed a multicast support for VLC, and          
-        contributed it back to the VideoLAN project under the GPL       
-        license.</p>                                                    
+        <p>AT&amp;T developed a multicast support for VLC, and
+        contributed it back to the VideoLAN project under the GPL
+        license.</p>
 
     </td>
   </tr>
 
   <tr>
     <td colspan="2">
-	<h2> VTHR </h2>
+    <h2> VTHR </h2>
     </td>
   </tr>
   <tr>
     <td>
-	<p class="center"><img
-	src="http://images.videolan.org/images/partners/vthr.png" width="149" height="50" alt="VTHR" /></p>
+    <p class="center"><img
+    src="http://images.videolan.org/images/partners/vthr.png" width="149" height="50" alt="VTHR" /></p>
     </td>
     <td>
 
- <p>VTHR used to be is the first digital network for High-Resolution               
- Video Transmission. We have developed the satellite input,             
- deinterlacing routines and support for 4:2:2 MPEG-2 in VLC             
- for them. VTHR gave us a computer which used to host the <a          
- href="http://nightlies.videolan.org/">VLC nightly builds</a> 
- and is still used for development.</p> 
+ <p>VTHR used to be is the first digital network for High-Resolution
+ Video Transmission. We have developed the satellite input,
+ deinterlacing routines and support for 4:2:2 MPEG-2 in VLC
+ for them. VTHR gave us a computer which used to host the <a
+ href="http://nightlies.videolan.org/">VLC nightly builds</a>
+ and is still used for development.</p>
 
-    </td>
-  </tr>
-
-  <tr>
-    <td colspan="2">
-	<h2> SNCF - Direction de la Recherche </h2>
-    </td>
-  </tr>
-  <tr>
-    <td>
-	<p class="center"><a href="http://recherche.sncf.com/"><img
-	src="http://images.videolan.org/images/partners/sncf.png" alt="SNCF" width="146" height="61" /></a></p>
-    </td>
-    <td>
-	<p>SNCF - Direction de la Recherche (French Railways - Research
-	Area) hired Samuel Hocevar, a VLC developer, to work full-time on
-	enhancing VideoLAN.</p>
     </td>
   </tr>
 
   <tr>
     <td colspan="2">
-	<h2> I.D.M </h2>
+    <h2> SNCF - Direction de la Recherche </h2>
     </td>
   </tr>
   <tr>
     <td>
-	<p class="center"><a href="http://www.idm.fr"><img
-	src="http://images.videolan.org/images/partners/idm.png" alt="IDM" width="64" height="64" /></a></p>
+    <p class="center"><a href="http://recherche.sncf.com/"><img
+    src="http://images.videolan.org/images/partners/sncf.png" alt="SNCF" width="146" height="61" /></a></p>
     </td>
     <td>
-	<p>I.D.M hired Simon Latapie, a VLC developer, to work on enhancing VideoLAN.
-	</p>
+    <p>SNCF - Direction de la Recherche (French Railways - Research
+    Area) hired Samuel Hocevar, a VLC developer, to work full-time on
+    enhancing VideoLAN.</p>
     </td>
   </tr>
 
   <tr>
     <td colspan="2">
-	<h2> LaCie </h2>
+    <h2> I.D.M </h2>
     </td>
   </tr>
   <tr>
     <td>
-	<p class="center"><a href="http://www.lacie.com"><img
-	src="http://images.videolan.org/images/partners/LaCie.png" alt="LaCie" width="142" height="61" /></a></p>
+    <p class="center"><a href="http://www.idm.fr"><img
+    src="http://images.videolan.org/images/partners/idm.png" alt="IDM" width="64" height="64" /></a></p>
     </td>
     <td>
-	<p>LaCie offered the team four DVD burners and a 1.6 Terabytes
+    <p>I.D.M hired Simon Latapie, a VLC developer, to work on enhancing VideoLAN.
+    </p>
+    </td>
+  </tr>
+
+  <tr>
+    <td colspan="2">
+    <h2> LaCie </h2>
+    </td>
+  </tr>
+  <tr>
+    <td>
+    <p class="center"><a href="http://www.lacie.com"><img
+    src="http://images.videolan.org/images/partners/LaCie.png" alt="LaCie" width="142" height="61" /></a></p>
+    </td>
+    <td>
+    <p>LaCie offered the team four DVD burners and a 1.6 Terabytes
            Hard Drive.
-	</p>
+    </p>
     </td>
   </tr>
 

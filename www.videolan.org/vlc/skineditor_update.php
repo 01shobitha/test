1116f2bbee1f743035f012f0be8fd16a809ec500
Daniel Dreibrodt <altglass@videolan.org>
2009-02-23 12:22:55 +0000

    another update script fix

--- www.videolan.org/vlc/skineditor_update.php
+++ www.videolan.org/vlc/skineditor_update.php
@@ -9,10 +9,10 @@ if(isset($_GET['os'])) $os = $_GET['os'];
 
 $header = "SKINEDITORUPDATEPAGE";
 
-//Before version 0.8 no operating system information was transmitted
+//Before version 0.8.0 no operating system information was transmitted
 if($v=="0.7.1") {
   echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_7_to_0_8_1.zip\n";    
-} else if($v=="0.8") {  
+} else if($v=="0.8.0") {  
   if(!(stripos($os,"Windows")===FALSE)) {
     //Windows package
     echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";    
@@ -20,6 +20,8 @@ if($v=="0.7.1") {
     //Unix package
     echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";    
   }
+} else {
+  echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";
 }
 
 ?>

64ee7f1ede2bae3a767fd9a9cb9938f5ac80a13b
Daniel Dreibrodt <altglass@videolan.org>
2009-02-23 12:33:35 +0000

    last but not least...

--- www.videolan.org/vlc/skineditor_update.php
+++ www.videolan.org/vlc/skineditor_update.php
@@ -1,7 +1,7 @@
 <?php
 //Version of the user
 $v = "0.7.1";
-//Os of the user
+//Operating system of the user
 $os = "Windows";
 
 if(isset($_GET['v'])) $v = $_GET['v'];
@@ -9,10 +9,23 @@ if(isset($_GET['os'])) $os = $_GET['os'];
 
 $header = "SKINEDITORUPDATEPAGE";
 
-//Before version 0.8.0 no operating system information was transmitted
+//Update information for versions prior to 0.8
 if($v=="0.7.1") {
   echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_7_to_0_8_1.zip\n";    
-} else if($v=="0.8.0") {  
+}
+
+//Update information for version 0.8
+else if($v=="0.8") {  
+  if(!(stripos($os,"Windows")===FALSE)) {
+    //Windows package
+    echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1_w32.zip\n";    
+  } else {
+    //Unix package
+    echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";    
+  }
+}
+//Update information for version 0.8.1
+else if($v=="0.8.1") {  
   if(!(stripos($os,"Windows")===FALSE)) {
     //Windows package
     echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";    
@@ -20,8 +33,11 @@ if($v=="0.7.1") {
     //Unix package
     echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";    
   }
-} else {
-  echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_8_to_0_8_1.zip\n";
+} 
+
+//Update information for any other version
+else {
+  echo $header."\n0.8.1\nhttp://www.videolan.org/mirror-geo-redirect.php?file=skin-editor/0.8.1/VLCSkinEditor_update_0_7_to_0_8_1.zip\n";
 }
 
 ?>

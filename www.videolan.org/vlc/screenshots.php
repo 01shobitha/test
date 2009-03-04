f059313a58c752094dc43259595ab723f0226ec0
Rémi Duraffort <ivoire@videolan.org>
2009-03-04 22:33:11 +0000

    revert previsous commit (more change needed).

--- www.videolan.org/vlc/screenshots.php
+++ www.videolan.org/vlc/screenshots.php
@@ -21,70 +21,70 @@ echo"    <div class=\"screenshot\"><a href='http://images.videolan.org/vlc/scree
 <?php
 screenshot( "0.9.2/qt-albumart.jpg",
             "0.9.2/tm_qt-albumart.jpg",
-            "VLC media player on GNU/Linux Debian - Gnome - Qt interface and album art", "1280x800" );
+            "GNU/Linux Debian - Gnome - Qt interface and album art", "1280x800" );
 screenshot( "0.9.2/qt-fullscreen.jpg",
             "0.9.2/tm_qt-fullscreen.jpg",
-            "VLC media player on GNU/Linux Debian - Gnome - Qt fullscreen controller", "1042x794" );
+            "GNU/Linux Debian - Gnome - Qt fullscreen controller", "1042x794" );
 screenshot( "0.9.2/qt-info.jpg",
             "0.9.2/tm_qt-info.jpg",
-            "VLC media player on GNU/Linux Debian - KDE 3 - Qt interface and compiz", "1280x800" );
+            "GNU/Linux Debian - KDE 3 - Qt interface and compiz", "1280x800" );
 screenshot( "0.9.2/screenshot-kde4.jpg",
             "0.9.2/tm_screenshot-kde4.jpg",
-            "VLC media player on Arch Linux - KDE 4 - Qt interface, album art and simple preferences", "1680x1050" );
+            "Arch Linux - KDE 4 - Qt interface, album art and simple preferences", "1680x1050" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "0.9.2/fullscreen_1.jpg",
             "0.9.2/tm_fullscreen_1.jpg",
-            "VLC media player on Windows XP - Qt fullscreen interface", "1280x800" );
+            "Windows XP - Qt fullscreen interface", "1280x800" );
 screenshot( "0.9.2/fullscreen_osd_4.jpg",
             "0.9.2/tm_fullscreen_osd_4.jpg",
-            "VLC media player on Windows XP - Qt fullscreen controller", "1280x800" );
+            "Windows XP - Qt fullscreen controller", "1280x800" );
 screenshot( "0.9.2/vista_colorthreshold.jpg",
             "0.9.2/tm_vista_colorthreshold.jpg",
-            "VLC media player on Windows Vista - Qt interface and filters", "1280x1024" );
+            "Windows Vista - Qt interface and filters", "1280x1024" );
 screenshot( "0.9.2/osx-0.9.2-ASS-softsubs.jpg",
             "0.9.2/tm_osx-0.9.2-ASS-softsubs.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Substation Alpha subtitle rendering in matroska files", "1680x1050" );
+            "Mac OS X 10.5.4 - Substation Alpha subtitle rendering in matroska files", "1680x1050" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "0.9.2/osx-0.9.2-equalizer.jpg",
             "0.9.2/tm_osx-0.9.2-equalizer.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Equalizer, folded controller  and Dirac encoded video", "958x473" );
+            "Mac OS X 10.5.4 - Equalizer, folded controller  and Dirac encoded video", "958x473" );
 screenshot( "0.9.2/osx-0.9.2-goom-albumart-tag.jpg",
             "0.9.2/tm_osx-0.9.2-goom-albumart-tag.jpg",
-            "VLC media player on Mac OS X 10.5.4 - MP3 tag editing and Goom visualization", "978x693" );
+            "Mac OS X 10.5.4 - MP3 tag editing and Goom visualization", "978x693" );
 screenshot( "0.9.2/osx-0.9.2-prefs.jpg",
             "0.9.2/tm_osx-0.9.2-prefs.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Video section of the simplified Preferences", "650x568" );
+            "Mac OS X 10.5.4 - Video section of the simplified Preferences", "650x568" );
 screenshot( "0.9.2/osx-0.9.2-gradient.jpg",
             "0.9.2/tm_osx-0.9.2-gradient.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Video filter panel with the Gradient filter applied", "909x564" );
+            "Mac OS X 10.5.4 - Video filter panel with the Gradient filter applied", "909x564" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "0.9.2/osx-0.9.2-inverted.jpg",
             "0.9.2/tm_osx-0.9.2-inverted.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Image adjust panel with the Inverted colors-filter applied", "866x572" );
+            "Mac OS X 10.5.4 - Image adjust panel with the Inverted colors-filter applied", "866x572" );
 screenshot( "0.9.2/osx-0.9.2-teletext.jpg",
             "0.9.2/tm_osx-0.9.2-teletext.jpg",
-            "VLC media player on Mac OS X 10.5.4 - Teletext presentation on EyeTV captured Digital Terrestial television signal", "1004x705" );
+            "Mac OS X 10.5.4 - Teletext presentation on EyeTV captured Digital Terrestial television signal", "1004x705" );
 ?>
 
 <h2 class="clearme">0.8.6</h2>
 <?php
 screenshot( "0.8.6/VLC-0.8.6-beryl.jpg",
 	    "0.8.6/tm_VLC-0.8.6-beryl.jpg",
-	    "VLC media player on GNU/Linux Debian - Beryl", "1680x1050" );
+	    "GNU/Linux Debian - Beryl", "1680x1050" );
 
 screenshot( "0.8.6/VLC-0.8.6-beryl-ubuntu.jpg",
 	    "0.8.6/tm_VLC-0.8.6-beryl-ubuntu.jpg",
-	    "VLC media player on GNU/Linux Ubuntu - Beryl", "1024x768" );
+	    "GNU/Linux Ubuntu - Beryl", "1024x768" );
 
 screenshot( "0.8.6/VLC-mac-Fullscreen.jpg",
 	    "0.8.6/tm_VLC-mac-Fullscreen.jpg",
-	    "VLC media player on Mac OS X - FullScreen Controller", "1280x800" );
+	    "Mac OS X - FullScreen Controller", "1280x800" );
 
 ?>
 
@@ -94,20 +94,20 @@ screenshot( "0.8.6/VLC-mac-Fullscreen.jpg",
 <?php
 screenshot( "0.8.5/VLC-screenshot-0.8.5.JPG",
             "0.8.5/tm_VLC-screenshot-0.8.5.JPG",
-            "VLC media player using Skins2 - Blista skin","1280x1024" );
+            "Skins2 - Blista skin","1280x1024" );
 screenshot( "0.8.5/VLC-default-skin.jpg",
             "0.8.5/tm_VLC-default-skin.jpg",
-            "VLC media player using Skins2 - New default skin", "1024x768" );
+            "Skins2 - New default skin", "1024x768" );
 ?>
 <h2 class="clearme">0.8.4</h2>
 
 <?php
 screenshot( "0.8.4/vista_screenshot.png",
             "0.8.4/tm_vista_screenshot.png",
-            "VLC media player on Windows Vista Beta 1","1280x1024" );
+            "Windows Vista Beta 1","1280x1024" );
 screenshot( "0.8.4/zeta_R1.1.jpg",
             "0.8.4/tm_zeta_R1.1.jpg",
-            "VLC media player on ZetaOS R1.1", "1280x1024" );
+            "ZetaOS R1.1", "1280x1024" );
 ?>
 
 <h2 class="clearme">0.8.2</h2>
@@ -115,43 +115,43 @@ screenshot( "0.8.4/zeta_R1.1.jpg",
 <?php
 screenshot( "0.8.2/linux-wxwin-playlist.png",
             "0.8.2/tm_linux-wxwin-playlist.png",
-            "VLC media player on Linux - Playlist", "1280x1024" );
+            "Linux - Playlist", "1280x1024" );
 screenshot( "0.8.2/linux-wxwin-prefs.png",
             "0.8.2/tm_linux-wxwin-prefs.png",
-            "VLC media player on Linux - Preferences", "1280x1024" );
+            "Linux - Preferences", "1280x1024" );
 screenshot( "0.8.2/linux-skins2-MediaPlayer.jpg",
             "0.8.2/tm_linux-skins2-MediaPlayer.jpg",
-            "VLC media player using Skins - Media Player skin", "1024x768" );
+            "Skins - Media Player skin", "1024x768" );
 screenshot( "0.8.2/linux-wxwin-mosaic.jpg",
             "0.8.2/tm_linux-wxwin-mosaic.jpg",
-            "VLC media player using Mosaïc" , "1024x768" );
+            "Mosaïc" , "1024x768" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "0.8.2/windows-wxwin-hdtv.png",
             "0.8.2/tm_windows-wxwin-hdtv.png",
-            "VLC media player on Windows - HD TV", "1280x1024" );
+            "Windows - HD TV", "1280x1024" );
 screenshot( "0.8.2/windows-wxwin-playlist.png",
             "0.8.2/tm_windows-wxwin-playlist.png",
-            "VLC media player on Windows - Playlist", "1280x1024" );
+            "Windows - Playlist", "1280x1024" );
 screenshot( "0.8.2/macosx-mp3.jpg",
             "0.8.2/tm_macosx-mp3.jpg",
-            "VLC media player on Mac OS X - Playlist", "1024x768") ;
+            "Mac OS X - Playlist", "1024x768") ;
 screenshot( "0.8.2/macosx-serenity.jpg",
             "0.8.2/tm_macosx-serenity.jpg",
-            "VLC media player on Mac OS X - Video","1024x768" );
+            "Mac OS X - Video","1024x768" );
 ?>
 <p class="clearme"></p>
 <?php 
 screenshot( "0.8.2/linux-wxwin-all.png",
             "0.8.2/tm_linux-wxwin-all.png",
-            "VLC media player on Linux", "1280x1024" );
+            "Linux", "1280x1024" );
 screenshot( "0.8.2/neutron-osx-screenshot.jpg",
             "0.8.2/tm_neutron-osx-screenshot.jpg",
-            "VLC media player on Mac OS X - Video","1024x768" );
+            "Mac OS X - Video","1024x768" );
 screenshot( "0.8.2/zeta_R1_Screenie.jpg",
             "0.8.2/tm_zeta_R1_Screenie.jpg",
-            "VLC media player on Zeta (BeOS) - Video", "1280x1024" );
+            "Zeta (BeOS) - Video", "1280x1024" );
 ?>
 
 <h2 class="clearme">0.7.2</h2>
@@ -159,46 +159,46 @@ screenshot( "0.8.2/zeta_R1_Screenie.jpg",
 <?php
 screenshot( "20040531-osx-1.jpg",
             "tn_20040531-osx-1.jpg",
-            "VLC media player on Mac OS X", "1280x960" );
+            "Mac OS X", "1280x960" );
 screenshot( "20040531-osx-2.jpg",
             "tn_20040531-osx-2.jpg",
-            "VLC media player on Mac OS X", "1024x768" );
+            "Mac OS X", "1024x768" );
 screenshot( "20040531-linux-1.jpg",
             "tn_20040531-linux-1.jpg",
-            "VLC media player using wxGTK on Linux", "1280x960");
+            "wxGTK Linux", "1280x960");
 screenshot( "20040531-linux-2.jpg",
             "tn_20040531-linux-2.jpg",
-            "VLC media player using wxGTK on Linux", "1280x960" );
+            "wxGTK Linux", "1280x960" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "20040601-linux-wx-subs.jpg",
             "tn_20040601-linux-wx-subs.jpg",
-            "VLC media player on Linux (UTF8 subtitles)", "1152x864" );
+            "Linux (UTF8 subtitles)", "1152x864" );
 screenshot( "20040601-linux-skins2-6.jpg",
             "tn_20040601-linux-skins2-6.jpg",
-            "VLC media player on Linux - Skins2", "1280x1024" );
+            "Linux Skins2", "1280x1024" );
 screenshot( "20040601-windows-skins2-1.jpg",
             "tn_20040601-windows-skins2-1.jpg",
-            "VLC media player on Windows - Skins2", "1280x1024" );
+            "Windows Skins2", "1280x1024" );
 screenshot( "20040602-windows-wxembed-2.jpg",
             "tn_20040602-windows-wxembed-2.jpg",
-            "VLC media player on Windows - WxWidgets", "1280x1024" );
+            "Windows WxWidgets", "1280x1024" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "20040602-windows-wxembed-4.jpg",
             "tn_20040602-windows-wxembed-4.jpg",
-            "VLC media player on Windows with Goom", "1280x1024" );
+            "Windows with Goom", "1280x1024" );
 screenshot( "20040602-windows-wxembed-5.jpg",
             "tn_20040602-windows-wxembed-5.jpg",
-            "VLC media player on Windows with Goom", "1280x1024" );
+            "Windows with Goom", "1280x1024" );
 screenshot( "20040602-beos-1.png",
             "tn_20040602-beos-1.png",
-            "VLC media player on BeOS", "800x550" );
+            "BeOS", "800x550" );
 screenshot( "20040602-beos-2.png",
             "tn_20040602-beos-2.png",
-            "VLC media player on BeOS", "1072x766" );
+            "BeOS", "1072x766" );
 ?>
 
 <h2 class="clearme">0.7.0</h2>
@@ -206,10 +206,10 @@ screenshot( "20040602-beos-2.png",
 <?php
 screenshot( "20040110-osx-00.jpg",
             "tn_20040110-osx-00.jpg",
-            "VLC media player on MacOS X","1051x651" );
+            "MacOS X","1051x651" );
 screenshot( "20040303-osx-00.jpg",
             "tn_20040303-osx-00.jpg",
-            "VLC media player on MacOS X","1024x768" );
+            "MacOS X","1024x768" );
 ?>
 
 <h2 class="clearme">VLC for Familiar Linux screenshots</h2>
@@ -233,22 +233,22 @@ screenshot( "familiar20021121-04.png",
 <?php
 screenshot( "20030731-wxgtk-00.png",
             "tn_20030731-wxgtk-00.jpg",
-            "VLC media player using wxWindows on Linux","1024x768" );
+            "wxWindows Linux","1024x768" );
 screenshot( "20030731-wxgtk-01.png",
             "tn_20030731-wxgtk-01.jpg",
-            "VLC media player using wxWindows on Linux", "1024x768" );
+            "wxWindows Linux", "1024x768" );
 screenshot( "20030731-wxgtk-02.png",
             "tn_20030731-wxgtk-02.jpg",
-            "VLC media player using wxWindows on Linux", "1024x768" );
+            "wxWindows Linux", "1024x768" );
 screenshot( "20030731-wxgtk-03.png",
             "tn_20030731-wxgtk-03.jpg",
-            "VLC media player using wxWindows on Linux", "1024x768" );
+            "wxWindows Linux", "1024x768" );
 ?>
 <p class="clearme"></p>
 <?php
 screenshot( "20030810-beos.jpg",
             "tn_20030810-beos.jpg",
-            "VLC media player on BeOS", "1280x1024" );
+            "BeOS", "1280x1024" );
 ?>
 
 <h3 class="clearme">VLC 0.5.0</h3>
@@ -256,40 +256,40 @@ screenshot( "20030810-beos.jpg",
 <?php
 screenshot( "20030228-gnome-01.png",
             "tn_20030228-gnome-01.jpg",
-            "VLC media player on Gnome", "1024x768" );
+            "Gnome screenshot", "1024x768" );
 screenshot( "20030228-gnome-02.png",
             "tn_20030228-gnome-02.jpg",
-            "VLC media player on Gnome", "1024x768" );
+            "Gnome screenshot", "1024x768" );
 screenshot( "20030228-gnome-03.png",
             "tn_20030228-gnome-03.jpg",
-            "VLC media player on Gnome", "1024x768" );
+            "Gnome screenshot", "1024x768" );
 screenshot( "20030202-win32-01.png",
             "tn_20030202-win32-01.jpg",
-            "VLC media player on Windows", "1024x768" );
+            "Windows screenshot", "1024x768" );
 ?>
 <p class="clearme"> </p>
 <?php
 screenshot( "20030202-osx-00.jpg",
             "tn_20030202-osx-00.jpg",
-            "VLC media player on Mac OS X","1152x768" );
+            "Mac OS X screenshot","1152x768" );
 screenshot( "20030202-osx-01.jpg",
             "tn_20030202-osx-01.jpg",
-            "VLC media player on Mac OS X", "1152x768" );
+            "Mac OS X screenshot", "1152x768" );
 screenshot( "20030202-osx-02.jpg",
             "tn_20030202-osx-02.jpg",
-            "VLC media player on Mac OS X",  "1152x768" );
+            "Mac OS X screenshot",  "1152x768" );
 screenshot( "20030202-osx-03.jpg",
             "tn_20030202-osx-03.jpg",
-            "VLC media player on Mac OS X", "1152x768" );
+            "Mac OS X screenshot", "1152x768" );
 ?>
 <p class="clearme"> </p>
 <?php
 screenshot( "20030202-osx-04.jpg",
             "tn_20030202-osx-04.jpg",
-            "VLC media player on Mac OS X", "1152x768" );
+            "Mac OS X screenshot", "1152x768" );
 screenshot( "20030202-osx-05.jpg",
             "tn_20030202-osx-05.jpg",
-            "VLC media player on Mac OS X", "1152x768" );
+            "Mac OS X screenshot", "1152x768" );
 ?>
 
 
@@ -298,22 +298,22 @@ screenshot( "20030202-osx-05.jpg",
 <?php
 screenshot( "20020406-00.jpeg",
             "tn_20020406-00.jpeg",
-            "VLC media player on Gnome", "1024x768" );
+            "Gnome screenshot","1024x768" );
 screenshot( "20020406-01.jpeg",
             "tn_20020406-01.jpeg",
-            "VLC media player on Gnome","1024x768" );
+            "Gnome screenshot","1024x768" );
 screenshot( "20020406-02.jpeg",
             "tn_20020406-02.jpeg",
-            "VLC media player on Gnome","1024x768" );
+            "Gnome screenshot","1024x768" );
 screenshot( "20020411-00.jpeg",
             "tn_20020411-00.jpeg",
-            "VLC media player on Mac OS X", "1152x768" );
+            "Mac OS X screenshot", "1152x768" );
 ?>
 <p class="clearme"> </p>
 <?php
 screenshot( "20020424-00.jpeg",
             "tn_20020424-00.jpeg",
-            "VLC media player on Windows", "1024x768" );
+            "Windows screenshot", "1024x768" );
 ?>
 
 <h3 class="clearme">VLC 0.2.x</h3>
@@ -327,16 +327,16 @@ eventually a <b>BeOS</b> screenshot. </p>
 <?php
 screenshot( "20010409-00.png",
             "tn_20010409-00.jpeg",
-            "VLC media player on Gnome", "1024x768" );
+            "Gnome screenshot", "1024x768" );
 screenshot( "20010409-01.png",
             "tn_20010409-01.jpeg",
-            "VLC media player using Gtk+", "1024x768" );
+            "Gtk+ screenshot", "1024x768" );
 screenshot( "20010409-02.png",
             "tn_20010409-02.jpeg",
-            "VLC media player using Qt", "1024x768" );
+            "Qt screenshot", "1024x768" );
 screenshot( "20010412-00.png",
             "tn_20010412-00.jpeg",
-            "VLC media player on BeOS", "1152x864" );
+            "BeOS screenshot", "1152x864" );
 ?>
 
 <p class="clearme"> Here is a screenshot of the 0.2.60 version showing
@@ -356,16 +356,16 @@ href="http://web.archive.org/web/20010603101909/www.linuxvideo.org/oms/oms-scree
 <?php
 screenshot( "20010214-00.png",
             "tn_20010214-00.jpeg",
-            "VLC media player playing MPEG-1", "1024x768" );
+            "MPEG-1", "1024x768" );
 screenshot( "20010215-00.jpeg",
             "tn_20010215-00.jpeg",
-            "VLC media player using Xinerama", "2560x1024" );
+            "Xinerama", "2560x1024" );
 screenshot( "20000708-00.png",
             "tn_20000708-00.jpeg",
-            "VLC media player - Dithering" ,"686x512" );
+            "Dithering" ,"686x512" );
 screenshot( "20000624-00.png",
             "tn_20000624-00.jpeg",
-            "VLC media player - Scaling", "1600x1200" );
+            "Scaling", "1600x1200" );
 ?>
 <p class="clearme"> </p>
 <?php

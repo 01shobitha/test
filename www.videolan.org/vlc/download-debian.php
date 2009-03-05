2ebd67bee39acca3725deb409d4702684baa13c2
Jean-Baptiste Kempf <jb@videolan.org>
2009-03-05 00:21:45 +0000

    Update debian names... This page needs more details...

--- www.videolan.org/vlc/download-debian.php
+++ www.videolan.org/vlc/download-debian.php
@@ -8,12 +8,12 @@
 <h1> VLC media player for <a href="http://debian.org">Debian GNU/Linux</a></h1>
 <div id="left">
 
-<h2> Debian stable (etch) </h2>
+<h2> Debian stable (lenny) </h2>
 
-<p> VLC is available too in etch, with a pretty recent version. Install 
+<p> VLC is available too in etch, with a quite old version. Install
 vlc, mozilla-plugin-vlc, the usual way you do it. </p>
 
-<h2> Debian testing (lenny) </h2>
+<h2> Debian testing (squeezey) </h2>
 
 <p> VLC is potentially available in testing. But due to the way testing works,
  we can't guarantee which version, if any, is currently available. Install 
@@ -37,9 +37,9 @@ branch. However you should still use our apt-get line if you need libdvdcss: </p
    # apt-get install vlc libdvdcss2
 </pre>
 
-<h2> Debian oldstable (sarge) </h2>
+<h2> Debian oldstable (etch) </h2>
 
-<p> A version (between 0.8.1 and 0.8.2) is packaged in Sarge.
+<p> A version (0.8.6) is packaged in Etch. 
 However, if you want libdvdcss (DVD decryption) support, you will need to add the following lines to your <b>/etc/apt/sources.list</b>:</p>
 
 <pre>

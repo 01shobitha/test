b9b104a51afebd7093e719c501e709c6fe89eefe
Rémi Duraffort <ivoire@videolan.org>
2009-03-08 18:03:35 +0000

    the site map is at the root of the site.

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -135,7 +135,7 @@ function footer($tag) {
 </div> <!-- PAGECONTAINER -->
 <div id="footer">
   <p> <a href="/">VideoLAN</a> &nbsp;-
-  &nbsp;<a href="map.html">Website Map</a>&nbsp;-
+  &nbsp;<a href="/map.html">Website Map</a>&nbsp;-
   &nbsp;<a href="/team/">Contact</a>&nbsp;-&nbsp;
       See the
      <a href="http://www.videolan.org/stats/downloads.html">statistics</a>

d0d3bb218f43a75dcb48a490091a56ba6736b76d
Jean-Baptiste Kempf <jb@videolan.org>
2009-05-12 08:51:18 +0000

    Fix typo

--- www.videolan.org/projects/index.php
+++ www.videolan.org/projects/index.php
@@ -24,7 +24,7 @@ mostly aimed at power end-users:</p>
 a Java application to manage multiple VLC instances through their telnet interface.</li>
 
 <li><a href="/projects/dvblast.html">DVBlast</a>,
-a powerfull DVB streamer, fast and reliable.</li>
+a powerful DVB streamer, fast and reliable.</li>
 
 <li><a href="/vlc/streaming.html">VLS (VideoLAN Server)</a>, our legacy
 streaming server, which is now mostly replaced by VLC.</li>

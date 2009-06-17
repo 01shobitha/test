01e4680cbf3b95702b331d083a6babaabfdf193a
Jean-Baptiste Kempf <jb@videolan.org>
2009-06-17 23:31:55 +0000

    Skins2 contest

---
+++ www.videolan.org/vlc/contest/index.php
@@ -0,0 +1,48 @@
+<?php
+   $title = "VLC skins contest";
+   $lang = "en";
+   $menu = array( "vlc", "download" );
+   $body_onload = "getCount()";
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<h2> Selected skins for the 1.0.0 skins2 contest</h2>
+<table style="text-align: left; width: 100%; margin-left: auto; margin-right: auto;" border="0" cellpadding="2" cellspacing="2">
+
+  <tbody>
+    <tr>
+      <td style="vertical-align: top; text-align: center;"><a href="Carbon.jpg"><img style="border: 0px solid ; width: 240px; height: 150px;" alt="Carbon" src="Carbon.medium.png"><br>
+Carbon</a><a href="Carbon.vlt"><br>
+ Test this skin</a></td>
+      <td style="vertical-align: top; text-align: center;"><a href="Dark%20Pepper%20Main.png"><img style="border: 0px solid ; width: 240px; height: 156px;" alt="Dark Pepper" src="Dark%20Pepper%20Main.medium.png"><br>
+Dark Pepper</a><a href="Dark%20Pepper.vlt"><br>
+Test this skin</a></td>
+      <td style="vertical-align: top; text-align: center;"><a href="Orangeade_1.0.png"><img style="border: 0px solid ; width: 240px; height: 157px;" alt="orange 1" src="Orangeade_1.0.medium.png"><br>
+Orangeade</a><br>
+      <a href="Orangeade_1.0.vlt">Test this skin</a>
+      <br>
+      </td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; text-align: center;"><a href="raptor.png"><img style="border: 0px solid ; width: 240px; height: 170px;" alt="Raptor" src="raptor.medium.png"><br>
+Raptor</a><br>
+      <a href="raptor.vlt">Test this skin</a>
+      </td>
+      <td style="vertical-align: top; text-align: center;"><a href="SIMPLy_1.0.png"><img style="border: 0px solid ; width: 240px; height: 175px;" alt="SIMPly" src="SIMPLy_1.0.medium.png"></a><br>
+      <a href="SIMPLy_1.0.png">SIMPly</a><br>
+<a href="SIMPLy_1.0.vlt">Test this skin</a>
+      </td>
+      <td style="vertical-align: top; text-align: center;"><a href="subX.png"><img style="border: 0px solid ; width: 240px; height: 109px;" alt="subX" src="subX.medium.png"><br>
+subX</a><a href="subX.vlt"><br>
+Test this skin</a><br>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<div style="text-align: center;">
+<br>
+<script language="JavaScript" src="http://www.micropoll.com/akira/MicroPoll?id=176185"></script><noscript><div><a href="http://www.micropoll.com/akira/mpview/610961-176185">Click Here for Poll</a><a href="http://www.questionpro.com" title="online surveys">Online Survey</a><BR> | <a href="http://www.micropoll.com" title="Website Polls">Website Polls</a><BR> | <a href="http://www.contactpro.com" title="email marketing">Email Marketing</a><BR><BR> | <a href="http://www.ideascale.com" title="crowdsourcing">Crowdsourcing</a><BR><a href="http://www.micropoll.com/akira/MicroPoll?mode=html&id=176185">View MicroPoll</A></div></noscript><!-- END MICROPOLL JAVASCRIPT CODE -->
+
+</div>

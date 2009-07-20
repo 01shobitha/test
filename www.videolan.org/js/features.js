bb9e184b7b5022ecabc3dd99ab4fc29b487c7321
Daniel Dreibrodt <altglass@videolan.org>
2009-07-20 14:00:11 +0000

    d'oh features page js fix again

--- www.videolan.org/js/features.js
+++ www.videolan.org/js/features.js
@@ -3,11 +3,15 @@ lastCat = "input";
 function openFeatures(cat) {
   if($("cat-"+lastCat)!=null) $("cat-"+lastCat).className="feature-cat";
   if($("cat-"+cat)!=null) $("cat-"+cat).className="feature-cat-sel";
-  new Effect.BlindUp("feature-div", {duration: 1.0, queue: 'end' });    
+  if($("feature-list-"+lastCat)!=null) {
+    new Effect.BlindUp("feature-list-"+lastCat, {duration: 1.0, queue: 'end' });
+  }  
+  lastCat = cat;
   new Ajax.Updater('feature-div', '/vlc/features.php?fdiv='+cat, {
     onSuccess: function(transport) {
-      new Effect.BlindDown("feature-div", {duration: 1.0, queue: 'end' });
+      if($("feature-list-"+cat)!=null) {
+        new Effect.BlindDown("feature-list-"+cat, {duration: 1.0, queue: 'end' });
+      }
     }
   });  
-  lastCat = cat;
 }

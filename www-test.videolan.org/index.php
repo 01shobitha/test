78dea08722ab41574f5b2687aa263702f7011a81
Daniel Dreibrodt <altglass@videolan.org>
2009-08-25 09:00:10 +0000

    I hate rss

--- www-test.videolan.org/index.php
+++ www-test.videolan.org/index.php
@@ -180,11 +180,11 @@
       $i = 0;
       $max = 15;
       foreach($rss->items as $item) {
-        echo "<p style=\"margin-bottom:5px;\">";
+        echo "<p style=\"margin-top:10px;\">";
         echo "<b>".htmlentities($item['title'])."</b></p>";
         echo "<p style=\"white-space:pre-wrap\">";
-        echo substr(strip_tags(html_entity_decode($item['content']['encoded'])),0,200);
-        echo "<a href=\"".$item['link']."\">[...]</a><br/>";
+        echo substr(strip_tags(html_entity_decode($item['content']['encoded'])),0,140);
+        echo " <a href=\"".$item['link']."\">[...]</a><br/>";
         echo "</p>";
         $i++;
         if($i>=$max) break;

30c4b92922f26ade5707dcc2feba75175d82f1cd
Jean-Baptiste Kempf <jb@videolan.org>
2009-08-31 21:57:50 +0000

    Use the new paypal account

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -140,7 +140,7 @@
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
             <p>
             <input name="cmd" value="_xclick" type="hidden"/>
-            <input name="business" value="treasurer@videolan.org" type="hidden"/>
+            <input name="business" value="sponsor@videolan.org" type="hidden"/>
             <input name="item_name" value="Development and communication of VideoLAN" type="hidden"/>
             <input name="no_note" value="0" type="hidden"/>
             <input name="currency_code" value="EUR" type="hidden"/>
@@ -154,7 +154,7 @@
           <td style="background:none;border:none;text-align:center;padding:0px">
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><p>
             <input name="cmd" value="_xclick" type="hidden"/>
-            <input name="business" value="treasurer@videolan.org" type="hidden"/>
+            <input name="business" value="sponsor@videolan.org" type="hidden"/>
             <input name="item_name" value="Development and communication of VideoLAN" type="hidden"/>
             <input name="no_note" value="0" type="hidden"/>
             <input name="currency_code" value="USD" type="hidden"/>

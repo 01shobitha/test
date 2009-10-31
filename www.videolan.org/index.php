d4b699407a0ddea60c7afe8cc820700e798dccd4
Christophe Mutricy <xtophe@videolan.org>
2009-10-31 22:07:19 +0000

    1.0.3 release

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -51,7 +51,7 @@
 
   <div id="right">
     <?php panel_start( "blue" ); ?>
-    <h1>VLC media player 1.0.2</h1>
+    <h1>VLC media player 1.0.3</h1>
     <table class="dl-table">
         <tr>
           <td style="width:64px;"><img src="images/dl-vlc.png" alt="Download VLC" /></td>

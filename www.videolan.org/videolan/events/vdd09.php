92369266754d1f09bf79f2f8eebe5c2e1510523b
Jean-Baptiste Kempf <jb@videolan.org>
2009-12-01 22:42:31 +0000

    Cosmetics

--- www.videolan.org/videolan/events/vdd09.php
+++ www.videolan.org/videolan/events/vdd09.php
@@ -13,7 +13,7 @@
 
 <h2> Welcome </h2>
 <p>The <a href="/videolan/">VideoLAN non-profit organisation</a> is happy to
-invite you to the multimedia open-source event of this end of year:<br />
+invite you to the multimedia open-source event of this end of year: <br />
 <strong>VideoLAN Dev Days '09</strong>.</p>
 <p>For the second time, people from the VideoLAN community will meet in <strong>Paris</strong> to gather, work, discuss and build a stronger community, on the <strong>18th, 19th and 20th of december 2009</strong>.</p>
 <p>Developers, designers and anonymous people around <a href="/vlc/">VLC</a>, <a href="/projects/dvblast.html">DVBlast</a>, VLMa, skin-designer or other multimedia projects will be there.</p>

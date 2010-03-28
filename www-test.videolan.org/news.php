b224f77f370d8e73fdb9834308fa315d4cd7b600
Jean-Baptiste Kempf <jb@videolan.org>
2010-03-28 16:28:22 +0000

    A bit less outdated news.php

--- www-test.videolan.org/news.php
+++ www-test.videolan.org/news.php
@@ -10,7 +10,8 @@
 
 <h1> News archive </h1>
 
-<p><span class="summary"><a href="http://www.videolan.org/">VideoLAN</a> is a Free Software and Open Source multi-platform video streaming solution. It also features a cross-plaform multimedia player, <a href="http://www.videolan.org/vlc/">VLC</a>, which can be used to read the stream from the network or display video read locally on the computer</span></p>
+<p><span class="summary"><a href="http://www.videolan.org/">VideoLAN</a> is a Free Software and Open Source project and organisation.<br />
+One of the software project is the famous cross-platform multimedia player, <a href="http://www.videolan.org/vlc/">VLC</a>.<span></p>
 
 <?php
    include($_SERVER["DOCUMENT_ROOT"]."/include/news.php");

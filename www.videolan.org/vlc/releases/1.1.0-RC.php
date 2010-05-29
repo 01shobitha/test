fc48854103fc05f3c4bb5914428866bfe6590e6b
Rémi Denis-Courmont <rem@videolan.org>
2010-05-29 18:06:56 +0000

    Remove webm for features

--- www.videolan.org/vlc/releases/1.1.0-RC.php
+++ www.videolan.org/vlc/releases/1.1.0-RC.php
@@ -113,7 +113,6 @@ function DoDL2( $os, $with_js=1 )
               <li><b>GPU</b> and <b>DSP</b> decoding, depending on the platform</li>
               <li>Faster and more stable</li>
               <li>New codecs and <b>HD codecs</b> enhancements</li>
-              <li><b>Webm</b> and <b>VP8</b> support</li>
               <li><b>MKV</b>, mp4 and avi improvements</li>
               <li><b>Extensions</b></li>
               <li>Important code cleanup and rewrite</li>

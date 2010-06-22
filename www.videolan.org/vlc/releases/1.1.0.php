e19873998e005bcc92b3ed3272a0c2a81e5aec5b
Jean-Baptiste Kempf <jb@videolan.org>
2010-06-22 09:57:29 +0000

    Update Reddit link

--- www.videolan.org/vlc/releases/1.1.0.php
+++ www.videolan.org/vlc/releases/1.1.0.php
@@ -153,7 +153,7 @@ function DoDL2( $os, $with_js=1 )
 </div>
 
 <p><a href="http://twitter.com/#search?q=vlc">Twitter</a> <a href="http://digg.com/software/VLC_1_1_0_release">Digg it!</a>
-<a href="http://news.ycombinator.com/item?id=1450418">Hacker News</a> <a href="http://reddit.com">Reddit (not yet)</a>
+<a href="http://news.ycombinator.com/item?id=1450418">Hacker News</a> <a href="http://www.reddit.com/r/programming/comments/chkf3/vlc_110_is_released/">Reddit</a>
 <a href="http://www.facebook.com/vlc.media.player">Facebook page</a> <a href="http://twitter.com/videolan">Twitter page</a>
 </p>
 <h1>What's new?</h1>

f23953cca88e9490d01b1ded72e4c7c417bff18d
Jean-Baptiste Kempf <jb@videolan.org>
2010-06-22 00:39:00 +0000

    Mac OS is ready

--- www.videolan.org/include/os-specific.php
+++ www.videolan.org/include/os-specific.php
@@ -44,7 +44,7 @@ function Screenshot( $os )
 function DoDL( $os, $with_js=1 )
 {
     $dl = array();
-    $version = '1.0.5';
+    $version = '1.1.0';
     $dl["Win32"] = array("http://sourceforge.net/projects/vlc/files/1.1.0/win32/vlc-1.1.0-win32.exe/download", "Windows, 17&nbsp;MB" );
     $dl["OSX-PPC"] = array("/mirror-geo.php?file=vlc/$version/macosx/vlc-$version-powerpc.dmg", "Mac OS X 10.5 (PowerPC), 19&nbsp;MB" );
     $dl["OSX-Intel"] = array( "/mirror-geo.php?file=vlc/$version/macosx/vlc-$version-intel.dmg","Mac OS X 10.5 or later (Intel 32bit), 19&nbsp;MB" );

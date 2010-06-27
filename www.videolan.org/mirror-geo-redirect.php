df9c00b3595fbbb9c663030734d3644e98779569
Jean-Baptiste Kempf <jb@videolan.org>
2010-06-27 21:33:00 +0000

    Fix for sf redirect

--- www.videolan.org/mirror-geo-redirect.php
+++ www.videolan.org/mirror-geo-redirect.php
@@ -8,7 +8,7 @@ if( strchr( $file, '<' ) or strchr( $file, '%' ) or strchr( $file, '>' ) ) die()
 
 if( $file = strstr( $file, 'vlc/') )
 {
-  $file = substr( $file, 3, strlen( $file ) - 4 )
+  $file = substr( $file, 4 );
   header('Location: http://sourceforge.net/projects/vlc/files/'.$file.'/download');
 }
 else

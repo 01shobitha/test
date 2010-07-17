72c6cbac4167747e47c7e5dc223456e1bcd4a657
Rémi Denis-Courmont <rem@videolan.org>
2010-07-17 12:07:21 +0000

    Typos
    Patch from Nidal Rashid <nidalrashid@gmail.com>

--- www.videolan.org/contribute.php
+++ www.videolan.org/contribute.php
@@ -13,7 +13,7 @@
 
 <img src="http://images.videolan.org/images/contribute-time.png" alt="" style="float: left; margin-right: 5px" />
 <div>
-<p>If you have time, we welcome your help in various area</p>
+<p>If you have time, we welcome your help in various areas</p>
 <ul>
   <li><strong>Programmers</strong>, see the <a href="/developers/">developer documentation</a>,
       subscribe to the <a href="/support/lists.html">mailing-lists</a>, have
@@ -42,7 +42,7 @@ warranty that your wish will be taken into account. </p>
 <h3>What is the legal structure of VideoLAN?</h3>
 
 <p> VideoLAN is a non-profit organisation, under French law.
-VideoLAN has its own bank account and is responsible the VideoLAN servers.
+VideoLAN has its own bank account and is responsible for running and maintaining the VideoLAN servers.
 . </p>
 
 <h3>What are the financial needs of VideoLAN?</h3>
@@ -60,9 +60,9 @@ VideoLAN has its own bank account and is responsible the VideoLAN servers.
 href="http://www.paypal.com/">PayPal</a> account. There is no minimal amount
 for your donation. </p>
 
-<p> If you click on one of the buttons below, you will be taken the secure
+<p> If you click on one of the buttons below, you will be taken to the secure
 PayPal Web site. Enter the amount of the donation you want to make.<br />
-You don't need to have a paypal account to do so.</p>
+You don't need to have a paypal account in order to make a donation.</p>
 
    <div>
    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">

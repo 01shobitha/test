9601764f58d93929b5618ce1ba656b0d0655fff9
Jean-Baptiste Kempf <jb@videolan.org>
2010-10-03 16:13:00 +0000

    VLC Release page beautify again

--- www.videolan.org/vlc/releases/index.php
+++ www.videolan.org/vlc/releases/index.php
@@ -1,13 +1,14 @@
 <?php
-   $title = " - VLC 1.1.0 Release Candidate";
+   $title = " - VLC Releases";
    $lang = "en";
-   $menu = array( "project", "news" );
+   $menu = array( "vlc", "download" );
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
 ?>
 
 <h1>VLC Releases</h1>
 
+<h2>VLC 1.1.x branch</h2>
 <a href="/vlc/releases/1.1.0-RC.html">VLC 1.1.0 - Release Candidate</a><br />
 <a href="/vlc/releases/1.1.0.html">VLC 1.1.0</a><br />
 <a href="/vlc/releases/1.1.1.html">VLC 1.1.1</a><br />

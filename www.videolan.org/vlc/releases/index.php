6c8bfcea452a53b11100b6702218a9dfcacd02b6
Jean-Baptiste Kempf <jb@videolan.org>
2010-10-03 16:11:06 +0000

    Make the vlc/releases/ page more pretty

--- www.videolan.org/vlc/releases/index.php
+++ www.videolan.org/vlc/releases/index.php
@@ -8,12 +8,12 @@
 
 <h1>VLC Releases</h1>
 
-<a href="/vlc/releases/1.1.0-RC.html">VLC 1.1.0 - Release Candidate</a>
-<a href="/vlc/releases/1.1.0.html">VLC 1.1.0</a>
-<a href="/vlc/releases/1.1.1.html">VLC 1.1.1</a>
-<a href="/vlc/releases/1.1.2.html">VLC 1.1.2</a>
-<a href="/vlc/releases/1.1.3.html">VLC 1.1.3</a>
-<a href="/vlc/releases/1.1.4.html">VLC 1.1.4</a>
+<a href="/vlc/releases/1.1.0-RC.html">VLC 1.1.0 - Release Candidate</a><br />
+<a href="/vlc/releases/1.1.0.html">VLC 1.1.0</a><br />
+<a href="/vlc/releases/1.1.1.html">VLC 1.1.1</a><br />
+<a href="/vlc/releases/1.1.2.html">VLC 1.1.2</a><br />
+<a href="/vlc/releases/1.1.3.html">VLC 1.1.3</a><br />
+<a href="/vlc/releases/1.1.4.html">VLC 1.1.4</a><br />
 
 
 <?php footer('$Id: news.php 6098 2010-05-26 23:50:46Z jb $'); ?>

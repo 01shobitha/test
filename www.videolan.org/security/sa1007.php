66f9dd7ff526d4a8c1282b0294a16d16ee83850e
Rémi Denis-Courmont <rem@videolan.org>
2010-12-17 01:07:58 +0000

    Reserve advisory 1007

---
+++ www.videolan.org/security/sa1007.php
@@ -0,0 +1,37 @@
+<?php
+   $title = "VideoLAN Security Advisory 1007";
+   $lang = "en";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<h1>Security Advisory 1007</h1>
+<pre>
+Summary           : Reserved
+Date              : December 2010
+Affected versions : TBD
+ID                : VideoLAN-SA-1007
+CVE reference     : TBD
+</pre>
+<h2>Details</h2>
+<p>This security advisory identifier is reserved.
+Details will be released at a later date if appropriate.
+</p>
+<h2>References</h2>
+<dl>
+<dt>The VideoLAN project</dt>
+<dd><a href="http://www.videolan.org/">http://www.videolan.org/</a>
+</dd>
+</dl>
+<h2>History</h2>
+<dl>
+</dl>
+
+<address>R&eacute;mi Denis-Courmont,<br />
+on behalf of the VideoLAN project</address>
+
+</div>
+
+<?php footer('$Id$'); ?>

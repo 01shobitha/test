339487a27e1db106bd26e383c5460573c46bce0b
Jean-Baptiste Kempf <jb@videolan.org>
2010-12-28 11:21:38 +0000

    More extensions work

--- www-test.videolan.org/vlc/extensions/extensions.php
+++ www-test.videolan.org/vlc/extensions/extensions.php
@@ -7,11 +7,11 @@ require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 
 require('functions.php.inc');
 
-getVariable('q','cat','sort','maxRows','page','when');
+/* Get params */
+getVariable('q','cat','sort','maxRows','page'/*,'when'*/);
 
 if(isset($getMaxRows) && is_numeric($getMaxRows) && ($getMaxRows>0)) $maxRows = $getMaxRows; else $maxRows = 20;
-if(isset($getPage) && is_numeric($getPage) && ($getPage>0)) $page = $getPage; else $page = 0;
-
+if(isset($getPage) && is_numeric($getPage) && ($getPage>0)) $page = $getPage; else $page = 1;
 if(in_array($getSort, array('add-a','rev-d','rev-a','rat-d','rat-a','dwn-d','dwn-a'))) $sort = $getSort; else $sort = 'add-d';
 
 /*if(in_array($getWhen, array('tody','week','month','year'))) $when = $getWhen; else $when = 'all';
@@ -34,24 +34,55 @@ switch($when) {
     break;
 }*/
 
+echo "<h1>VLC addons</h1>";
 require('ext_headers.php.inc');
 
+/* Narrowing query with search and category */
 if(isset($getQ)) {
-    $query_index = ' WHERE name LIKE '.GetSQLValueString('%'.$getQ.'%', "text");
+    $query_suffix = ' WHERE skins.name LIKE '.GetSQLValueString('%'.$getQ.'%', "text");
     if(isset($getCat))
-        $query_index = " AND categories.slug =%s".GetSQLValueString($getCat, "text");
+        $query_suffix = " AND category = $mainCategoryId";
     $h1title = "Search Result: <em>".htmlentities(urldecode($getQ), ENT_QUOTES).'</em>';
 }
 elseif(isset($getCat)) {
-    $select_category = " WHERE categories.slug = %s".GetSQLValueString($getCat, "text");
-    $h1title = $mainCategoryName;
+    $query_suffix = " WHERE category = $mainCategoryId";
+    $h1title = $mainCategoryName; 
 }
 
 if( !isset($h1title) )
     $h1title = "Extensions";
 
+/* Actual start of the content */
 echo "<h1>VLC $h1title</h1>";
+echo "<div class='sort'>";
+echo 'Sort by: <a href="">Name</a> <a href="">Creation</a> <a href="">Downloads</a> <a href="">Rating</a> <a href="">Reviews</a>';
+echo "</div>";
 
+$query_ext = 'SELECT AVG(rating) as avg, COUNT(rating) as count, COUNT(extension_id) as review_nb, skins.id as id, skins.name, author, downloads, date_added, date_modified, image, skins.url, min_version, size FROM skins INNER JOIN "skins-rating" ON skins.id="skins-rating".skin_id LEFT OUTER JOIN reviews ON extension_id=skins.id';
+
+$query_ext .= $query_suffix;
+$query_ext .= ' GROUP BY skins.id, skins.name, skins.author, skins.downloads, skins.date_added, skins.date_modified, skins.image, skins.url, skins.min_version, skins.size';
+
+
+$first_row   = ($page - 1) * $maxRows + 1;
+$query_limit = " OFFSET $first_row LIMIT $maxRows";
+$query_ext  .= $query_limit;
+
+echo $query_ext."<br />";
+$extensions  = pg_query($connect,$query_ext);
+
+$count=0;
+while ( $row_extensions   = pg_fetch_array($extensions) )
+{
+        echo $row_extensions['name']."<br/>\n";
+        $count++;
+}
+pg_close( $connect );
+
+echo '<div class="pages">';
+if( $page < 1 ) echo '<a href="">Previous Page</a>';
+if( $count = $maxRows ) echo '<a href="">Next Page</a>';
+echo '</div>';
 
 ?>
 

9b0069d88ef7f312a09c85c4570eb84878bea653
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-15 01:04:37 +0000

    Contact page

---
+++ www-test.videolan.org/contact.php
@@ -0,0 +1,70 @@
+<?php
+   $title = "VideoLAN - Free Software and Open Source video streaming solution for every OS!";
+   $lang = "en";
+   $body_color = "blue";
+   $additional_css = array("/panels.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+
+?>
+
+<div>
+    <h1 class="bigtitle">Contact</h1>
+
+    <div class="clearme"> </div>
+
+    <p>Before contacting us, please be sure that you <b>actually</b> need to contact us!</p>
+
+    <table class="doublecolumn">
+        <tr>
+            <td>
+                <h2>VLC Support</h2>
+                <p>For <a href="/vlc/">VLC media player</a> user-related questions,
+                   please see our <a href="/support/">Support section</a>.<br />
+                   See also our <a href="http://wiki.videolan.org/">Wiki</a> and 
+                   our <a href="http://forum.videolan.org/">Forum</a>.</p>
+            </td>
+            <td>
+                <h2>VLC Development</h2>
+                <p>For <a href="/vlc/">VLC media player</a> developer questions,
+                   please see our <a href="http://wiki.videolan.org/Developers_Corner">Developer section</a>.</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h2>Press</h2>
+                    <p>If you are a journalist or a blogger and need answers to questions,
+                    please check the <a href="/press/">press center</a> and use <b>e-mail</b></p>
+                    <p>In case you write an article about VideoLAN, we would be very glad to
+                    have a copy of it to add to our press book. You can send us the article
+                    at the <b>postal address</b>.</p>
+            </td>
+            <td>
+                <h2>Legal</h2>
+                    <p>If you have a question regarding the use of a product, please check
+                    the <a href="/legal.html">legal FAQ</a>.</p>
+                    <p>Any legal document must be sent to the <b>postal address</b>.
+            </td>
+        </tr>
+    </table>
+
+<h2>E-mail</h2>
+<p>To contact us by e-mail, please use <a href="mailto:videolan@REMOVETHIS@videolan.org">videolan - at - videolan.org</a>
+and write in French or English.</p>
+<p>Any question about VLC usage will not be answered.</p>
+<h2>Postal Address</h2>
+<pre style="margin-left: 20px;">
+VideoLAN
+18, rue Charcot
+75013 Paris
+France
+</pre>
+<h2>Phone</h2>
+<p>The phone will very rarely answer:</p>
+<pre>+33 951 11 20 20</pre>
+<p>You can send faxes to us; except for legal issues, where faxes are not accepted :</p>
+<pre>+33 956 11 20 20</pre>
+
+
+</div>
+
+<?php footer(); ?>

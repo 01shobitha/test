a57088b70c3db8312a0519182a5fa0eba7db320c
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-17 22:33:34 +0000

    More about publicity of archives

--- www-test.videolan.org/support/lists.php
+++ www-test.videolan.org/support/lists.php
@@ -15,7 +15,7 @@
 <li>announces for VideoLAN releases and events.</li>
 </ul>
 
-<p>They are <b>English-speaking</b> mailing-lists.</p>
+<p>They are <b>English-speaking</b> <strong>public</strong> mailing-lists.</p>
 
 <h2>Important mailing lists</h2>
 <p>The most important mailing-lists are:</p>
@@ -55,9 +55,8 @@ hours or days, but is a necessary evil to avoid spam.
 <p>All the mailing-lists are in <strong>PUBLICLY archived</strong>
 which mean that your messages <strong>WILL be PUBLIC</strong>
 and mirrored in a way that <strong>we CANNOT remove</strong> the posts.</p>
-<p>If you want to stay private, use <a href="http://forum.videolan.org">our forum</a>.
-</p>
-
+<p>If you want to stay private, use <a href="http://forum.videolan.org">our forum</a> for user questions,
+or <a href="/contact.html">contact us</a> for any other topic.</p>
 
 </div>
 

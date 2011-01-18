c306c953a1d882daaf2d5f521704c79647916310
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-18 07:30:27 +0000

    And, with the correct file...

---
+++ www-test.videolan.org/legal.php
@@ -0,0 +1,156 @@
+<?php
+   $title = 'Legal';
+   $lang = 'en';
+   $body_color = "green";
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<div>
+<h1>Legal concerns</h1>
+
+<h2>May I use the VideoLAN logo?</h2>
+<p>You may freely use the VideoLAN logo using the following copyright:
+</p>
+<blockquote><p>
+Copyright &copy; 1996-2009 VideoLAN.
+This logo or a modified version may be used or modified by anyone to refer to the VideoLAN project
+or any product developed by the VideoLAN team, but does not indicate endorsement by the project.
+</p></blockquote>
+
+<h2>What are the usage restrictions for VideoLAN software?</h2>
+<p>Short answer: there are none.</p>
+
+<p>You can use the software in the way you want (within the boundary of law),
+for personal, educational, research, military, governmental and/or professional
+purpose.
+</p>
+
+<h2>Can I obtain VideoLAN software under different licensing terms?</h2>
+<p>You would need to negotiate a new copyright license
+with every single copyright holder of the affected piece of software.
+You may also need to relicense some underlying libraries.
+</p>
+<p>In other words, you practically cannot obtain our software
+under other licensing terms than normal open-source ones.
+</p>
+
+<h2>What about other licenses?</h2>
+<p>The binary packages from the VideoLAN website and mirrors
+are wholly licensed under their specified license
+(normally the GNU General Public License version 2).
+You do not need to obtain any separate copyright license.
+</p>
+
+<h3>What about patent licenses?</h3>
+<p>Regarding <cite>Patentable Inventions</cite>,
+the second paragraph
+of article 52
+of the European Patent Convention (EPC) states:
+</p>
+<blockquote><p>
+The following (..) shall not be regarded as [patentable] inventions:
+scientific theories and mathematical methods; (...)
+programs for computers (...).
+</p></blockquote>
+<p>The VideoLAN project only provides software, i.e. &quot;programs for computers&quot;.
+Those are outside the scope of patentability.
+Hence there is not need to obtain any patent licenses
+for VideoLAN software <strong>within most of Europe</strong>
+(European Union, Croatia, Liechtenstein, Macedonia, Monaco, Norway,
+ San Marino, Switzerland and Turkey).
+In other venues, please check any applicable law.
+</p>
+<p>Please note however that the combination of software with hardware
+may yield something patentable.
+As such, if you do make a hardware product with VideoLAN software,
+you may conceivably need to obtain patent licenses for the hardware parts.
+</p>
+
+<h2>May I redistribute a piece of VideoLAN software?</h2>
+<p>
+Yes, you may distribute an original or a modified version of a piece of VideoLAN software as long as you comply with its license terms. It is the GNU General Public License Version 2 (referred herein as GPL), and it is in the file named COPYING in our products. 
+</p>
+
+<p><strong>Note</strong>: You do not need to ask the VideoLAN team the permission to do so!
+</p>
+
+<h3>How do I do comply with those terms?</h3>
+<p>The easiest way to conform to the GPL is to accompany the product you want to distribute with its sources.
+</p>
+
+<p>For example, if you plan to distribute the latest binary version of VLC on a DVD-ROM,
+you should also include the <a href="http://www.videolan.org/vlc/download-sources.html">VLC source code</a>
+and the source code for the underlying included libraries on the same media.
+</p>
+
+<p>If you plan to distribute the latest binary version of VLC on a website,
+you should provide a link to the <a href="http://www.videolan.org/vlc/download-sources.html">VLC source code</a>.
+</p>
+
+<p>There are other ways to comply with the GPL,
+but this should be the simplest one as it does not rely on a contract.
+</p>
+
+<h2>Is libdvdcss legal?</h2>
+<p> The use and distribution of the libdvdcss library is controversial
+in some signatories countries
+of the <a href="http://www.wipo.int/">WIPO</a> Copyright Treaty,
+and the corresponding European Union Copyright Directive (EUCD) within the EU.
+For instance, the United States of America,
+it is implemented through the DMCA (Digital Millennium Copyright Act).
+</p>
+<p> Different countries implement subtly different restrictions
+on the use of anti-piracy counter-measures.
+If you are unsure about the legality of using and distributing this library
+in your country, please consult your lawyer.
+</p>
+
+<p><strong>Beware</strong>:
+VLC media player installers for Microsoft Windows and MacOS X
+are distributed with the libdvdcss library included.
+The VLC media player source code does <strong>not</strong> include libdvdcss.
+Also Linux VLC packages normally do not include libdvdcss.
+</p>
+
+<h2>Third parties are selling your software.
+<a name="sale">Is this legal?</a></h2>
+<p>Yes, absolutely.
+</p>
+<p>Our licensing terms explicitly allow selling or sublicensing our software.
+We allow that very much on purpose.
+For instance, we like computer magazines to include our software on their CDs,
+and we also like Linux distributions, PC manufacturers and storage device
+vendors to pre-install our software and make money out of it.
+For us, that is basically zero-cost advertisement.
+See also <a href="http://www.gnu.org/philosophy/selling.html">the rationale
+from the Free Software Foundation</a> (those who wrote our licensing terms).
+</p>
+<p>We reckon that certain form of sales, such as online commercial downloads,
+provide no value to us neither our customers.
+Many people consider those sales to be deceptive, if not outright fraudulent.
+As our software can be downloaded for free from our website and mirrors,
+<cite>there should be no need to pay</cite>.
+Unfortunately, it is impossible to differentiate good and bad sales,
+so we allow all of them.
+Besides, we are open-source enthusiasts, not lawyers.
+</p>
+
+<h2>I bought your software.
+<a name="refund">How do I get a refund?</a></h2>
+<p>The VideoLAN project and its <kbd>videolan.org</kbd> website
+are in no way involved with retail or gross sale of any product.
+As a consequence, the VideoLAN project is unable to provide any refund.
+To put it bluntly, we did not get your money, so we cannot give it back.
+</p>
+<p>If you want a refund for software from the VideoLAN project,
+you must solicit it from the merchant that sold you the product.
+Depending on the applicable consumer protection laws and contractual terms
+you could have agreed to,
+you might consider turning to the financial intermediaries,
+and/or the competent administration for commercial fraud
+(e.g. the DGCCRF in France).
+</p>
+
+</div>
+
+<?php footer();?>

44adea2146fc6bbd666648df5aa1e05e695ee50f
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-18 07:15:15 +0000

    Rewrite the support page

--- www-test.videolan.org/support/index.php
+++ www-test.videolan.org/support/index.php
@@ -1,27 +1,35 @@
 <?php
    $title = "VideoLAN - User support and help";
    $lang = "en";
+   $body_color = "red";
    $menu = array( "support", "overview" );
    $additional_css = array("/panels.css");
-   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php"); 
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
 <div id="left">
 
-<h1> User support - Help</h1>
+<h1 class="bigtitle">Support and Help center</h1>
+<div class="clearme"></div>
 
 <p>VideoLAN is free and open source software; and is not backed by any company.
 Developers are mostly <strong>volunteers</strong>.<br />
 Therefore, user support is provided by <strong>volunteers</strong>.</p>
+        <strong>Please remember</strong> that everyone in here is
+        doing this in his free time. Don't come running in and demanding
+        an answer. Sometimes the persons there have no knowledge of that
+        particular subject. Sometimes simply no one is present. If you want
+        to be sure to get a REAL answer you can often better try the <a
+        href="lists.html">mailing-lists</a>.<br />
 
-<p><strong>I need:</strong><br />
+<p><strong>I want:</strong><br />
 <a id="normalButton" href="#documentation">Documentation</a>
-<a id="normalButton" href="#help">Help</a>
-<a id="normalButton" href="#bugs">Bugs</a>
+<a id="normalButton" href="#help">Help and support</a>
+<a id="normalButton" href="#bugs">To report a bug</a>
 </p>
-<div class="clearme"></div>
+<div class="clearme" style="margin-bottom: 30px;"></div>
 
-<h2>I need Documentation</h2>
+<h1>Documentation</h1>
 <a id="documentation"></a>
 
 <h3>FAQ</h3>
@@ -33,107 +41,99 @@ Therefore, user support is provided by <strong>volunteers</strong>.</p>
  <a href="http://wiki.videolan.org/Documentation:Documentation"> Documentation</a>,
 about <a href="http://wiki.videolan.org/Documentation:Play_HowTo">VLC playback</a> and
 <a href="http://wiki.videolan.org/Documentation:Streaming_HowTo">VLC Streaming</a>.</p>
-<hr style="margin: 20px; width:30%;" />
 
-<h2>I need help</h2>
+<div style="margin: 40px;"></div>
+
+<h1>Help and Support</h1>
 <a id="help"></a>
-<h3>I have a Usage Problem</h3>
-<p>If you have problems using our software, you should:</p>
-<ul class="bullets">
-<li>use the <a href="http://wiki.videolan.org/Common_Problems">troubleshouting guide</a>,</li>
-<li> <a href="http://forum.videolan.org/search.php">search</a> the official
-<a href="http://forum.videolan.org/">forum</a> for an answer.</li>
-</ul>
-
-<h3>Ask a question</h3>
-<p>If this is not enough, ask a question.<br />
-For that, you <strong>need</strong> to prepare a <strong>correct report</strong>
- using the <a href="http://wiki.videolan.org/VLC_report">VLC report page</a>.
-<p>Then ask your question, on:</p>
-<ul class="bullets">
-<li>our <a href="http://forum.videolan.org">Forum</a>, in the correct section;</li>
-<li>our <a href="lists.html">users mailing-lists</a>, especially <a href="mailto:vlc@REMOVETHIS@videolan.org">vlc - at - videolan.org</a>;</li>
-<li>our IRC channel on the <a href="http://www.freenode.net">freenode</a> Network (<em>irc.videolan.org, #videolan</em>), for live answers. Use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.</li>
-</ul>
-
-<hr style="margin: 20px; width:30%;" />
-<h2>Reporting bugs</h2>
-<a id="bugs"></a>
-<p> Read and conform yourself to our <a href="http://wiki.videolan.org/Report_bugs">Reporting Bugs</a>
- policy and look on our <a href="http://trac.videolan.org">Bugtracking</a>.</p>
-
-<h2>Professional questions and other questions</h2>
-
-<h3>Consulting services</h3>
-
-<p>If you are a professionnal, and need services around our products, 
-some companies provide professionnal support on VideoLAN. They are
-listed on the <a href="/vlc/partners.html">consultants</a> page.</p>
-
-<h3>Contacting the team</h3>
-See the <a href="/contact.html">contact page</a>.
+<div class="onecolumn" style="margin-bottom: 30px;">
+    <h3>I have a Usage Problem</h3>
+    <p>If you have problems using our software, you should:</p>
+    <ul class="bullets">
+        <li>use the <a href="http://wiki.videolan.org/Common_Problems">troubleshouting guide</a>,</li>
+        <li> <a href="http://forum.videolan.org/search.php">search</a> the official
+        <a href="http://forum.videolan.org/">forum</a> for an answer.</li>
+    </ul>
 </div>
 
-<div id="right">
-<?php panel_start("orange"); ?>
-<h1>The forums</h1>
-
-<p>Our <a href="http://forum.videolan.org">Web forums</a> can help you
-find answers to your questions or problems. Please check that your
-question has not yet been answered before asking.</p>
-
-<?php panel_end(); ?>
-
-<?php panel_start( "blue" ); ?>
- <h1> VideoLAN Wiki </h1>
-	 
- <p>The VideoLAN Wiki is at <a   
- href="http://wiki.videolan.org">http://wiki.videolan.org/</a></p>
-
- <p>The Wiki is a website that anyone can change. Its goal is to document
- all that is not in the official documentation: tips and tricks for each
- O.S., graphical interfaces, etc...</p>
-<?php panel_end(); ?>
-
-<?php panel_start("orange"); ?>
-<h1>The mailing-lists</h1>
-
-<p>The user mailing-lists are dedicated to user support and
-announces for VideoLAN releases and events. Please see the <a
-href="lists.html">mailing-lists</a> page to subscribe, unsubscribe,
-consult the archives and read about the rules and restrictions.</p>
-
-<?php panel_end(); ?>
-
-<?php panel_start( "blue" ); ?>
-<h1>IRC</h1>
-
-<p> If you want more real-time stuff, you can meet many VideoLAN
-developers and users on IRC.<br />
-<strong>Please remember</strong> that everyone in here is
-doing this in his free time. Don't come running in and demanding
-an answer. Sometimes the persons there have no knowledge of that
-particular subject. Sometimes simply no one is present. If you want
-to be sure to get a REAL answer you can often better try the <a
-href="lists.html">mailing-lists</a>.<br />
-Also consider the timezone. Most people in this chatroom are from Europe.</p>
-
-<ul class="bullets">
-  <li>IRC network: <code>irc.videolan.org</code></li>
-  <li>Channel: <code>#videolan</code></li>
-</ul>
-
-<p> You can connect with any IRC client. If you cannot connect from your
-place, try the web-based <a href="http://www.videolan.org/webirc/">IRCWeb</a> interface.
-</p>
-<?php panel_end(); ?>
-
-<?php panel_start("orange"); ?>
-<h1>Consulting services</h1>
-
-<p>Some companies provide professionnal support on VideoLAN. They are
-listed on the <a href="/vlc/partners.html">consultants</a> page.</p>
-<?php panel_end(); ?>
+<div>
+    <h3>Ask a question</h3>
+    <p>To ask a question, you <strong>need</strong> to prepare a <strong>correct report</strong>
+     using the <a href="http://wiki.videolan.org/VLC_report">VLC report page</a>.
+    <p>Then ask your question, on:</p>
+    <ul class="bullets">
+        <li>our <a href="http://forum.videolan.org">Forum</a>, in the correct section;</li>
+        <li>our <a href="lists.html">users mailing-lists</a>, especially <a href="mailto:vlc@REMOVETHIS@videolan.org">vlc - at - videolan.org</a>;</li>
+        <li>our IRC channel on the <a href="http://www.freenode.net">freenode</a> Network (<em>irc.videolan.org, #videolan</em>), for live answers. Use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.</li>
+    </ul>
 </div>
 
+<div class="tricolumn" style="padding-top: 10px;">
+    <div class="onecolumn">
+        <h1>Reporting bugs</h1>
+        <a id="bugs"></a>
+        <p> Read and conform yourself to our <a href="http://wiki.videolan.org/Report_bugs">Reporting Bugs</a>
+         policy and look on our <a href="http://trac.videolan.org">Bugtracking</a>.</p>
+    </div>
+
+    <div class="onecolumn">
+        <h1>Consulting services</h1>
+
+        <p>If you are a professionnal, and need services around our products,
+        some companies provide professionnal support on VideoLAN. They are
+        listed on the <a href="/vlc/partners.html">consultants</a> page.</p>
+    </div>
+
+    <div class="lastcolumn">
+        <h1>Contacting the team</h1>
+        For other matters, see the <a href="/contact.html">contact page</a>.
+    </div>
+</div>
+
+<div class="tricolumn" style="padding-top: 15px;">
+<h1>Means of support</h1>
+    <div class="onecolumn">
+        <h2>The forums</h2>
+
+        <p>Our <a href="http://forum.videolan.org">Web forums</a> can help you
+        find answers to your questions or problems.<br />Please use search to check
+        that your question has not yet been answered before asking.</p>
+
+        <h2>The mailing-lists</h2>
+
+        <p>The user mailing-lists are dedicated to user support and
+        announces for VideoLAN releases.<br />
+        Please see the <a href="lists.html">mailing-lists</a> page to subscribe, unsubscribe,
+        consult the archives and read about the rules and restrictions.</p>
+    </div>
+
+    <div class="onecolumn">
+        <h2>VideoLAN Wiki</h2>
+        <p>The VideoLAN Wiki is at <a
+        href="http://wiki.videolan.org">http://wiki.videolan.org/</a></p>
+
+        <p>The Wiki is a website that anyone can change. Its goal is to document
+        all that is useful to the users.</p>
+    </div>
+
+    <div class="lastcolumn">
+        <h2>IRC</h2>
+
+        <p> If you want more real-time stuff, you can meet many VideoLAN
+        developers and users on IRC.<br />
+        Please consider the timezone. Most people in this chatroom are from Europe.</p>
+
+        <ul class="bullets">
+          <li>IRC network: <code>irc.videolan.org</code></li>
+          <li>Channel: <code>#videolan</code></li>
+        </ul>
+
+        <p> You can connect with any IRC client.<br />
+        Else, use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.
+        </p>
+    </div>
+
+</div>
+
+</div>
 <?php footer('$Id$'); ?>

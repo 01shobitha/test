b1e82d95b5f27630949ca1f0408e05b99c534726
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-21 14:29:13 +0000

    Improve support page

--- www-test.videolan.org/support/index.php
+++ www-test.videolan.org/support/index.php
@@ -48,6 +48,7 @@ about <a href="http://wiki.videolan.org/Documentation:Play_HowTo">VLC playback</
         <li>use the <a href="http://wiki.videolan.org/Common_Problems">troubleshouting guide</a>,</li>
         <li> <a href="http://forum.videolan.org/search.php">search</a> the official
         <a href="http://forum.videolan.org/">forum</a> for an answer.</li>
+        <li>read the <a href="http://wiki.videolan.org">wiki</a></li>
     </ul>
 </div>
 
@@ -59,7 +60,7 @@ about <a href="http://wiki.videolan.org/Documentation:Play_HowTo">VLC playback</
     <ul class="bullets">
         <li>our <a href="http://forum.videolan.org">Forum</a>, in the correct section;</li>
         <li>our <a href="lists.html">users mailing-lists</a>, especially <a href="mailto:vlc@REMOVETHIS@videolan.org">vlc - at - videolan.org</a>;</li>
-        <li>our IRC channel on the <a href="http://www.freenode.net">freenode</a> Network (<em>irc.videolan.org, #videolan</em>), for live answers. Use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.</li>
+        <li>our live-chat IRC channel on the <a href="http://www.freenode.net">freenode</a> Network (<em>irc.videolan.org, #videolan</em>), for live answers. Use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.</li>
     </ul>
 </div>
 
@@ -83,51 +84,6 @@ about <a href="http://wiki.videolan.org/Documentation:Play_HowTo">VLC playback</
         <h1>Contacting the team</h1>
         For other matters, see the <a href="/contact.html">contact page</a>.
     </div>
-</div>
-<div class="clearme" style="border-bottom: 1px solid #444; width: 30%; margin: 0 auto; padding-top: 40px;"></div>
-<div class="tricolumn" style="padding-top: 15px;">
-<h1>Means of communications</h1>
-    <div class="onecolumn">
-        <h2>The forums</h2>
-
-        <p>Our <a href="http://forum.videolan.org">Web forums</a> can help you
-        find answers to your questions or problems.<br />Please use search to check
-        that your question has not yet been answered before asking.</p>
-
-        <h2>The mailing-lists</h2>
-
-        <p>The user mailing-lists are dedicated to user support and
-        announces for VideoLAN releases.<br />
-        Please see the <a href="lists.html">mailing-lists</a> page to subscribe, unsubscribe,
-        consult the archives and read about the rules and restrictions.</p>
-    </div>
-
-    <div class="onecolumn">
-        <h2>VideoLAN Wiki</h2>
-        <p>The VideoLAN Wiki is at <a
-        href="http://wiki.videolan.org">http://wiki.videolan.org/</a></p>
-
-        <p>The Wiki is a website that anyone can change. Its goal is to document
-        all that is useful to the users.</p>
-    </div>
-
-    <div class="lastcolumn">
-        <h2>IRC</h2>
-
-        <p> If you want more real-time stuff, you can meet many VideoLAN
-        developers and users on IRC.<br />
-        Please consider the timezone. Most people in this chatroom are from Europe.</p>
-
-        <ul class="bullets">
-          <li>IRC network: <code>irc.videolan.org</code></li>
-          <li>Channel: <code>#videolan</code></li>
-        </ul>
-
-        <p> You can connect with any IRC client.<br />
-        Else, use the <a href="http://webchat.freenode.net/">Freenode Web</a> interface, if necessary.
-        </p>
-    </div>
-
 </div>
 
 </div>

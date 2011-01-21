8bbfabdeadbf732759cc0c3e38428d88b9c6f6f7
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-21 13:39:32 +0000

    Styling center

--- www-test.videolan.org/include/header.php
+++ www-test.videolan.org/include/header.php
@@ -208,7 +208,7 @@ function footer($tag = "") {
 </div>
    <div id='footer'>
       <div style='background-image: url( "/images/divider.png" ); height: 15px; width: 100%;'></div>
-      <div style='padding: 40px 40px 10px 50px;'>
+      <div style='padding: 40px 40px 10px 50px; margin: 0 auto; width: 980px'>
            <a style='float: left; margin-right: 50px;' href='/'>
             <?php image( 'VideoLAN.png', 'Association VideoLAN' ); ?></a>
          <div class='footer_col'>

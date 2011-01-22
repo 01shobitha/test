f02d36285a3613894cb53228c755072e7d632e18
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-22 00:23:34 +0000

    Update libVLC

--- www-test.videolan.org/vlc/libvlc.php
+++ www-test.videolan.org/vlc/libvlc.php
@@ -7,7 +7,13 @@
 
 <div>
     <h1 class="bigtitle">libVLC</h1>
+<div class="projectDescription">
+<p><b>libVLC</b> is the interface to the core of <a href="/vlc/">VLC media player</a> that allows
+developers to create application using the <a href="/vlc/">VLC</a>
+ <a href="/vlc/features.html">features</a>.</p>
+</div>
 
+<h2>Technical Diagram</h2>
 <?php image( "libvlc_stack.png", "libVLC dev stack" ); ?>
 
 </div>

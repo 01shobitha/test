909a3fccbde36a056b78cf96c76c2d59643f2019
Felix Paul Kühne <fkuehne@videolan.org>
2011-01-31 11:40:36 +0000

    revert 6624

--- www-test.videolan.org/news.php
+++ www-test.videolan.org/news.php
@@ -5,7 +5,7 @@
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<div class="clearme" style='padding-top: 35px;'>
+
 <h1 class="bigtitle"> News archive </h1>
 
 <div class='longtext'>
@@ -15,6 +15,5 @@
    shownews("full",0);
 ?>
 
-</div>
 </div>
 <?php footer('$Id$'); ?>

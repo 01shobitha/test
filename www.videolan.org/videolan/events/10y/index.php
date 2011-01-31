8028ad897cc63aa620354a48d0c79b1e874cb54a
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-31 23:48:30 +0000

    Use the grey panel for the side bar of the 10y

--- www.videolan.org/videolan/events/10y/index.php
+++ www.videolan.org/videolan/events/10y/index.php
@@ -6,14 +6,15 @@
 ?>
 
 <h1>10 years of Open Source</h1>
-<h2>Welcome to the Celebration of the 10 years</h2>
-<p>We are here to celebrate the 10 years of the open sourcing of VLC.</p>
+<h2>Welcome to the Celebration of the 10 years pages</h2>
+<p>We are here to celebrate the 10 years of the open sourcing of VideoLAN and VLC.</p>
+<p>10 days of surprises, ideas and stories will pop up here.</p>
 
 <div id="left">
 <h2>Day 1: a history</h2>
 </div>
 <div id="right">
-<?php panel_start( "green" ); ?>
+<?php panel_start( "grey" ); ?>
 <h1>Day 1</h1>
 <?php panel_end(); ?>
 

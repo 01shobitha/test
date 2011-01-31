9c848581f26d0cfea07725f9b728fde08d36808d
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-31 23:39:05 +0000

    10y

---
+++ www.videolan.org/videolan/events/10y/index.php
@@ -0,0 +1,18 @@
+<?php
+   $title = "VideoLAN 10years";
+   $lang = "en";
+   $menu = array( "project", "events" );
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<h1>10 years of Open Source</h1>
+<div id="left">
+<h2> Welcome </h2>
+</div>
+<div id="right">
+<?php panel_start( "green" ); ?>
+<?php panel_end(); ?>
+
+</div>
+
+<?php footer('$Id: index.php 5400 2009-07-19 15:37:21Z jb $'); ?>

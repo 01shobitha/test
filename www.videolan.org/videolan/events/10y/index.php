864fa273dbee76c2289ad8eaf5ce0b0eb115e53f
Jean-Baptiste Kempf <jb@videolan.org>
2011-02-01 11:20:59 +0000

    Niceties on the page

--- www.videolan.org/videolan/events/10y/index.php
+++ www.videolan.org/videolan/events/10y/index.php
@@ -6,34 +6,36 @@
 ?>
 
 <h1>10 years of Open Source</h1>
-<img src="/images/10cones_small.png" alt="10 cones" />
-<h2>Welcome to the Celebration of the 10 years pages</h2>
+<h2>Welcome to the pages for the Celebration of our 10 years of open source</h2>
 <p>We are here to celebrate the 10 years of the open sourcing of VideoLAN and VLC.</p>
 <p><b>10 days</b> of surprises, ideas and stories will pop up here.</p>
+<img src="/images/10cones_small.png" alt="10 cones" />
 
 <div id="left">
 <h1>Day 1: a VideoLAN history</h1>
-<p>Most people know VLC, but they don't know the history of the VideoLAN project.</p>
+<p>Most people know <a href="/vlc"/>VLC</a>, but they don't know the history of the <a href="/videolan/">VideoLAN project</a>.</p>
 <p>Well, they don't even know what VideoLAN is, and that we are not a company...</p>
 <h2>1st February 2011</h2>
-<p>Today is the 10<sup>th</sup> anniversary of the switch to GPL of the VideoLAN project
-application...</p>
-<p>But what was before the GPL?</p>
+<p>Today is the <b>10<sup>th</sup> anniversary</b> of the switch to <a href="http://www.opensource.org/">GPL</a> of the VideoLAN project
+applications...</p>
+<p>But, but, but... What was <em>before</em> the GPL?</p>
+
 <h3>1996: First VideoLAN project</h3>
 <p>The network of the campus of the <a href="http://www.ecp.fr">École Centrale Paris</a>,
 one of the prestigious French "<a href="http://en.wikipedia.org/wiki/Grande_ecole">Grande École</a>"
 university, has always been managed by a student organisation named <a href="http://www.via.ecp.fr">
 VIA Centrale Réseaux</a>.</p>
-<p>In 1996, it had a very slow Token-Ring network, but the students wanted an upgrade. They found investors
+<p>In 1996, it had a very slow <a href="http://en.wikipedia.org/wiki/Token_ring">Token-Ring</a> network, but the students wanted an upgrade. They found investors
 at the condition that they could justify the need for a new network...</p>
-<p>Therefore, they decided to push <b>Video</b> on the network...</p>
-<p>Remember, this is 1996, were your average Pentium couldn't decode a DVD and when Youtube and Google didn't exist...</p>
+<p>Therefore, they decided to push <b>Video</b> on the network...<br />
+<p>Remember, this is <b>1996</b>, were your average Pentium couldn't decode a DVD and when Youtube and Google didn't exist...</p>
 <p>This <b>student project</b> was a success in early 1998, and a new network came.</p>
 
 <h3>1998: Second VideoLAN project</h3>
-<p>It was therefore decided to start the project from scratch, in 1998.</p>
-<p>But in the mind of open source and modularity.</p>
-<p>This is were the <a href="/vlc/">VLC media player</a> that you know of, was born.<br />
+<p>As it was successful, it was decided to go on the project.</p>
+<p>And they restarted the project from scratch, in <b>1998</b>.<br />
+<p>But in the mind of <a href="http://www.opensource.org/">open source</a> and modularity.</p>
+<p>This is when the <a href="/vlc/">VLC media player</a> that you know of, was <i>born</i>.<br />
 If you look at the first commit of the repository, in August 1999, you'll see that the most copyright
 indeed is from <b>1998</b>.</p>
 

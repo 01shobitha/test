7dd3e1d23a1c31b0d236c025fa24bbbe2574171e
Jean-Baptiste Kempf <jb@videolan.org>
2011-02-02 17:12:13 +0000

    Small header fix

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -178,7 +178,7 @@ function draw_menus()
    </div>
 </div>
 <div class="clearme"></div>
-<a href="/videolan/events/10y/"><img alt="10 cones" src="http://images1.videolan.org/images/10years.png" style="border: 2px solid rgb(61, 61, 61);" ></a>
+<a href="/videolan/events/10y/"><img alt="10 cones" src="http://images1.videolan.org/images/10years.png" ></a>
 
 <?php
 }

7f852378aef4268145215c3f0a5562613726701b
Jean-Baptiste Kempf <jb@videolan.org>
2011-03-13 10:17:55 +0000

    Update SUSE webpage

--- www.videolan.org/vlc/download-suse.php
+++ www.videolan.org/vlc/download-suse.php
@@ -7,14 +7,14 @@
 
 <?php function pkgitem($version,$name,$comment) { ?><li><a href="http://download.videolan.org/pub/videolan/<?php echo $version."/".$name; ?>"><code><?php echo $name; ?></code></a> <?php echo $comment; ?> </li><?php } ?>
 
-<h1 class="bigtitle">VLC media player for <a href="http://www.opensuse.org/">openSUSE</a> x86 and x86_64</h1>
+<h1 class="bigtitle">VLC media player for <a href="http://www.opensuse.org/">openSUSE</a></h1>
+<h2>Download VLC for openSUSE</h2>
 
-<div id="left">
+<p>In order to install <a href="/vlc/">VLC</a> on an openSUSE, you have several
+options.</p>
 
-<p>In order to install VLC on an openSUSE, you have several
-options (besides downloading and installing the RPMs manually).</p>
-
-<h2 style="float: clear">Installing VLC using <a href="http://en.opensuse.org/One_Click_Install/UserFAQ">1-click-installs</a></h2>
+<h2 style="float: clear">Install VLC using
+ <a href="http://en.opensuse.org/One_Click_Install/UserFAQ">1-click-installs</a></h2>
 <div>
 <div class="installbtn" style="float: left;">
   <a href="http://download.videolan.org/pub/vlc/SuSE/11.4/vlc.ymp">VLC on 11.4</a>
@@ -33,13 +33,13 @@ options (besides downloading and installing the RPMs manually).</p>
 <br />
 <br />
 
-<h2>Install VLC media player for SUSE Linux using YaST</h2>
+<h2>Install VLC using YaST</h2>
 <h3>Add VideoLAN's repository</h3>
 <p>
 First, add a new repository (installation source) in YaST (YaST2 ->
 Software -> Installation Sources)</p>
 
-<p>Add the following Repository:</p>
+<p>Then, add the following Repository:</p>
 <pre>
 Type: HTTP / Servername: download.videolan.org /
 Directory: /pub/videolan/vlc/SuSE/&lt;SUSE Version&gt;
@@ -53,10 +53,10 @@ Version. Supported at this time are :</p>
 11.4<br />
 Factory<br />
 </p>
-<p>Confirm the addition.<br />
+<p>Finally, confirm the addition.<br />
 </p>
 
-<p>
+<h3>Security</h3>
 The repository is signed with a GPG Key 
 (gpg key C8DA93D2 fingerprint  BC8D 780D E330 8581 B2E0  7085 33DE 8FB7 C8DA 93D2).
 Accept this key in YaST and it will warn you in case there is something
@@ -66,12 +66,12 @@ Accept everything by clicking <b>Finish</b>.</p>
 
 <h3>Installation</h3>
 <p>
-Go to Software Management and enter VLC in the searchbox and search for
-it. Select it and click <b>Accept</b> to install it with all it's
-dependencies. This might require the SUSE CD or DVD.</p>
+Go to <em>Software Management</em> and enter <em>VLC</em> in the searchbox.<br />
+Select it and click <b>Accept</b> to install it with all its dependencies.<br />
+This might require the SUSE CD or DVD.</p>
 
-<p>If you want VLC's Mozilla Plugin, you have to select the
-vlc-mozillaplugin manually. It does not get installed by dependencies.</p>
+<p><b>NB:</b>If you want VLC's Mozilla Plugin, you have to select the
+vlc-mozillaplugin manually. It does not get installed by default.</p>
 
 <p>
 You're done with the installation.
@@ -85,14 +85,7 @@ sudo zypper mr -r VLC
 sudo zypper in vlc
 </pre>
 
-<p>openSUSEversion can be 11.0, 11.1, 11.2 or 11.3 </p>
-
-</div>
-
-<div id="right">
-
-</div>
-
+<p>openSUSEversion can be 11.1, 11.2, 11.3 or 11.4 </p>
 
 
 <?php footer('$Id$'); ?>

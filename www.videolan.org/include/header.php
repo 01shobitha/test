9cf721764dee0452bd4b6ddf6ed54df62c10b3eb
Jean-Baptiste Kempf <jb@videolan.org>
2011-04-26 09:59:22 +0000

    Small fix for alignment

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -101,7 +101,7 @@ function start_top( $body_color )
                <input name="tax" value="0" type="hidden"/>
                <input name="lc" value="GB" type="hidden"/>
                <input name="return" value="http://www.videolan.org/thank_you.html" type="hidden"/>
-               <input class='text' type='text' name="amount" value='10.00' style='background: #fff url("/images/euro.png") no-repeat 65px 3px;' />
+               <input class='text' type='text' name="amount" value='10.00' style='background: #fff url("/images/euro.png") no-repeat 65px 2px;' />
                <button class='button' type='submit'>donate</button>
        </p></form>
        <form style='clear: both; padding-top: 10p;' action="https://www.paypal.com/en_US/cgi-bin/webscr" method="post">

2262a5b3fc78ac91d42e6bcf5eb5c754ca61e09b
Jean-Baptiste Kempf <jb@videolan.org>
2011-05-23 21:32:06 +0000

    Android page

---
+++ www.videolan.org/vlc/download-android.php
@@ -0,0 +1,17 @@
+<?php
+   $title = "VLC media player for Android";
+   $lang = "en";
+   $menu = array( "vlc", "download" );
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<h1 class="bigtitle"> VLC media player for <a href="http://android.com/">Android</a></h1>
+
+<h1> Android </h1>
+
+<p><a href="/vlc/">VLC</a> for Android is <span style="font-weight: bold; color: red;">NOT ready YET</span>.</p>
+<p>Please, stop asking until it is released.</p>
+
+<?php
+  footer('$Id$');
+?>

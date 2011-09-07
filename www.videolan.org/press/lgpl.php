d39d9ab432e78d408db68d8b0c3a18af00a84f51
Jean-Baptiste Kempf <jb@videolan.org>
2011-09-07 20:14:07 +0000

    Add lgpl press release.

---
+++ www.videolan.org/press/lgpl.php
@@ -0,0 +1,40 @@
+<?php
+   $title = "VideoLAN - Press Release on LGPL";
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+<h1>VideoLAN starting the process of changing </h1>
+<h3>Paris, 7. Sept. 2011</h3>
+
+<p>During the third VideoLAN Dev Days, last weekend in Paris, numerous developers approved the process of changing the license of the VLC engine to LGPL.</p>
+<p>VLC is a highly popular libre and open source media player and multimedia engine, used by a large number of individuals, professionals, companies and institutions. Using open source technologies and libraries, the VLC engine has been ported to most computing platforms, including GNU/Linux, Windows, Mac OS X, BSD, iOS and Android. VLC can play most multimedia files, discs, streams, allows playback from devices, and is able to convert to or stream in various formats. The VideoLAN project was started at the university École Centrale Paris who relicensed VLC under the GPLv2 license in February 2001. Since then, VLC has been downloaded close to one billion times.</p>
+<p>VLC is a project of the VideoLAN non-profit organization, which supports the development of multiple libre and open source multimedia projects. In contrast to some other open source projects, VideoLAN does not require copyright assignment to the organization; instead the authors keep their copyright.</p>
+<p>This change of license was an initiative started by some of VLC's main developers and will be a change from the current license (GPLv2 or later) to the LGPLv2.1 or later license. This change was motivated to match the evolution of the video industry and to spread the VLC engine as a multi-platform open-source multimedia engine. The VideoLAN non-profit organisation and the École Centrale Paris approve this initiative.</p>
+<p>At first, the change will affect the VLC engine, also known as libVLC, allowing applications or plugins based on the VLC engine to be built under non-GPL licenses. The libVLC bindings for other languages are also concerned.</p>
+<p>In a second pass, more parts of VLC will change license, in the same way: important plugins and modules will change license depending on the agreement of the copyright holders.</p>
+<p>The license of the VLC media player will continue to be GPLv2 or later. This will not impact normal users of VLC in any way.</p>
+<p>Since the beginning of the process, a few months ago, the vast majority of concerned developers were contacted by VideoLAN. So far the major 40 developers have agreed and more than 80% of the copyright holders on VLC's core have agreed to this change. So far, no contributor has objected to this change, but some of them are difficult to contact. Past contributors that have not been reached yet should contact us.</p>
+<p>We would like to thank all the VLC copyright holders, developers and community. We would like to thank the École Centrale Paris for the support.</p>
+<p>For any questions, please <a href="/contact.html">contact us</a>.</p>
+
+</p>Jean-Baptiste Kempf
+on behalf of the VideoLAN project.</p>
+
+<h2>FAQ:</h2>
+<p>Will this change anything to VLC's distribution?<br />
+No, not in any way.</p>
+<p>Will VLC cost money in the future? Will there be a premium version of VLC?<br />
+No and no.</p>
+<p>Will VideoLAN move to a corporate structure?<br />
+No, VideoLAN will stay a non-profit organization managed by volunteers.</p>
+<p>It's awesome! How can I help?<br />
+Help us find the remaining missing contributors.
+If you have resources that can help us, please share these too./<p>
+<p>When will it be ready?<br />
+We hope to be able to do the change for VLC 1.2, but we do not have a release date yet.</p>
+<p>Why did you choose LGPLv2 instead of (L)GPLv3?<br />
+See http://www.videolan.org/press/2007-1.html</p>
+<p>Will this license change allow VLC to be available on the Apple stores?<br />
+So far, we don't know if this will change anything.</p>
+</div>

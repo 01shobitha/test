ecc38c1b7c589b51242f358678b38921d9ae28f6
Jean-Baptiste Kempf <jb@videolan.org>
2011-11-19 17:01:28 +0000

    Try to improve SEO for "download vlc"

--- www.videolan.org/vlc/download-windows.php
+++ www.videolan.org/vlc/download-windows.php
@@ -1,5 +1,5 @@
 <?php
-   $title = "Official VLC media player for Windows";
+   $title = "Download official VLC media player for Windows";
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>

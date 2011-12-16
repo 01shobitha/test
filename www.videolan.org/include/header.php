1f4def36118fb794f824e595eb5fe3c9fc3739d8
Jean-Baptiste Kempf <jb@videolan.org>
2011-12-16 19:50:29 +0000

    Less money...

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -119,7 +119,7 @@ function start_top( $body_color )
                <input name="lc" value="GB" type="hidden"/>
                <input name="no_shipping" value="1" type="hidden"/>
                <input name="return" value="http://www.videolan.org/thank_you.html" type="hidden"/>
-               <input class='text' type='text' name="amount" value='5.00' style='background: #fff url("/images/euro.png") no-repeat 65px 2px;' />
+               <input class='text' type='text' name="amount" value='4.00' style='background: #fff url("/images/euro.png") no-repeat 65px 2px;' />
                <button class='button' type='submit'><?php echo _("donate"); ?></button>
        </p></form>
        <form style='clear: both; padding-top: 10px;' action="https://www.paypal.com/en_US/cgi-bin/webscr" method="post">
@@ -133,7 +133,7 @@ function start_top( $body_color )
                <input name="lc" value="US" type="hidden"/>
                <input name="no_shipping" value="1" type="hidden"/>
                <input name="return" value="http://www.videolan.org/thank_you.html" type="hidden"/>
-               <input id="dtext" class='text' type='text' name="amount" value='7.00' style='background: #fff url("/images/dollar.png") no-repeat 0 3px; padding-right: 10px; width: 75px;' />
+               <input id="dtext" class='text' type='text' name="amount" value='5.00' style='background: #fff url("/images/dollar.png") no-repeat 0 3px; padding-right: 10px; width: 75px;' />
                <button class='button' type='submit'><?php echo _("donate"); ?></button>
        </p></form>
    </div>

fb3b0b29d796f3bc901638c5e5c07560846a03d8
Konstantin Pavlov <thresh@videolan.org>
2011-12-20 06:11:50 +0000

    Fix some other images links.

--- www.videolan.org/goodies.php
+++ www.videolan.org/goodies.php
@@ -211,11 +211,11 @@ href="http://images.videolan.org/images/videolan-backgrounds-base-2-1.src.rpm">h
 
 <div>
   <img src="http://images.videolan.org/images/goodies/videolan-wikingsoft.png" alt="Background" width="251" height="188" /><br />
-<a href="http://images.videolan.org/images/VideoLAN.png">Image 640x480</a> (188 kB)
+<a href="http://images.videolan.org/images/goodies/videolan.png">Image 640x480</a> (188 kB)
 </div>
 <div>
-  <img src="http://images.videolan.org/images/videolan-wikingsoft2.png" alt="Background" width="251" height="188" /><br />
-<a href="http://images.videolan.org/images/videolan-wikingsoft2.png">Image 1024x768</a> (56 kB)
+  <img src="http://images.videolan.org/images/goodies/videolan-wikingsoft2.png" alt="Background" width="251" height="188" /><br />
+<a href="http://images.videolan.org/images/goodies/videolan-wikingsoft2.png">Image 1024x768</a> (56 kB)
 </div>
 
 <p>This background image was made by <a href="http://www.xtremestuff.net">Asim Siddiqui</a>:</p>

dc184d54c1ba5ebd8a656c8ddbf5d95417e08d30
Jean-Baptiste Kempf <jb@videolan.org>
2012-02-09 16:16:49 +0000

    Fix Blogs encoding

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -177,7 +177,7 @@
         echo "<p>";
         echo "<b><a href=\"".$item['link'].'" style="color: black;">'.htmlentities($item['title'],ENT_COMPAT,"UTF-8")."</a></b></p>";
         echo "<p style=\"border-bottom: 1px dashed #CCC;padding-bottom:5px;margin-bottom:5px\">";
-        echo substr(strip_tags(html_entity_decode($item['content']['encoded'])),0,$chars_per_entry);
+        echo substr(strip_tags(html_entity_decode($item['content']['encoded'],ENT_COMPAT,"UTF-8")),0,$chars_per_entry);
         echo "<a href=\"".$item['link']."\">[...]</a><br/>";
         echo "</p>";
         $i++;

302f4de415ac0a57c7fc43944a0657c3d49191b4
Jean-Baptiste Kempf <jb@videolan.org>
2012-02-18 02:20:08 +0000

    More 2.0.0 bullshit

--- www.videolan.org/vlc/releases/2.0.0.php
+++ www.videolan.org/vlc/releases/2.0.0.php
@@ -17,18 +17,18 @@
             <?php image( 'largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'projectImg' ); ?>
             <div style='float: left; width: 500px;'>
                 <div class='projectDescription'>
-                    <div style="padding-bottom: 8px;">Faster when decoding, using multi-cores, GPU and mobile hardware,
-                         bringing support for more codecs, notably professional, HD and 10bits codecs,
-                         VLC 2.0 is a major upgrade for VLC.</div>
+                    <div style="padding-bottom: 8px;">With faster decoding on multi-core, GPU, and mobile hardware and the ability to open more
+                  formats, notably professional, HD and 10bits codecs, 2.0 is a major upgrade for VLC.
+                         </div>
                 
                     <div style="padding-bottom: 8px;">Twoflower has a new rendering pipeline for video, with higher quality subtitles,
                          and new video filters to enhance your videos.</div>
 
-                    <div style="padding-bottom: 8px;">It supports many new devices and Blu-Ray Discs (experimental).
+                    <div style="padding-bottom: 8px;">It supports many new devices and BluRay Discs (experimental).
                         </div>
 
-                    <div style="padding-bottom: 8px;">Completely reworked Mac and Web interfaces, and improvements on the other ones
-                        should help your usage of VLC.</div>
+                    <div style="padding-bottom: 8px;">Completely reworked Mac and Web interfaces and improvements in 
+                  the other interfaces make VLC easier than ever to use.</div>
 
                     <div>Finally, Twoflower fixes hundreds of bugs, in more than 7000 commits from 160 contributors.</div>
                 </div>
@@ -53,12 +53,16 @@
         <div class="clearme" style='padding-top: 20px;'>
             <h1 style='margin-bottom: 12px;'><?php echo _("Features"); ?></h1>
 
-            <div style='float: left; width: 480px;'>
+            <div style='float: left; width: 480px; margin-bottom: 20px;'>
                 <div class='audienceCallout'><?php echo _("Video"); ?></div>
                 <div class='clearme'>
                     <div class='productDescription'>
                         <ul>
-                           <li> test </li>
+                           <li>Rewritten video output core and modules, allowing subpicture blending in GPU.</li>
+                           <li>Shader support in the OpenGL output, for chroma conversion, including 10bits.</li>
+                           <li>New video outputs for Windows 7, Android, iOS and OS/2.</li>
+                           <li>New debanding, grain, denoising and anti-flickering filters.</li>
+                           <li>New deinterlacing filter, including an inverse telecine algorithm.</li>
                         </ul>
                     </div>
                 </div>
@@ -67,12 +71,100 @@
                 <div class='audienceCallout'><?php echo _("Audio"); ?></div>
                 <div class='clearme'>
                     <div class='productDescription'>
+                        <ul>
+                           <li>Simplification of the audio core for faster processing.</li>
+                           <li>New resamplers for higher quality audio.</li>
+                           <li>New dynamic range compressor and karaoke filters.</li> 
+                           <li>New audio outputs for iOS, Android and OS/2.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
+
+            <div class="clearme" style='float: left; width: 480px; margin-bottom: 20px;'>
+                <div class='audienceCallout'><?php echo _("Formats"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Multi-threaded decoding for H.264, Mpeg-4/Xvid and Webm.</li>
+                           <li>Support for 10bits codecs, WMV image and some other codecs.</li>
+                           <li>Rewritten support for images, including jpeg, png, xcf, bmp...</li>
+                           <li>Important changes in RealVideo and Real Format support.</li>
+                           <li>CrystalHD cards and Android OpenMAL support for hardware decoding.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div style='float: left; width: 480px;'>
+                <div class='audienceCallout'><?php echo _("Input and Devices"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Support for new adaptive streaming protocols, like HLS and DASH.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="clearme" style='float: left; width: 480px; margin-bottom: 20px;'>
+                <div class='audienceCallout'><?php echo _("For Mac Users"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Completly new interface, based on one single Window.</li>
+                           <li> - Available in 2 colors: Lion grey and QTX black.</li>
+                           <li> - Supporting extensions and better integrated in Lion.</li>
+                           <li>Support for all QTKit devices through qtcapture and qtsound modules.</li>
+                           <li>Still supporting X.5 and G5 computers.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div style='float: left; width: 480px;'>
+                <div class='audienceCallout'><?php echo _("For Anime Fans"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Vastly improved MKV demuxer.</li>
+                           <li>Rewritten linked segments and ordered chapter files support.</li>
+                           <li>Correct support for FLAC, RV and Hi10p in MKV.</li>
+                           <li>Rewritten Seeking support.</li>
+                           <li>Various ASS subtitles improvements.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="clearme" style='float: left; width: 480px;'>
+                <div class='audienceCallout'><?php echo _("For professional Users"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Support for ProRes 422 and 4444, AVC/Intra.</li>
+                           <li>Support for Jpeg-2000 and DNxHD/VC-3 in 10bits.</li>
+                           <li>Support for EBU subtitles (stl) and EIA-608.</li>
+                           <li>SDI and HD-SDI card support for input on Linux.</li>
+                           <li>New Dirac/VC-2 encoder, faster than the previous one.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div style='float: left; width: 480px;'>
+                <div class='audienceCallout'><?php echo _("For Developers"); ?></div>
+                <div class='clearme'>
+                    <div class='productDescription'>
+                        <ul>
+                           <li>Simplification of the audio core for faster processing.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
         <div class="clearme"></div>
+        <h1>Screenshots</h1>
 
         <a name="download" id="getit"></a>
         <h1>Download VLC</h1>

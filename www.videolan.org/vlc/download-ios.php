4ec238b730fc37f916c05b81827b127bb46e1615
Felix Paul Kühne <fkuehne@videolan.org>
2012-04-25 14:47:13 +0000

    removed current iOS binaries from the download page

--- www.videolan.org/vlc/download-ios.php
+++ www.videolan.org/vlc/download-ios.php
@@ -5,7 +5,7 @@
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<h1 class="bigtitle"> VLC media player for <a href="http://en.wikipedia.org/wiki/IOS_%28Apple%29">iOS</a> 3.2 and later</h1>
+<h1 class="bigtitle"> VLC media player for iOS</h1>
 
 <h2> Introduction </h2>
 <p>VLC for iOS is a bit different from the classic VLC.</p>
@@ -13,21 +13,16 @@
    However, the interface works in a slightly different way, that is more suited to this OS.</p>
 
 <h2> Devices and version </h2>
-<p>So far, it works on the iPad, iPhone4 and 3GS only.</p>
+<p>VLC for iOS requires an an iPad (any generation), an iPhone 3GS or a 3rd generation iPod touch running iOS 3.2 or later.</p>
 
 <h2> Binaries </h2>
-
-<p> You can find the binary here:</p>
-<code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/vlc-iOS-1.1.0.zip">vlc-iOS-1.1.0.zip</a></code>
-
+<p> We do not provide binary packages at this point.</p>
 
 <h2> Source </h2>
-
-<p> You can find the source here:</p>
-<code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/VLC-1.1.0-iOS.tar.bz2">VLC-iOS-1.1.0-sources</a></code>
-<code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/MediaLibraryKit-1.1.0.zip">MediaLibraryKit-1.1.0-sources</a></code>
+<p> You can find the source here for the last release here:</p>
+<code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/VLC-1.1.0-iOS.tar.bz2">VLC-iOS-1.1.0-sources</a></code><br />
+<code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/MediaLibraryKit-1.1.0.zip">MediaLibraryKit-1.1.0-sources</a></code><br />
 <code><a href="http://download.videolan.org/pub/videolan/testing/vlc-iOS/1.1.0/MobileVLC-1.1.0.tar.gz">MobileVLC-1.1.0-sources</a></code>
-
 
 <?php
   footer('$Id$');

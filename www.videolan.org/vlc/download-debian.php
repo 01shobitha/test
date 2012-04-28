c32cc42e3ad141d95b142c635be61f14b17c953f
Jean-Baptiste Kempf <jb@videolan.org>
2012-04-28 18:05:06 +0000

    Debian update

--- www.videolan.org/vlc/download-debian.php
+++ www.videolan.org/vlc/download-debian.php
@@ -12,67 +12,29 @@
 <h2> Debian stable (squeeze) </h2>
 
 <p> VLC is available in squeeze, with a quite old version.<br />
-Install vlc, mozilla-plugin-vlc, the usual way you do it. </p>
+Install <code>vlc</code>, <code>mozilla-plugin-vlc</code>, the usual way you do it. </p>
 
 
 <h2> Debian testing (wheezy) </h2>
 
 <p> VLC is in testing.<br />
- But due to the way testing works, we can't guarantee if the latest version is available.<br /> Install
-vlc, mozilla-plugin-vlc, the usual way you do it. </p>
+But due to the way testing works, we can't guarantee if the latest version is available.<br /> Install
+<code>vlc</code>, <code>mozilla-plugin-vlc</code>, the usual way you do it. </p>
 
 
 <h2> Debian unstable (sid) </h2>
 
-<p> VLC's latest packaged version is always in the official Debian unstable
-branch.</p>
+<p> VLC's latest packaged version is always in the official Debian unstable branch.</p>
 
 
 <?php panel_start( "blue" ); ?>
 <h1>Playing DVD</h1>
-<p>Development snapshots are available on <a href="http://nightlies.videolan.org">nightlies.videolan.org</a>.</p>
+
+To play DVD, you need to install the libdvdcss package.</p>
+
 <?php panel_end(); ?>
 
- However you should still use our apt-get line if you need libdvdcss: </p>
-
-<pre>
-     deb http://download.videolan.org/pub/videolan/debian sid main
-     deb-src http://download.videolan.org/pub/videolan/debian sid main
-</pre>
-
-<p> For a normal install, do:</p>
-
-<pre>
-   # apt-get update
-   # apt-get install vlc libdvdcss2
-</pre>
-
-<h2> Debian oldstable (lenny) </h2>
-
-<p> A very old version (0.8.6) is packaged in lenny. 
-However, if you want libdvdcss (DVD decryption) support, you will need to add the following lines to your <b>/etc/apt/sources.list</b>:</p>
-
-<pre>
-     deb http://download.videolan.org/pub/videolan/debian etch main
-     deb-src http://download.videolan.org/pub/videolan/debian etch main
-</pre>
-
-<p> For a normal install, do:</p>
-
-<pre>
-   # apt-get update
-   # apt-get install vlc libdvdcss2
-</pre>
-
 </div>
-
-<!--div id="right">
-
-<?php //panel_start( "blue" ); ?>
-<h1>Development snapshots</h1>
-<p>Development snapshots are available on <a href="http://nightlies.videolan.org">nightlies.videolan.org</a>.</p>
-<?php //panel_end(); ?>
-</div -->
 
 <?php
   footer('$Id$');

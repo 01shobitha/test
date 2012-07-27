b7eb6dc52de6a3763167fdea9bf1565b12c6f390
Jean-Baptiste Kempf <jb@videolan.org>
2012-07-27 18:33:57 +0000

    Trying to improve VLC for windows download page

--- www.videolan.org/vlc/download-windows.php
+++ www.videolan.org/vlc/download-windows.php
@@ -10,12 +10,17 @@
 <?php function pkgitemdev($version,$name) { ?><li><a href="http://downloads.videolan.org/pub/videolan/testing/<?php echo $version."/".$name; ?>"><code><?php echo $name; ?></code></a></li><?php } ?>
 
 <h1 class="bigtitle"> VLC media player for Windows </h1>
+<div id="sidebar">
+<?php panel_start( "blue" ); ?>
+<h2>Windows version</h2>
+<p>Windows XP SP2, 2003 SP2, Vista SP1, 2008 SP1, 7 and 8</p>
+<?php panel_end(); ?>
+</div>
 <div id="left">
 
 <?php $vlc_version = '2.0.3'; ?>
 <h1>Download latest VLC - <?php echo $vlc_version; ?></h2>
-<p>VLC currently supports <b>Windows XP SP2, Vista and 7</b>.</p>
-
+<p>VLC currently supports <b>Windows XP SP2 or later</b>.</p>
 <?php
     pkgitem_sf("Installer package",
                    "$vlc_version/win32","vlc-$vlc_version-win32.exe","vlc", "Exe installer",         "ba39d5def71d5193ade6bfb24672a487") ;
@@ -25,7 +30,7 @@
                   "$vlc_version/win32","vlc-$vlc_version-win32.zip", "vlc", "(No installer needed)", "e6faf4f33b32688ef9fbe8d6d71f7a8f" );
 ?>
 
-   <div class="clearme"> </div>
+<div class="clearme"> </div>
 <h1>Download latest VLC source code</h2>
 
 <p>If you want, you can download the <a href="download-sources.html">source code</a> of VLC media player.</p>

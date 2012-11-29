2df24b16beaff4d834bb4bf2022c8cac7f1ff6c5
Felix Paul Kühne <fkuehne@videolan.org>
2012-11-29 12:08:18 +0000

    add Win 8 KS PR

---
+++ www.videolan.org/press/win8ks.php
@@ -0,0 +1,41 @@
+<?php
+   $title = "VideoLAN - Press Release - Windows 8 Fundraiser";
+   $lang = "en";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<div class="press">
+<h1>VLC for the new Windows 8 User Experience fundraiser</h1>
+
+<p>Today, some VideoLAN team members launched a fundraiser on Kickstarter to support a port to the new User Experience on Windows 8 (aka "Metro") and Windows RT.</p>
+
+<p>VLC for Windows is widely used since a few years now and it is by far the dominant platform for VLC according to our user base numbers. Installation numbers are estimated as followed:<br />
+<li>100,000,000 on Windows</li>
+<li>10,000,000 on Mac OS X</li>
+<li>2,000,000 on Android</li>
+<li>countless on GNU/Linux, BSD and OS/2</li></p>
+
+<p>The current version of VLC is able to run on Windows 8, that was launched last month. However, VLC is now limited to the desktop experience, which is hidden together with other classical programs, behind one of the tiles (interface element of the Start screen).
+We want to allow users to get access to VLC from the Start screen and Metro experience too.</p>
+
+<p>To run within Metro, important parts of VLC need to be re-written or ported and an entirely new interface needs to be created. This is very time-consuming and considering our current Windows design, we need professional designers to adapt the interface to Metro. Additionally, we would like to have it in the hands of our Windows 8 users as soon as possible.</p>
+
+<p>Porting VLC to Metro relying only on volunteers working on it during  their spare time would make this port very slow to happen, like for VLC on Android. This is why we decided to run this fundraiser. If successful, it will allow two of us to work on this project full time, funders will get access to a broad range of goodies and users will benefit from quality software, which is going to be published early next year.</p>
+
+<p>Running a fundraiser is something the VideoLAN project has never done before. While the donations received through the VideoLAN association (a French non-profit organization) allow us to purchase testing devices, attend and prepare conferences and pay for servers, employing developers on a contract base would be beyond its current capabilities. This Kickstarter program will make it possible to do such contracts.</p>
+
+<p>We hope that you like our idea as much as we do and we are looking forward to answer any question you might have.
+</p>
+
+<h2>Contact</h2>
+<p>Felix Paul Kühne, <a href="mailto:fkuehne@videolan.org">fkuehne at videolan dot org</a></p>
+<p>Jean-Baptiste Kempf, <a href="mailto:jb@videolan.org">jb at videolan dot org</a></p>
+
+</div>
+
+</div>
+
+<?php footer('$Id: 2007-1.php 6098 2010-05-26 23:50:46Z jb $'); ?>

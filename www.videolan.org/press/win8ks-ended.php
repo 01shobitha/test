16d14c94ff4d5b91f7fc7bc936a2200a15cf5259
Felix Paul Kühne <fkuehne@videolan.org>
2012-12-29 17:55:00 +0000

    open external links in separate window/tab

--- www.videolan.org/press/win8ks-ended.php
+++ www.videolan.org/press/win8ks-ended.php
@@ -11,7 +11,7 @@
 <div class="longtext">
 <div class="date">Paris, 29 Dec 2012</div>
 
-<p><b>Today, the <a href="http://www.kickstarter.com/projects/1061646928/vlc-for-the-new-windows-8-user-experience-metro">kickstarter campaign</a> run by several core members of the VLC development team ended. £47,056 was pledged by 3080 generous backers. We would like to thank everyone who made this possible!</b></p>
+<p><b>Today, the <a href="http://www.kickstarter.com/projects/1061646928/vlc-for-the-new-windows-8-user-experience-metro" target="_blank">kickstarter campaign</a> run by several core members of the VLC development team ended. £47,056 was pledged by 3080 generous backers. We would like to thank everyone who made this possible!</b></p>
 
 <p>This is a tremendous and somewhat unexpected success. We are very pleased about it.</p>
 
@@ -22,16 +22,16 @@ We will perform detailed scheduling early next month and reach out to major IT c
 <br />
 <p><b>VLC media player</b> for Windows has been widely used for a few years now, and it is by far the dominant platform for VLC according to our user base numbers. We estimate an installed base  of about 100,000,000 individual devices on Windows only, used by consumers, professionals, companies and government agencies around the world.</p>
 <p>The current version of VLC is able to run on Windows 8. However, it is limited to the desktop experience, hidden by default along with other classic applications behind one of the tiles (interface element of the Start screen). We want to allow users to get access to VLC from the Start screen with the full Metro experience.</p>
-<p>To run in Metro, important parts of VLC need to be re-written or ported and an entirely new interface needs to be created. This is very time-consuming, and considering our current Windows design, we need professional designers to adapt the interface to Metro. Additionally, we would like to have it in the hands of our Windows 8 users as soon as possible.<br />
-The project funded through this campaign includes support for Windows RT and Windows Phone 8.</p>
+<p>To run in Metro, important parts of VLC need to be re-written or ported and an entirely new interface needs to be created. This is very time-consuming, and considering our current Windows design, we need professional designers to adapt the interface to Metro. Additionally, we would like to have it in the hands of our Windows 8 users as soon as possible.</p>
+<p>The project funded through this campaign includes support for Windows RT and Windows Phone 8.</p>
 <br />
 <p><b>Windows 8</b> is the latest release of Microsoft's Windows operating system, for use on personal computers, tablets, and home theater PCs. The operating system was released for general availability on October 26, 2012 and introduced the Modern UI a.k.a. Metro experience known from Windows Phone 7. With <b>Windows RT</b>, this major release adds support for the ARM platform.</p>
 <p><b>Windows Phone 8</b> is the latest release of Windows for mobile devices featuring the Modern UI. It was published on October 29, 2012 and features major improvements over the previous version, which allow us to port VLC to this platform. This is the third major mobile platform supported by VLC following iOS and Android.</p>
 
 <h2>Links</h2>
-<p><a href="http://www.kickstarter.com/projects/1061646928/vlc-for-the-new-windows-8-user-experience-metro">Fundraiser on Kickstarter</a></p>
-<p><a href="http://windows.microsoft.com/en-US/windows-8/meet">Windows 8 & RT</a></p>
-<p><a href="http://www.windowsphone.com">Windows Phone</a></p>
+<p><a href="http://www.kickstarter.com/projects/1061646928/vlc-for-the-new-windows-8-user-experience-metro" target="_blank">Fundraiser on Kickstarter</a></p>
+<p><a href="http://windows.microsoft.com/en-US/windows-8/meet" target="_blank">Windows 8 & RT</a></p>
+<p><a href="http://www.windowsphone.com" target="_blank">Windows Phone</a></p>
 
 <h2>Contacts</h2>
 <p>Felix Paul Kühne, <a href="mailto:fkuehne@videolan.org">fkuehne at videolan dot org</a></p>

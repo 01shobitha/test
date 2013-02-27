35a559783d66cc8c922499deb1986a27e42a2757
Ludovic Fauvet <etix@videolan.org>
2013-02-27 15:19:53 +0000

    Misplaced tag

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -123,6 +123,18 @@ function StartHtml( $title, $color, $language,
         }
     } ?>
 
+    <!-- analytics -->
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-38853043-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
     </head>
 
 <?php
@@ -381,17 +393,6 @@ function footer($tag = "") {
          </div>
       </div>
    </div>
-   <!-- analytics -->
-   <script type="text/javascript">
-     var _gaq = _gaq || [];
-     _gaq.push(['_setAccount', 'UA-38853043-1']);
-     _gaq.push(['_trackPageview']);
-     (function() {
-         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-       })();
-   </script>
    <!-- plusone button -->
    <script type="text/javascript">
      (function() {

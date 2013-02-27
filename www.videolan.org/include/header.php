f9771543b7e6b200b4c03d8eebfa59f501ff168b
Ludovic Fauvet <etix@videolan.org>
2013-02-27 14:33:08 +0000

    Add an analytics tracker for few days

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -381,6 +381,17 @@ function footer($tag = "") {
          </div>
       </div>
    </div>
+   <!-- analytics -->
+   <script type="text/javascript">
+     var _gaq = _gaq || [];
+     _gaq.push(['_setAccount', 'UA-38853043-1']);
+     _gaq.push(['_trackPageview']);
+     (function() {
+         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+       })();
+   </script>
    <!-- plusone button -->
    <script type="text/javascript">
      (function() {

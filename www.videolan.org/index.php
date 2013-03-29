605c22788ff9cdef6d0713246b695ff1b07d2858
Christoph Miebach <christoph.miebach@web.de>
2013-03-29 20:01:35 +0000

    Deliver ur (Urdu) by browser settings and add it to Traductions

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -6,7 +6,7 @@
    $additional_js = array("/js/slimbox2.js");
    $additional_css= array("/js/css/slimbox2.css");
 
-   $alternate_lang = array( "ast", "bn_IN", "bs", "cs", "de", "es", "et", "fr", "gd", "gl", "gu",  "hi", "he", "ja","km", "mr", "nb", "nl", "or", "pl", "pt", "pt_BR", "ru", "sk", "th", "tr", "uk", "wa"  );
+   $alternate_lang = array( "ast", "bn_IN", "bs", "cs", "de", "es", "et", "fr", "gd", "gl", "gu",  "hi", "he", "ja","km", "mr", "nb", "nl", "or", "pl", "pt", "pt_BR", "ru", "sk", "th", "tr", "uk", "ur", "wa"  );
 
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");

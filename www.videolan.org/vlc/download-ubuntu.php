d0d4febda03a2831c3e5a1e572bfec7afc294be0
Rafaël Carré <funman@videolan.org>
2013-05-09 12:07:01 +0000

    Remove howto add packages, universe (on by default), pulse (installed by default).

--- www.videolan.org/vlc/download-ubuntu.php
+++ www.videolan.org/vlc/download-ubuntu.php
@@ -24,23 +24,19 @@ Ubuntu 12.04 LTS <span style='font-size: smaller'>&ldquo;Precise Pangolin&rdquo;
 <h3>Graphical way</h3>
 <p>Open <b>Synaptic</b> application<br />
 Click on System -&gt; Administration -&gt; Synaptic Package Manager.<br />
-In Settings -&gt; Repositories, make sure you have an
-<code>universe</code> repository activated.
+In Settings -&gt; Repositories.
 </p>
 
-<p>Search for <kbd>vlc</kbd> and install it,
-as well as <kbd>vlc-plugin-pulse</kbd>.
+<p>Search for <kbd>vlc</kbd> and install it.
 You may also want to install <kbd>browser-plugin-vlc</kbd>.</p>
 <p>If you are interested in streaming or transcoding,
 you should additionnally install <kbd>libavcodec-extra-53</kbd>.</p>
 
 <h3>Command line way</h3>
-<p>You need to check that a <code>universe</code> mirror is listed
-in your <code>/etc/apt/sources.list</code> file.
-<blockquote>
+<p><blockquote>
 <pre>
 % sudo apt-get update
-% sudo apt-get install vlc vlc-plugin-pulse browser-plugin-vlc
+% sudo apt-get install vlc browser-plugin-vlc
 </pre>
 </blockquote>
 </p>
@@ -58,16 +54,6 @@ but no further major VLC version updates until the next Ubuntu release.
 </div>
 
 <div id="right">
-
-<?php panel_start( "blue" ); ?>
-<h1>How To Add repositories</h1>
-<p>If you don't know how to add repositories, follow the guides:
-</p>
-<ul>
-<li><a href="http://www.debianadmin.com/simple-package-management-with-synaptic-package-manager-in-ubuntu.html">Graphical Way</a></li>
-<li><a href="https://help.ubuntu.com/community/Repositories/CommandLine#head-927a60f6dbb71bf70af641185e9e0a76f5db28a2">Command-Line Way</a></li>
-</ul>
-<?php panel_end(); ?>
 
 <?php panel_start( "orange" ); ?>
 <h1>Playing DVD (libdvdcss)</h1>

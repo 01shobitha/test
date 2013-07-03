7ec4a4bd23f9468864a3573c0c51835a283e88f2
Rafaël Carré <funman@videolan.org>
2013-07-03 12:38:04 +0000

    x264.nl is dead

--- www.videolan.org/developers/x264.php
+++ www.videolan.org/developers/x264.php
@@ -64,7 +64,7 @@
 
     <p>You can <a href="http://git.videolan.org/?p=x264.git;a=summary">browse the source</a> on-line.</p>
 
-    <p>Builds for Windows are available from <a href="http://doom10.org/index.php?topic=3.0">the list of unofficial builds</a>, or from <a href="http://x264.nl/">Jarod</a>.</p>
+    <p>Builds for Windows are available from <a href="http://doom10.org/index.php?topic=3.0">the list of unofficial builds</a>.</p>
 
 </div>
 

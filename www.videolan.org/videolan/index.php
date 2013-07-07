6d30d0b0dc86d73764ec461ec12f98b76f2ef8d3
Ludovic Fauvet <etix@videolan.org>
2013-07-07 15:37:16 +0000

    Remove duplicate VideoLAN in $title

--- www.videolan.org/videolan/index.php
+++ www.videolan.org/videolan/index.php
@@ -1,5 +1,5 @@
 <?php
-   $title = "VideoLAN - Free Software and Open Source video streaming solution for every OS!";
+   $title = "Free Software and Open Source video streaming solution for every OS!";
    $lang = "en";
    $body_color = "blue";
    $additional_css = array("/style/panels.css");
--- www.videolan.org/videolan/mirrors.php
+++ www.videolan.org/videolan/mirrors.php
@@ -1,5 +1,5 @@
 <?php
-   $title = "VideoLAN - Free Software and Open Source video streaming solution for every OS!";
+   $title = "Free Software and Open Source video streaming solution for every OS!";
    $lang = "en";
    $body_color = "red";
    $additional_css = array("/style/panels.css", "/style/mirrors.css");

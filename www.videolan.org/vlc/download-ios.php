4d2152f9a39b871386a054b491a1c5d959cc2367
Felix Paul Kühne <fkuehne@videolan.org>
2013-07-18 16:45:35 +0000

    Push draft for Aspen

--- www.videolan.org/vlc/download-ios.php
+++ www.videolan.org/vlc/download-ios.php
@@ -5,24 +5,47 @@
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<h1 class="bigtitle"> VLC media player for iOS</h1>
+<h1 class="bigtitle"> VLC for iOS</h1>
 
 <h2> Introduction </h2>
-<p>VLC for iOS is a bit different from the classic VLC.</p>
-<p>It is based on libVLC, libVLCcore and all libVLC plugins, like the normal VLC.<br />
-   However, the interface works in a slightly different way, that is more suited to this OS.</p>
+<p>VLC for iOS can play all your movies and shows in most formats directly without conversion. You can synchronize media to your device using iTunes, WiFi Upload, Dropbox, or direct downloads from the web.</p>
 
-<h2> Devices and version </h2>
-<p>VLC for iOS requires at least an iPhone 3GS or a 3rd generation iPod touch or an iPad (any generation) running iOS 3.2 or later.</p>
+<h2> Devices and iOS version </h2>
+<p>VLC for iOS requires iOS 5.1 or later. It runs on any iPhone, iPad, or iPod touch.</p>
 
 <h2> Binaries </h2>
-<p> We do not provide binary packages at this point.</p>
+<p><a href="https://itunes.apple.com/app/id650377962"><img src="../images/appstoredownload-en.png" border="0" /></a><br /><br />
+<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.0.1/VLC-2.0.1-iOS-binary.zip">VLC for iOS 2.0.1 binary</a></code>
+</p>
+
+<h2> License </h2>
+<p> VLC for iOS is bi-licensed under the <a href="http://opensource.org/licenses/MPL-2.0">Mozilla Public License Version 2</a> as well as the <a href="http://opensource.org/licenses/GPL-2.0">GNU General Public License Version 2 or later</a>. You can modify or redistribute its sources under the conditions of these licenses. Note that additional terms apply for trademarks owned by the VideoLAN association.</p>
+
+<h2> Appearance </h2>
+<div style='float: right; width: 450px; padding-left: 70px;'>
+    <?php
+    vlcscreenshot( "ios2/ipadblackplayback.png",
+                "ios2/ipadblackplayback-small.png",
+                "Playback on iPad");
+    vlcscreenshot( "ios2/ipadwhitelibrary.png",
+                "ios2/ipadwhitelibrary-small.png",
+                "iPad Library" );
+    vlcscreenshot( "ios2/iphoneblackplayback.png",
+                "ios2/iphoneblackplayback-small.png",
+                "Playback on iPhone" );
+    vlcscreenshot( "ios2/iphoneblacklibrary.png",
+                "ios2/iphoneblacklibrary-small.png",
+                "Library on iPhone" );
+    ?>
+</div>
 
 <h2> Source </h2>
 <p> You can find the source code for the last release here:</p>
-<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/1.1.0/VLC-1.1.0-iOS.tar.bz2">VLC-iOS-1.1.0-sources</a></code><br />
-<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/1.1.0/MediaLibraryKit-1.1.0.zip">MediaLibraryKit-1.1.0-sources</a></code><br />
-<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/1.1.0/MobileVLC-1.1.0.tar.gz">MobileVLC-1.1.0-sources</a></code>
+<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.0.1/VLC-2.0.1-iOS.tar.bz2">VLC for iOS 2.0.1 source code</a></code><br />
+<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.0.1/MediaLibraryKit-2.0.0.zip">MediaLibraryKit 2.0.0 source code</a></code><br />
+<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.0.1/MobileVLCKit-2.1.0-pre1.tar.gz">MobileVLCKit 2.1.0-pre1 source code</a></code><br />
+<br />
+<p> Additionally, the latest code is always available on our <a href="http://git.videolan.org">git repositories</a>.</p>
 
 <?php
   footer('$Id$');

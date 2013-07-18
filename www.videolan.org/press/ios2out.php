f0174caadd9f27df25a5ffcbca8716fd6e795dd0
Felix Paul Kühne <fkuehne@videolan.org>
2013-07-18 17:44:28 +0000

    pr: add basic Aspen statement

---
+++ www.videolan.org/press/ios2out.php
@@ -0,0 +1,35 @@
+<?php
+   $title = "VideoLAN - Press Release - Windows 8 Fundraiser";
+   $lang = "en";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<h1>VLC for iOS 2.0</h1>
+<br />
+
+<div class="longtext">
+<div class="date">Paris, Jul 18 2013</div>
+
+<p><b>We have exciting news to share. VLC for iOS will be <a href="https://itunes.apple.com/app/id650377962">back on the App Store</a> today!</b></p>
+
+<p>It will be available free of charge in any country, requires iOS 5.1 or later and runs on any iPhone, iPad or iPod touch.</p>
+
+<p>This is more than an upgrade of the initial version: it's a full re-write. It's way faster thanks to modern output modules for audio and video, offers multi-core decoding, and supports any video files available in VLC media player for desktop operating systems.</br />
+In addition to the original feature set, we added more ways to synchronize media (upload via WiFi, native Dropbox integration, support for 3rd party apps through the Share dialog, web download), support for network streams, video filters, subtitle rendering, passcode lock and playback speed manipulation.</p>
+
+<p>VLC for iOS is fully open-source. Its code will be available online by tomorrow and is bi-licensed under both the Mozilla Public License Version 2 as well as the GNU General Public License Version 2 or later. The MPLv2 is applicable for distribution on the App Store.<br />
+We worked hard with all major copyright holders to not repeat history and we are confident to have achieved this goal.</p>
+
+<p>We are happy to answer any questions you may have</p>
+
+<h2>Links</h2>
+<p><a href="https://itunes.apple.com/app/id650377962">VLC for iOS on the App Store</a></p>
+
+<h2>Contacts</h2>
+<p>Felix Paul Kühne, <a href="mailto:fkuehne@videolan.org">fkuehne at videolan dot org</a></p>
+<p>Jean-Baptiste Kempf, <a href="mailto:jb@videolan.org">jb at videolan dot org</a></p>
+
+</div>
+
+<?php footer('$Id: 2007-1.php 6098 2010-05-26 23:50:46Z jb $'); ?>

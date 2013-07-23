febde983167f4f889586e82d456775f0a907227d
Rafaël Carré <funman@videolan.org>
2013-07-23 22:53:02 +0000

    Bump debian names

--- www.videolan.org/vlc/download-debian.php
+++ www.videolan.org/vlc/download-debian.php
@@ -9,7 +9,7 @@
 <h1 class="bigtitle"> VLC media player for <a href="http://debian.org">Debian GNU/Linux</a></h1>
 <div id="left">
 
-<h2> Debian stable (squeeze) </h2>
+<h2> Debian stable (wheezy) </h2>
 
 <p> It is highly recommended that you get a recent version of <a href="/vlc/">VLC</a> on squeeze by installing the version from <b>squeeze-backports</b>.<br />
 Add the <a href="http://backports-master.debian.org/Instructions/#index2h2">squeeze-backports repository</a> and install VLC: <code>sudo apt-get -t squeeze-backports install vlc</code>
@@ -18,7 +18,7 @@ Add the <a href="http://backports-master.debian.org/Instructions/#index2h2">sque
 <p> Without backports, you will end up with the very old version of VLC in vanilla squeeze (install vlc and browser-plugin-vlc as usual).</p>
 
 
-<h2> Debian testing (wheezy) </h2>
+<h2> Debian testing (jessie) </h2>
 
 <p> VLC is in testing.<br />
 But due to the way testing works, we can't guarantee if the latest version is available.<br /> Install

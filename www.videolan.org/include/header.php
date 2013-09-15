9300e2dacb09b0a048eb76707677a65b6345e584
Christoph Miebach <christoph.miebach@web.de>
2013-09-15 22:01:26 +0000

    Adding mn_MN (Mongolian)

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -37,6 +37,7 @@ function getLocaleFromLanguage( $language )
     case 'ky': $locale = "ky_KG"; break;
     case 'lt': $locale = "lt_LT.utf8"; break;
     case 'mk': $locale = "mk_MK.UTF-8"; break;
+    case 'mn_MN': $locale = "mn_MN"; break;
     case 'mr': $locale = "mr_IN.UTF-8"; break;
     case 'my': $locale = "my_MM"; break;
     case 'nb': $locale = "nb_NO.UTF-8"; break;

8fa78aa1b2de23d205a16ab48792de1f060075d3
Jean-Paul Saman <jpsaman@videolan.org>
2013-10-02 16:15:19 +0000

    Release 1.1.1

--- www.videolan.org/developers/libdvbpsi.php
+++ www.videolan.org/developers/libdvbpsi.php
@@ -30,10 +30,13 @@ and ITU-T H.222.0.</p>
 
 <h2>Get libdvbpsi</h2>
 
-<p>The latest <code>libdvbpsi</code> (LGPL v2.1) release <a href="http://www.videolan.org/pub/libdvbpsi/1.1.0/"><b>version 1.1.0</b></a>. Note: the API has changed since version 0.2.2 you need to update your applications.</p>
+<p>The latest <code>libdvbpsi</code> (LGPL v2.1) release <a href="http://www.videolan.org/pub/libdvbpsi/1.1.1/"><b>version 1.1.1</b></a>. Note: the API has changed since version 0.2.2 you need to update your applications.</p>
 <p>The latest <code>libdvbpsi</code> GPL v2 release is <a href="http://www.videolan.org/pub/libdvbpsi/0.1.7/"><b>version 0.1.7</b></a>.</p>
-<p> <code>libdvbpsi</code> is available through <a href="http://wiki.videolan.org/git">git</a> at:<br />
-<code>git clone git://git.videolan.org/libdvbpsi.git</code></p>
+<p> <code>libdvbpsi</code> source code is available through <a href="http://wiki.videolan.org/git">git</a> at:<br />
+<code>git clone git://git.videolan.org/libdvbpsi.git</code><br />
+The stable release source code archive is available through:<br />
+<code>git checkout -t origin/branch/1.1.0-bugfix</code>
+</p>
 
 <p>All releases can be found by browsing the <a href="http://www.videolan.org/pub/libdvbpsi/"><b>releases</b></a>.</p>
 <p>The source code can browsed at  <a href="http://git.videolan.org/?p=libdvbpsi.git">git.videolan.org</a>.</p>

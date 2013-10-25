08aec2572ee8455c8f2d1c6652271db7c34b2689
Christoph Miebach <christoph.miebach@web.de>
2013-10-25 15:38:37 +0000

    Adding Persian (fa)

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -19,6 +19,7 @@ function getLocaleFromLanguage( $language )
     case 'el': $locale = "el_GR.UTF-8"; break;
     case 'es': $locale = "es_ES.UTF-8"; break;
     case 'et': $locale = "et_EE.utf8"; break;
+    case 'fa': $locale = "fa_IR"; break;
     case 'fi': $locale = "fi_FI.UTF-8"; break;
     case 'fr': $locale = "fr_FR.UTF-8"; break;
     case 'gd': $locale = "gd_GB.UTF-8"; break;

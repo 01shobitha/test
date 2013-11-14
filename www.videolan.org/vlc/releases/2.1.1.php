e45b079808b4d31d65a40821c898339307fbaa9b
Jean-Baptiste Kempf <jb@videolan.org>
2013-11-14 13:40:03 +0000

    Update 2.1.1 release page

--- www.videolan.org/vlc/releases/2.1.1.php
+++ www.videolan.org/vlc/releases/2.1.1.php
@@ -156,7 +156,7 @@
                     <img style='position: absolute; top: -10px; left: -10px;' src='/images/downloadVLC.png' alt='Download VLC icon' />
                     <span class='downloadText'><?php echo _("Get VLC now!"); ?></span>
                     <span id='downloadDetails' style='font-size: 12px; color: white;'>
-                    Version <span id='downloadVersion'>2.1.0</span>
+                    Version <span id='downloadVersion'>2.1.1</span>
                     </span>
                 </a>
                 <script>

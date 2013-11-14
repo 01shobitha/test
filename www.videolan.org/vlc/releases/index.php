5846cac568ad4a4654b52f11f54ce26518ffb1af
Jean-Baptiste Kempf <jb@videolan.org>
2013-11-14 13:31:28 +0000

    Update release pages

--- www.videolan.org/vlc/releases/index.php
+++ www.videolan.org/vlc/releases/index.php
@@ -8,6 +8,22 @@
 
 <h1>VLC Releases</h1>
 
+<h2>VLC 2.1.x branch</h2>
+<a href="/vlc/releases/2.1.0.html">VLC 2.1.0</a><br />
+<a href="/vlc/releases/2.1.1.html">VLC 2.1.1</a><br />
+
+<h2>VLC 2.0.x branch</h2>
+<a href="/vlc/releases/2.0.0.html">VLC 2.0.0</a><br />
+<a href="/vlc/releases/2.0.1.html">VLC 2.0.1</a><br />
+<a href="/vlc/releases/2.0.2.html">VLC 2.0.2</a><br />
+<a href="/vlc/releases/2.0.3.html">VLC 2.0.3</a><br />
+<a href="/vlc/releases/2.0.4.html">VLC 2.0.4</a><br />
+<a href="/vlc/releases/2.0.5.html">VLC 2.0.5</a><br />
+<a href="/vlc/releases/2.0.6.html">VLC 2.0.6</a><br />
+<a href="/vlc/releases/2.0.7.html">VLC 2.0.7</a><br />
+<a href="/vlc/releases/2.0.8.html">VLC 2.0.8</a><br />
+
+
 <h2>VLC 1.1.x branch</h2>
 <a href="/vlc/releases/1.1.0-RC.html">VLC 1.1.0 - Release Candidate</a><br />
 <a href="/vlc/releases/1.1.0.html">VLC 1.1.0</a><br />
@@ -22,16 +38,5 @@
 <a href="/vlc/releases/1.1.9.html">VLC 1.1.9</a><br />
 <a href="/vlc/releases/1.1.10.html">VLC 1.1.10</a><br />
 <a href="/vlc/releases/1.1.11.html">VLC 1.1.11</a><br />
-
-<h2>VLC 2.0.x branch</h2>
-<a href="/vlc/releases/2.0.0.html">VLC 2.0.0</a><br />
-<a href="/vlc/releases/2.0.1.html">VLC 2.0.1</a><br />
-<a href="/vlc/releases/2.0.2.html">VLC 2.0.2</a><br />
-<a href="/vlc/releases/2.0.3.html">VLC 2.0.3</a><br />
-<a href="/vlc/releases/2.0.4.html">VLC 2.0.4</a><br />
-<a href="/vlc/releases/2.0.5.html">VLC 2.0.5</a><br />
-<a href="/vlc/releases/2.0.6.html">VLC 2.0.6</a><br />
-<a href="/vlc/releases/2.0.7.html">VLC 2.0.7</a><br />
-<a href="/vlc/releases/2.0.8.html">VLC 2.0.8</a><br />
 
 <?php footer('$Id: news.php 6098 2010-05-26 23:50:46Z jb $'); ?>

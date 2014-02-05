4e09e3036738a6a01c59e793962c6986c1dbddf5
Jean-Baptiste Kempf <jb@videolan.org>
2014-02-05 02:02:39 +0000

    Add a privacy file for WinRT

---
+++ www.videolan.org/vlc/privacy.php
@@ -0,0 +1,26 @@
+<?php
+   $title = 'Legal Privacy';
+   $body_color = "green";
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<div>
+<h1>Privacy</h1>
+
+<h2>VLC for WinRT</h2>
+<p>
+This privacy policy cover the use of the 'VLC for WinRT' application.  It may not be applicable
+to other software produced or released by VideoLAN.
+</p>
+<p>
+VideoLAN does not collect any statistics, personal information, or analytics from our users,
+other than built in mechanisms that are present for all applications in the 'Windows Store'.
+</p>
+<p>
+The 'VLC for WinRT' application does allow for videos to be played via various network
+transports.  Cookies and authentication credentials are not stored at any point.
+</p>
+
+</div>
+
+<?php footer();?>

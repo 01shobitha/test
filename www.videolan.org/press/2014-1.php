e13dd2b6d02e6b02f40b8d9db449699bd4effc36
Jean-Baptiste Kempf <jb@videolan.org>
2014-04-01 08:13:37 +0000

    More info

--- www.videolan.org/press/2014-1.php
+++ www.videolan.org/press/2014-1.php
@@ -15,13 +15,13 @@
 <p>The multimedia world evolves <em>quickly</em>, new codecs need more and more processing power to achieve higher compression rates with better quality.<br />
  Yet tablets and smartphones are too small to go to the <b>Ultra-HD level</b>, and some old computers shouldn't be sent to trash just to see Hi-Quality videos.</p>
 
-<p>Seeing that VLC counts dozens of million of users around the world and that many PC where it runs have a lot of unused computing power, the VideoLAN Team has finished its work on a distributed decoder that could allow our most privileged users to help the other users decode their video. Imagine millions of computers all working together to make multimedia contents available to everyone!</p>
+<p>Seeing that VLC counts dozens of million of users around the world and that many PC where it runs have a lot of unused computing power, the VideoLAN Team has finished its work on a distributed codec named <b>"CloudCodet"</b> that could allow our most privileged users to help the other users decode their video. Imagine millions of computers all working together to make multimedia contents available to everyone!</p>
 
 <p>Of course, we won't push CPU and electric load on our beloved users for nothing!</p>
 
 <p>As a non-profit organisation, VideoLAN is unable to pay you real money, which is why, following the latest trend, the team is proud to introduce its own electronic money: <b>ConeCoins</b>!</p>
 
-<p>Every CPU cycle used to help other people decode their video and audio will be counted as "mining" and will earn you these ConeCoins. We will soon introduce a shop to exchange these coins for goodies like T-shirt, mugs and chocolates!</p>
+<p>Every CPU cycle used to help other people decode their video and audio will be counted as <em>"mining"</em> and will earn you these ConeCoins. We will soon introduce a shop to exchange these coins for goodies like T-shirt, mugs, cones and chocolates!</p>
 
 <p>If everybody helps, we can make this connected world a more equal one! See you soon on the <b>ConeMarket</b>!</p>
 

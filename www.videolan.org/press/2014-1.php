46ac4cf1e1eaf7bf498ff45ed7e4c7b25ad8c403
Jean-Baptiste Kempf <jb@videolan.org>
2014-04-01 07:54:16 +0000

    Press

---
+++ www.videolan.org/press/2014-1.php
@@ -0,0 +1,42 @@
+<?php
+   $title = "VideoLAN - Press Release - ConeCoin";
+   $lang = "en";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+<div id="fullwidth">
+
+<div class="press">
+<h1>Press Release about VLC distributed decoding and codecoin</h1>
+
+<h2>VLC starts a distributed decoding offer</h2>
+
+<p>The multimedia evolves quickly, new codecs need more and more processing power to achieve higher compression rates with better quality. Yet computer are expensive and some people cannot afford to buy new one just to see videos.</p>
+
+<p>Seeing that VLC counts millions of users throughout the world and that many PC where it runs have a lot of unused computing power, the VideoLAN Team started to work on a distributed decoder that could allow our most privileged users to help the other decode their video. Imagine millions of computers all working together to make multimedia contents available to everyone!</p>
+
+<p>Of course, we won't push CPU and electric load on our beloved users for nothing!</p>
+
+<p>As a non-profit organisation, VideoLAN is unable to pay you real money, which is why, following the latest trend, the team is proud to introduce its own electronic money: <b>ConeCoins</b>!</p>
+
+<p>Every CPU cycle used to help other people decode their video and audio will be counted as "mining" and will earn you these ConeCoins. We will soon introduce a shop to exchange these coins for goodies like T-shirt, mugs and chocolates!</p>
+
+<p>If everybody helps, we can make this connected world a more equal one! See you soon on the <b>ConeMarket</b>!</p>
+
+<b>The VideoLAN Team</b>
+
+<h2>Contact VideoLAN</h2>
+<p>To contact VideoLAN, see the contact page.</p>
+
+<h2>References</h2>
+<dl>
+<dt>VLC media player</dt>
+<dd><a href="http://www.videolan.org/vlc/">http://www.videolan.org/vlc/</a>
+</dd>
+</dl>
+</div>
+
+</div>
+
+<?php footer('$Id: 2007-1.php 6098 2010-05-26 23:50:46Z jb $'); ?>

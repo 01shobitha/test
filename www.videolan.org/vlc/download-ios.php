1db13d5b35ff7b776b20941eae1847fd6790697e
Jean-Baptiste Kempf <jb@videolan.org>
2014-04-18 09:09:44 +0000

    UPdate iOS page

--- www.videolan.org/vlc/download-ios.php
+++ www.videolan.org/vlc/download-ios.php
@@ -2,14 +2,16 @@
    $title = "VLC for iOS";
    $lang = "en";
    $menu = array( "vlc", "download" );
-   
+
    $additional_js = array("/js/slimbox2.js");
    $additional_css= array("/js/css/slimbox2.css");
-   
+
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+
+   $version = "2.3.0";
 ?>
 
-<h1 class="bigtitle"> VLC for iOS 2.2.2</h1>
+    <h1 class="bigtitle"> VLC for iOS <?php echo $version; ?></h1>
 
 <h2> Introduction </h2>
 <p>VLC for iOS can play all your movies and shows in most formats directly without conversion.<br />
@@ -69,9 +71,12 @@ function vlcscreenshot($img,$thumb, $desc){
 
 <h2> Source </h2>
 <p> You can find the source code for the last release here:</p>
-<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.2.2/vlc-iOS-2.2.2.tar.gz">VLC for iOS 2.2.2 source code</a></code><br />
+
+<?php
+   echo "<code><a href='http://download.videolan.org/pub/videolan/vlc-iOS/$version/vlc-iOS-$version.tar.gz'>VLC for iOS $version source code</a></code><br />"; ?>
 <code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.2.1/MediaLibraryKit-2.2.1.tar.gz">MediaLibraryKit 2.2.1 source code</a></code><br />
 <code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.2.2/VLCKit-2.2.0-pre3.tar.gz">MobileVLCKit 2.2.0-pre3 source code</a></code><br />
+
 <p> Additionally, the latest code is always available on our <a href="http://git.videolan.org/?p=vlc-ports/ios.git;a=summary">git repositories</a>.</p>
 
 <h2> Developers </h2>
@@ -81,7 +86,9 @@ You can see the <a href="//wiki.videolan.org/iOSCompile">instructions to build</
 
 <h2> Binary </h2>
 <p> You can also get the binary from our servers instead of the App Store:
-<code><a href="http://download.videolan.org/pub/videolan/vlc-iOS/2.2.2/vlc-iOS-2.2.2.ipa">VLC for iOS 2.2.2 binary</a></code>.
+
+<?php
+   echo "<code><a href='http://download.videolan.org/pub/videolan/vlc-iOS/$version/vlc-iOS-$version.ipa'>VLC for iOS $version binary</a></code>."; ?>
 </p>
 
 <h2> License </h2>

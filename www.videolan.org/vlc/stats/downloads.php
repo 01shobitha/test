c189410f2c99fae497ec384eb53515e02d36037e
Jean-Baptiste Kempf <jb@videolan.org>
2014-07-25 02:21:28 +0000

    Downloads: Fix error when the DB is not present

--- www.videolan.org/vlc/stats/downloads.php
+++ www.videolan.org/vlc/stats/downloads.php
@@ -37,6 +37,11 @@
         echo "<td style='text-align: right;'>".number_format($n,0,"."," ")."</td>";
     }
 
+    $wintotal = 0;
+    $mactotal = 0;
+    $srctotal = 0;
+
+
     $filenamedb = '/home/videolan/etc/db.php';
     if( is_readable( $filenamedb ) ) {
         require '/home/videolan/etc/db.php';
@@ -49,10 +54,6 @@
         }
 
         $requests = array(  "0.7.2" , "0.8.0","0.8.1","0.8.2", "0.8.4", "0.8.5", "0.8.6", "0.9.2", "0.9.3", "0.9.4", "0.9.5", "0.9.6", "0.9.8a", "0.9.9", "0.9.10", "1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5", "1.0.6" );
-        $wintotal = 0;
-        $mactotal = 0;
-        $srctotal = 0;
-
         foreach( $requests as $v )
         {
             $versiontotal= 0;

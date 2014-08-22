1ac6dbcc894456d114766fae23a43a0bb2231408
Christoph Miebach <christoph.miebach@web.de>
2014-08-22 19:35:56 +0000

    Adding lb (Luxembourgish)

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -41,6 +41,7 @@ function getLocaleFromLanguage( $language )
     case 'kn': $locale = "kn_IN"; break;
     case 'ko': $locale = "ko_KR.utf8"; break;
     case 'ky': $locale = "ky_KG"; break;
+    case 'lb': $locale = "de_LU.utf8"; break;
     case 'lt': $locale = "lt_LT.utf8"; break;
     case 'lv': $locale = "lv_LV.utf8"; break;
     case 'mk': $locale = "mk_MK.UTF-8"; break;

89d12104bc90394acecb1fcef459c5bc9baff1d6
Carola Nitz <nitz.carola@googlemail.com>
2014-10-20 00:04:35 +0000

    added a contents  menu to the faq

--- www.videolan.org/support/faq.php
+++ www.videolan.org/support/faq.php
@@ -17,9 +17,49 @@ The text of the license can be found on
 <a href="http://www.gnu.org/copyleft/gpl.html">the GNU website</a>.
 </p>
 
-<h1>VideoLAN</h1>
+</div>
 
-<h2>What is VideoLAN? What is the VideoLAN Team?</h2>
+<h1>Contents</h1>
+</br>
+<ul>
+<li><a href="#VideoLAN"><span>1</span> <span>VideoLAN</span></a>
+<ul class="bullets">
+<li><a href="#WhatIs"><span>1.1</span> <span>What is VideoLAN? What is the VideoLAN Team?</span></a></li>
+<li><a href="#LegalStructure"><span>1.2</span> <span>What is the legal structure of VideoLAN?</span></a></li>
+<li><a href="#Differences"><span>1.3</span> <span>What are the differences between VLC, VLC media player, VideoLAN Client, VideoLAN Server and VideoLAN?</span></a>
+<li><a href="#Contribute"><span >1.4</span> <span>How can I contribute to VideoLAN?</span></a></li>	
+<li><a href="#Mailinglist"><span >1.5</span> <span>Why are your mailing-lists and your website in english?</span></a></li>	
+<li><a href="#What"><span >1.6</span> <span>What should I do if I can't find an answer to my question here?</span></a></li>	
+</ul>
+</li>
+<li><a href="#MediaPlayer"><span>2</span> <span>VLC Media Player</span></a>
+<ul class="bullets">
+<li><a href="#DVDS"><span>2.1</span> <span>Does VLC support DVDs from all regions?</span></a></li>
+<li><a href="#Config"><span>2.2</span> <span>Where does VLC store its config file?</span></a></li>
+<li><a href="#StrangeBehaviour"><span>2.3</span> <span>VLC has a strange behavior...</span></a>
+<li><a href="#TooDark"><span >2.4</span> <span>Videos are too dark</span></a></li>	
+<li><a href="#CannotDVD"><span >2.5</span> <span>I cannot read DVDs!</span></a></li>	
+<li><a href="#Choppy"><span >2.6</span> <span>Video is choppy</span></a></li>	
+<li><a href="#OutOfSync"><span >2.7</span> <span>Audio and video are out of sync</span></a></li>
+<li><a href="#Crash"><span >2.8</span> <span>VLC crashes</span></a></li>
+<li><a href="#Screenshot"><span >2.9</span> <span>How can I take screenshots?</span></a></li>
+<li><a href="#WhereScreenshots"><span >2.10</span> <span>Where are my screenshots?</span></a></li>
+<li><a href="#DoesntWork"><span >2.11</span> <span>My file doesn't seem to work!</span></a></li>
+<li><a href="#Subtitles"><span >2.12</span> <span>VLC doesn't display all subtitles</span></a></li>
+</ul>
+</li>
+<li><a href="#Support"><span>3</span> <span>Support And Feature Requests</span></a>
+<ul class="bullets">
+<li><a href="#Banned"><span >3.1</span> <span>Why am I permanently banned from the forum?</span></a></li>
+<li><a href="#WindowsPhone"><span >3.2</span> <span>Will VLC be available on Windows Phone?</span></a></li>
+<li><a href="#ChromeCast"><span >3.3</span> <span>When will VLC be able to stream to Chromecast?</span></a></li>
+</ul>
+</li>
+</ul>
+
+<h1><span id="VideoLAN">VideoLAN</h1>
+
+<h2><span id="WhatIs">What is VideoLAN? What is the VideoLAN Team?</h2>
 
 <p>VideoLAN is a <a href="../videolan">non-profit organization</a>, that produces and distributes
 free and open source software for video and multimedia purpose,
@@ -29,11 +69,11 @@ released under Open Source licenses.</p>
 but is now a worldwide project with developers from everywhere
 and dozens of millions of people using VideoLAN's software.</p>
 
-<h2>What is the legal structure of VideoLAN?</h2>
+<h2<span id="LegalStructure">>What is the legal structure of VideoLAN?</h2>
 <p>VideoLAN is a French non-profit organization.<br />
 VideoLAN has its own bank account and is fully responsible of this website.</p>
 
-<h2>What are the differences between VLC, VLC media player, VideoLAN Client, VideoLAN Server and VideoLAN?</h2>
+<h2><span id="Differences">What are the differences between VLC, VLC media player, VideoLAN Client, VideoLAN Server and VideoLAN?</h2>
 <p><b>VLC</b> is the official name of VideoLAN's main product, often named VLC.
 VideoLAN Client is an ancient name of this product.</p>
 
@@ -41,23 +81,23 @@ VideoLAN Client is an ancient name of this product.</p>
 
 <p>VideoLAN is <strong>not</strong> a software, see 1.1 of this FAQ.</p>
 
-<h2>How can I contribute to VideoLAN?</h2>
+<h2><span id="Contribute">How can I contribute to VideoLAN?</h2>
 <p>You might either contribute time, material, or money. You <strong>don't need</strong> to be a programmer to help us.
 We are also always needing some translators and designers.<br />
 You will find all information you need on the
 <a href="/contribute.html">contribution page</a>.</p>
 
-<h2>Why are your mailing-lists and your website in english?</h2>
+<h2><span id="Mailinglist">Why are your mailing-lists and your website in english?</h2>
 <p>VideoLAN's developers come from all around the world and english is the only language they can use to communicate together.
  Although great care is given to the translation of VLC in various languages, maintaining translations of our website costs too much more time than we can afford.</p>
 
-<h2>What to do if I can't find an answer to my question here?</h2>
+<h2><span id="What">What should I do if I can't find an answer to my question here?</h2>
 <p>Read the other section of the <a href="/support/">Support center</a> </p>
 
 
-<h1>VLC media player</h1>
+<h1><span id="MediaPlayer">VLC media player</h1>
 
-<h2>Does VLC support DVDs from all regions?</h2>
+<h2><span id="DVDS">Does VLC support DVDs from all regions?</h2>
 <p>This mostly depends on your DVD drive.
 Testing it is usually the quickest way to find out.
 The problem is that a lot of newer drives are RPC2 drives these days.
@@ -71,7 +111,7 @@ So just pop the disc in your drive and try it out, while you get a coffee.
 RPC1 drives should 'always' work regardless of the regioncode.
 </p>
 
-<h2>Where does VLC store its config file?</h2>
+<h2><span id="Config">Where does VLC store its config file?</h2>
 <p>Currently, a config file is created on a per user basis (there is no global configuration file).
 If you modify the available options in VLC and save the new configuration,
 then a configuration file will be created in your user directory.
@@ -86,11 +126,11 @@ The precise location of this file depends on the Operating System you are runnin
 <li>BeOS: <code>config/settings/vlcrc</code></li>
 </ul>
 
-<h2>VLC has a strange behavior...</h2>
+<h2><span id="StrangeBehaviour">VLC has a strange behavior...</h2>
 <p> Please consider the <a href="https://wiki.videolan.org/Reset_Settings/">Reset preferences</a> section in our Wiki for a variety of possible solutions.<br />
 </p>
 
-<h2>Videos are too dark</h2>
+<h2><span id="TooDark">Videos are too dark</h2>
 <p>Often this is caused by video hardware overlay problems:
 </p>
 <ul>
@@ -100,7 +140,7 @@ and try to increase the Gamma value in the &quot;Video&quot; tab.</li>
 If the videos now play with proper lighting, then deinstalling and reinstalling the video drivers might help.</li>
 </ul>
 
-<h2>I cannot read DVDs!</h2>
+<h2><span id="CannotDVD">I cannot read DVDs!</h2>
 <p>Here are a few things to check:</p>
 <ul>
 <li>If you are on Linux/Unix, did you install the '''''libdvdcss''''' package?</li>
@@ -110,7 +150,7 @@ where <code>/dev/dvd</code> is the device corresponding to your DVD drive.
 </li>
 </ul>
 
-<h2>Video is choppy</h2>
+<h2><span id="Choppy">Video is choppy</h2>
 <p>Your system might be too slow to decode all pictures. It might be that your CPU basically is not fast enough. It can also be that the subsystem is misconfigured/misdriven, this happens for example under Redhat Linux. Here are some elements to improve speed:</p>
 <ul>
 <li>Turn on DMA on your DVD device, for instance:
@@ -130,7 +170,7 @@ If supported, check that the xvideo plug-in is effectively used with:
 
 <p>Framedropping behaviour can be configured in the Video preferences of VLC.</p>
 
-<h2>Audio and video are out of sync</h2>
+<h2><span id="OutOfSync">Audio and video are out of sync</h2>
 <p>Try using another audio output plugin and, under Unix, kill <code>esd</code>, <code>artsd</code> or <code>pulseaudio</code> if they are running.
 If the problem is due to the input file, have a look at the &quot;Audio desynchronisation compensation&quot; option.
 </p>
@@ -145,12 +185,12 @@ Please, for the clueless people arguing that it doesn't work, include the inform
 
 <span style="font-size: x-small">Please do put this tag into the appropiate location or remove it once it is a documented feature inside the software, agreed?</span></div>
 
-<h2>VLC crashes.</h2>
+<h2><span id="Crash">VLC crashes</h2>
 <p>Increase the verbosity level (either in the preferences or with a <code>-vv</code> command line option) and look at the debug messages (in the terminal or in the Messages window).</p>
 
 <p>If you are convinced that it is a bug in VLC, have a look at the <a href="http://www.videolan.org/support/bug-reporting.html">bug reporting page</a>.</p>
 
-<h2>How can I take screenshots?</h2>
+<h2><span id="Screenshot">How can I take screenshots?</h2>
 <p>To take a snapshot of the video displayed by VLC, you just need to press the pre-defined snapshot hotkey:</p>
 <ul>
 <li>Windows / Linux / Unix: <strong>Ctrl+Alt+S</strong></li>
@@ -164,7 +204,7 @@ Please, for the clueless people arguing that it doesn't work, include the inform
 <p>To change the snapshot format or directory, go to <em>Preferences -&gt; Video</em>.
 </p>
 
-<h2>Where are my screenshots?</h2>
+<h2><span id="WhereScreenshots">Where are my screenshots?</h2>
 <p>If you haven't changed the snapshot directory in your preferences, your screenshots should go to:</p>
 <ul>
 <li>Windows: <code>My Documents\My Pictures\</code></li>
@@ -174,10 +214,10 @@ Please, for the clueless people arguing that it doesn't work, include the inform
 <p>To change it, go to <em>Preferences -&gt; Video -&gt; Video snapshot directory</em>.
 </p>
 
-<h2>My file doesn't seem to work!</h2>
+<h2><span id="DoesntWork">My file doesn't seem to work!</h2>
 <p>Are you sure VLC supports the file? Try checking the <a href="http://www.videolan.org/vlc/features.html">features page</a>. If it supported and you compiled VLC yourself, check if you have downloaded and installed all the codecs correctly. If it is not supported, then you are out of luck for now.</p>
 
-<h2>VLC doesn't display all subtitles</h2>
+<h2><span id="Subtitles">VLC doesn't display all subtitles</h2>
 <p>If VLC has autodetected your subtitles file, or if you opened it manually, but VLC only diplays some subtitles from time to time, you will need to change the subtitles file encoding.</p>
 
 <p>Go to <em>Preferences -&gt; Input / Codecs -&gt; Other codecs -&gt; Subtitles</em>, and set <em>Subtitle text encoding</em> to the right one.
@@ -189,7 +229,9 @@ Please, for the clueless people arguing that it doesn't work, include the inform
 Also, you might need to disable the &quot;YUV -&gt; RGB&quot; checkbox in <em>DirectX</em> section of <em>Preferences -&gt; Video -&gt; Output modules</em>.
 </p>
 
-<h2>Why is my ip permanently banned from the forum?</h2>
+<h1><span id="Support">Support and feature requests</h1>
+
+<h2><span id="Banned">Why am I permanently banned from the forum?</h2>
 
 <p>Probably your IP address was banned, but it is not necessarily your fault.<br />
 - If you are at home, your dynamic IP was probably previously abused by a spammer.<br />
@@ -197,12 +239,12 @@ Also, you might need to disable the &quot;YUV -&gt; RGB&quot; checkbox in <em>Di
 Try to go through the list of blacklists presented to you on https://forum.videolan.org/403.html and remove yourself, if you are listed.
 Finally, don't forget to ensure that your computer and web browser are free of trojans/malware, and failing everything wait for a new dynamic IP.<p>
 
-<h2>Will VLC be available on Windows Phone?</h2>
+<h2><span id="WindowsPhone">Will VLC be available on Windows Phone?</h2>
 
 <p>We're currently working on it. Since development for Windows Phone is harder in comparison to other mobile platforms, we can't estimate the time needed to release a version for Windows Phone. 
 The latest updates can be received by following us on twitter or subscribing to our RSS feed.</p>
 
-<h2>When will VLC be able to stream to Chromecast?</h2>
+<h2><span id="ChromeCast">When will VLC be able to stream to Chromecast?</h2>
 
 <p>We're currently working on it but can't give a release date. </p>
 

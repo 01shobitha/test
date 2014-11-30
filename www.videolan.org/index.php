34b565efbf69826220c631bc8a0089e89beb8eb3
Christoph Miebach <christoph.miebach@web.de>
2014-11-30 16:57:01 +0000

    fixing RTL issue in an inconsistant way

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -174,7 +174,7 @@
     <?php /* Social and other BS */ ?>
     <h1 style="position:relative">
     <?php echo _("Social media"); ?>
-    <span class="ltrFloatRight" style="padding-right: 20px;">
+    <span class="ltrFloatRight" <?php if ( isRTL( $language ) ) { echo ' style="padding-left: 20px;"'; } else { echo ' style="padding-right: 20px;"'; } ?>>
         <a href="http://www.twitter.com/videolan"><?php image( 'twitter.png', "Twitter" );?></a>
         <a href="http://www.facebook.com/vlc.media.player"><?php image( 'facebook.png', "Facebook" );?></a>
     </span>

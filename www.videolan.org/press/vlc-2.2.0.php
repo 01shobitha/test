50ea14b0fd05908e5367470276f6d35c254e22f0
Jean-Baptiste Kempf <jb@videolan.org>
2015-02-27 11:27:05 +0000

    Add some BS press release

---
+++ www.videolan.org/press/vlc-2.2.0.php
@@ -0,0 +1,91 @@
+<?php
+   $title = "VideoLAN - Press Release - VLC 2.2.0 releases";
+   $lang = "en";
+   $menu = array( "vlc" );
+   require($_SERVER["DOCUMENT_ROOT"]."include/header.php");
+?>
+
+
+<h1>VLC media player 2.2.0 and VLC mobile port releases</h1>
+<br />
+
+<div class="longtext">
+ <div class="date"><b>Paris, Feb 27 2015</b></div>
+
+<p>
+VideoLAN and the VLC development teams are proud to publish a major new release of VLC.<br
+/><br
+/>The&nbsp; 2.2.0 release of VLC media player for the desktop is coordinated, for&nbsp; the first time ever, with releases on iOS, Android (including Android&nbsp; TV), WinRT (Windows RT and x86) and Windows Phone. <i>VLC runs everywhere!</i><br
+/><br
+/>Those&nbsp; releases are the result of more than one year of volunteer work, add&nbsp; numerous new features, fix more than one thousand bugs and improves&nbsp; the scope of the formats supported.<br
+/>The releases on all the platforms share the same core to support video and audio formats, named libVLC:<i> VLC plays everything!</i><br
+/><br
+/>So, what's new in VLC 2.2.0, codename WeatherWax?<br
+/><ul><li>Fight&nbsp; the popular vertical video syndrome! VLC automatically detects rotated&nbsp; videos and rotates them using hardware acceleration (on compatible&nbsp; platforms)!</li
+><li>This is supported for MP4/MOV, MKV and raw H264.</li
+><li>Resume playback&nbsp; where you left off. Supported on all the mobile versions of VLC for&nbsp; quite some time, it is now available on the desktop.</li
+><li>Vastly improved support for UltraHD video codecs like VP9 and H265, including encoding.</li
+><li>New&nbsp; hardware acceleration mechanism, GPU 0-copy decoding, faster and&nbsp; implementations for Linux, Android, and Raspberry Pi. (Other OSes will&nbsp; have it in 3.0.0)</li
+><li>Extensions: supported since a long time, we now feature an in-app downloader for the desktop, like Firefox</li
+><li>Subtitles downloading extension</li
+><li>Compatibility with a very large number of unusual codecs</li
+><li><br/></li
+><li>Vastly improved compatibility for problematic files in Ogg, MP4, and WMV.</li
+><li>Support for Digital Cinema Package to play native movie theater formats.</li
+><li><br/></li
+><li>Experimental support of Interactive Menus of BluRays: BD-J</li
+><li>On OS X, we've updated the interface for Yosemite compatibility.</li
+><li>On Android, we rewrote most of the UI to match Google Material Design.</li
+><li>This is the first public beta releases for Windows Phone, Windows RT and Android TV.</li
+><li>It is also the first non-beta release on Android.</li
+><li><br/></li></ul
+>This&nbsp; is made possible through a lot of improvements of the VLC engine, the&nbsp; libVLC library, which starts showing its maturity and solidity.<br
+/><br
+/>Therefore, today, we release:<br
+/><ul><li>VLC media player 2.2.0</li
+><li>VLC for iOS 2.4.1</li
+><li>VLC for Android 1.1.0</li
+><li>VLC for Android TV 1.1.0</li
+><li>VLC for WinRT 1.1.0</li
+><li>VLC for Windows RT 1.1.0 Beta</li
+><li>VLC for Windows Phone 1.1.0 Beta</li></ul
+>We hope that you like our new release as much as we do and we are looking forward to answer any question you might have.<br
+/><br
+/>
+<h2>Quotes</h2>
+Jean-Baptiste Kempf, president of the VideoLAN non-profit:<br
+/>"I'm&nbsp; very happy about those releases. Moving VLC to the mobile world was&nbsp; difficult, but the difficult is done. VLC runs everywhere, plays&nbsp; everything. We're working on many new features for VLC 3.0.0 to finish&nbsp; what we've started here."<br
+/><br
+/>"We're quite happy to see that libVLC&nbsp; and the libVLC SDK on all platforms are now demonstrating that this&nbsp; framework is now ready for production."<br
+/><br
+/>Felix Paul Kühne, secretary of the VideoLAN non-profit and iOS Lead Developer<br
+/>"Synchronizing&nbsp; the release leads to an unprecedented level of run-time stability and&nbsp; future innovation. We are excited about sharing further results from&nbsp; that work later this year."<br
+/><br
+/>
+<h2>Notes on 3.0.0</h2>
+/>So&nbsp; far, the development on VLC 3.0.0, which will be released later this&nbsp; year, brings hardware decoding and acceleration to more platforms, a&nbsp; better support for mp4, adaptive streaming and TS streams and a partial&nbsp; ChromeCast integration.<br
+/><br
+      />
+<h2>About VideoLAN</h2>
+/>VideoLAN is a non-profit organization,&nbsp; with no employees, that supports the development of free and open source&nbsp; multimedia solutions.<br
+/>There is no business model under VLC usage,&nbsp; and VLC is completely free: we don't use any ads, any spyware or other&nbsp; mis-features, that could be deceptive to the users.<br
+/>
+<h2>Links</h2>
+VLC <a href="http://www.videolan.org/vlc/">http://www.videolan.org/vlc/</a><br
+/>VLC for Android and Android TV:<br
+/><a href="https://play.google.com/store/apps/details?id=org.videolan.vlc">https://play.google.com/store/apps/details?id=org.videolan.vlc</a><br
+/>VLC for iOS:<br
+/><a href="https://itunes.apple.com/app/vlc-ios/id650377962">https://itunes.apple.com/app/vlc-ios/id650377962</a><br
+/>VLC for Windows Phone:<br
+/>link to come<br
+/>VLC for Windows RT:<br
+/><a href="http://apps.microsoft.com/windows/en-us/app/vlc-for-windows-8/c527ff2d-b5d0-45b6-bfc3-92fb7357ef72">http://apps.microsoft.com/windows/en-us/app/vlc-for-windows-8/c527ff2d-b5d0-45b6-bfc3-92fb7357ef72</a><br
+/><br
+     />
+<h2>Press Contact</h2>
+press@videolan.org<br
+/><br
+/>
+</div>
+<?php footer('$Id:$'); ?>
+

25a530996497934c56c009cb2299d34a786491ec
Jean-Baptiste Kempf <jb@videolan.org>
2015-04-14 21:59:56 +0000

    Update privacy for Android and iOS

--- www.videolan.org/vlc/privacy.php
+++ www.videolan.org/vlc/privacy.php
@@ -7,17 +7,17 @@
 <div>
 <h1>Privacy</h1>
 
-<h2>VLC for WinRT</h2>
+<h2>VLC Mobile</h2>
 <p>
-This privacy policy cover the use of the 'VLC for WinRT' application.  It may not be applicable
-to other software produced or released by VideoLAN.
+This privacy policy cover the use of the 'VLC for Android', 'VLC for WinRT' and 'VLC for iOS' applications.<br />
+It may not be applicable to other software produced or released by VideoLAN.
 </p>
 <p>
 VideoLAN does not collect any statistics, personal information, or analytics from our users,
-other than built in mechanisms that are present for all applications in the 'Windows Store'.
+other than built in mechanisms that are present for all the mobile applications in the main distribution channels.
 </p>
 <p>
-The 'VLC for WinRT' application does allow for videos to be played via various network
+The mobile versions of 'VLC' do allow for videos to be played via various network
 transports.  Cookies and authentication credentials are not stored at any point.
 </p>
 

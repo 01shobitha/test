190ffcd089c33ca3e6efbccca7cfae5aff43508b
Felix Paul Kühne <fkuehne@videolan.org>
2015-05-07 14:35:00 +0000

    update mac download page

--- www.videolan.org/vlc/download-macosx.php
+++ www.videolan.org/vlc/download-macosx.php
@@ -1,18 +1,17 @@
 <?php
    $language = "";
-   $title = "Download official VLC media player for Mac OS X";
+   $title = "Official Download of VLC media player for OS X";
    $additional_css = array("/style/countries.css","/style/panels.css");
    require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
-<h1 class="bigtitle"> VLC media player for Mac OS X </h1>
+<h1 class="bigtitle"> VLC media player for OS X </h1>
 
 <div id="left">
 
 <?php
-$vlc_version = "1.0.5";
 $vlc_version_old = "2.0.10";
 $vlc_version_intel = "2.2.1";
 ?>
@@ -21,7 +20,7 @@ $vlc_version_intel = "2.2.1";
 
 <?php
 pkgitem_sf( "VLC for OS X $vlc_version_intel",
-    "$vlc_version_intel/macosx","vlc-$vlc_version_intel.dmg",        "vlc", "64 bits dmg package", "4b9ff700342cf783af8d6b0a4accf26b");
+    "$vlc_version_intel/macosx","vlc-$vlc_version_intel.dmg",        "vlc", "64 bit package", "4b9ff700342cf783af8d6b0a4accf26b");
 ?>
 
 <div class="clearme"> </div>
@@ -29,11 +28,11 @@ pkgitem_sf( "VLC for OS X $vlc_version_intel",
 <br />
 <br />
 
-<h2>Web browser plugins (release <?php echo $vlc_version_intel; ?>)</h2>
+<h2>Web browser plugins</h2>
 
 <?php
 pkgitem_sf( "VLC for OS X web plugin $vlc_version_intel",
-    "$vlc_version_intel/macosx","VLC-webplugin-$vlc_version_intel.dmg",        "vlc", "32 & 64 bits Universal Binary dmg package", "1821442e0d18740cb14680f9341945b6");
+    "$vlc_version_intel/macosx","VLC-webplugin-$vlc_version_intel.dmg",        "vlc", "32 & 64 bit Universal Binary package", "1821442e0d18740cb14680f9341945b6");
     ?>
 
 <div class="clearme"> </div>
@@ -41,12 +40,14 @@ pkgitem_sf( "VLC for OS X web plugin $vlc_version_intel",
 <br />
 <br />
 
-<h2>Download VLC for Mac OS X 10.5 Leopard (release <?php echo $vlc_version_old; ?>)</h2>
+<h2>Last version of VLC for Mac OS X 10.5 Leopard (release <?php echo $vlc_version_old; ?>)</h2>
+<p>Note that this version is <b>no longer maintained</b> and will not receive any updates.</p>
 <?php
-pkgitem_sf( "32 bits Intel Mac",
+pkgitem_sf( "Intel (32 bit)",
     "$vlc_version_old/macosx","vlc-$vlc_version_old-intel32.dmg","vlc", "dmg package", "5b3aedda97e06500c7954c56f0902cb3");
-pkgitem_sf( "PowerPC Mac",
+pkgitem_sf( "PowerPC",
     "$vlc_version_old/macosx","vlc-$vlc_version_old-powerpc.dmg","vlc", "dmg package", "4e4c956647ba6ed948f9fd89f250c999"); ?>
+<p>Need help to download the correct package for your Mac? <a href="https://support.apple.com/en-us/HT201580">Click here</a>.</p>
 
 
 <div class="clearme"> </div>
@@ -55,8 +56,8 @@ pkgitem_sf( "PowerPC Mac",
 <br />
 
 <div class="clearme"> </div>
-<h2>Download older versions of VLC media player</h2>
-<p>Use VLC <b>0.9.10</b> for 10.4 Tiger. <b>Mac OS X 10.4.7</b> or later, is <b>required</b>.</p>
+<h2>Download previous versions of VLC for legacy versions of OS X</h2>
+<p>Use VLC <b>0.9.10</b> for 10.4 Tiger. <b>Mac OS X 10.4.7</b> or later is <b>required</b>.</p>
 <p>Use VLC <b>0.8.6i</b> for 10.3 Panther.</p>
 <p>Use VLC <b>0.8.4a</b> for 10.2 Jaguar.</p>
 <p>Use VLC <b>0.7.0</b> for 10.1 Puma .</p>

2c7968f6efa1a7a94157cb6e9abeb61605739b21
Felix Paul Kühne <fkuehne@videolan.org>
2015-05-07 14:42:48 +0000

    minor layout tweak

--- www.videolan.org/vlc/download-macosx.php
+++ www.videolan.org/vlc/download-macosx.php
@@ -42,12 +42,12 @@ pkgitem_sf( "VLC for OS X web plugin $vlc_version_intel",
 
 <h2>Last version of VLC for Mac OS X 10.5 Leopard (release <?php echo $vlc_version_old; ?>)</h2>
 <p>Note that this version is <b>no longer maintained</b> and will not receive any updates.</p>
+<p>Need help to download the correct package for your Mac? <a href="https://support.apple.com/en-us/HT201580">Click here</a>.</p>
 <?php
 pkgitem_sf( "Intel (32 bit)",
     "$vlc_version_old/macosx","vlc-$vlc_version_old-intel32.dmg","vlc", "dmg package", "5b3aedda97e06500c7954c56f0902cb3");
 pkgitem_sf( "PowerPC",
     "$vlc_version_old/macosx","vlc-$vlc_version_old-powerpc.dmg","vlc", "dmg package", "4e4c956647ba6ed948f9fd89f250c999"); ?>
-<p>Need help to download the correct package for your Mac? <a href="https://support.apple.com/en-us/HT201580">Click here</a>.</p>
 
 
 <div class="clearme"> </div>
@@ -56,7 +56,7 @@ pkgitem_sf( "PowerPC",
 <br />
 
 <div class="clearme"> </div>
-<h2>Download previous versions of VLC for legacy versions of OS X</h2>
+<h2>Download previous versions of VLC for legacy versions of Mac OS X</h2>
 <p>Use VLC <b>0.9.10</b> for 10.4 Tiger. <b>Mac OS X 10.4.7</b> or later is <b>required</b>.</p>
 <p>Use VLC <b>0.8.6i</b> for 10.3 Panther.</p>
 <p>Use VLC <b>0.8.4a</b> for 10.2 Jaguar.</p>

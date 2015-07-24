0f2fb46729af667d74439675b9da130791bc8c7c
Jean-Baptiste Kempf <jb@videolan.org>
2015-07-24 15:19:04 +0000

    Remove ugly BR

--- www.videolan.org/videolan/events/vdd15/index.php
+++ www.videolan.org/videolan/events/vdd15/index.php
@@ -7,8 +7,7 @@
 
 <p style="margin-top: -15px"><?php image( "events/vdd14.jpg" , "VDD14 Group picture"); ?></p>
 
-<h1>VideoLAN Dev Days 2015</h1><br/>
-
+<h1>VideoLAN Dev Days 2015</h1>
 <h3>The Open Multimedia Conference that frees the cone in you!</h3>
 
 <div id="sidebar">

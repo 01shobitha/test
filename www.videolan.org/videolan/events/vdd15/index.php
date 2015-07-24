7c07fca19f82b5a9762823eeb6635583882dae18
Felix Paul Kühne <fkuehne@videolan.org>
2015-07-24 13:42:44 +0000

    add VDD15 stub

---
+++ www.videolan.org/videolan/events/vdd15/index.php
@@ -0,0 +1,78 @@
+<?php
+   $title = "VideoLAN Dev Days 2015, September 19 - 20, 2015";
+   $additional_css = array("/style/panels.css");
+   $body_color = "green";
+   require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<h1>VideoLAN Dev Days 2015</h1><br/>
+<h3>The Open Multimedia Conference that frees the cone in you!</h3>
+
+<p><?php image( "events/vdd14.jpg" , "VDD14 Group picture"); ?></p>
+
+<div id="sidebar">
+<?php panel_start( "green" ); ?>
+<h2>Sponsors</h2>
+<a href="/videolan/"><?php image( "logoGrey.png" , "VideoLAN association"); ?></a>
+<br />
+<p style="text-align: center"><a href="http://videolabs.io">Videolabs SAS</p>
+<br />
+<p style="text-align: center"><a href=/contact.html>Your Name Here</p>
+<h2>Host</h2>
+<p style="text-align: center">To be announced</a></p>
+<?php panel_end(); ?>
+
+<?php panel_start( "blue" ); ?>
+<h2>Register!</h2>
+<b>Registration will open shortly</b>
+<!--<b><a href="http://goo.gl/mUEER">Register <strong>now!</strong></a></b>-->
+<?php panel_end(); ?>
+
+</div>
+
+<p><b>The <a href="/videolan/">VideoLAN non-profit organisation</a> is happy to
+invite you to the multimedia open-source event of the end of summer!</b></p><br/>
+
+<p>For its <b>seventh edition</b>, people from the VideoLAN and open source multimedia communities will meet in
+ <strong>Paris, France</strong> to gather, work, discuss and meet, on the <strong>19th and 20th of September 2015</strong>.</p>
+<p>This event is focused on technical discussions and decisions about <b>multimedia</b>, and targets a technical crowd.</p>
+<p>Like in previous years, an important number of developers from <a href="/developers/x264.html">x264</a>, <a href="http://www.ffmpeg.org/">FFmpeg</a>, <a href="http://libav.org/">Libav</a> and KDE will join us as well as major industry partners!
+Developers, designers and anonymous people around <a href="/vlc/">VLC</a>, all of its ports to desktop, web browser environments and mobile operating systems, <a href="/projects/dvblast.html">DVBlast</a> or other multimedia projects will be there too.</p>
+
+<h1>Who can come? </h1>
+<p><strong>Anyone</strong> who cares about open source multimedia technologies and development.<br />
+ If you are not a technical person, but an enthusiast, we recommend you to come to the less technical events, like in the evenings.</p>
+
+<h1>Where? </h1>
+<p>The venue will be announced at a later point.
+
+<h1>Cost and sponsorship </h1>
+<p>The cost for attendance is <b>free</b>.</p>
+<p>Like previous years, active developers can get a full sponsorship covering travel costs. We will also provide accomodation.</p>
+
+<h1>Schedule</h1>
+<div id="schedule" style="margin: 10px 25px 0px; padding: 0 20px; border: 1px outset #4d4d4d; width: 600px;">
+
+<h2>Friday 18th September</h2>
+<p>This year we'll do a full community bonding day at <a href="http://www.disneylandparis.com">Disneyland Paris</a>!<br/>
+The VideoLAN organization will pay for the amusement park tickets.<br/>
+<b>To participate you'll need to be in Paris Friday at 8am!</b> Please plan an extra night if you can't make it.</p>
+<h2>Saturday / Sunday</h2>
+<p>To be announced.</p>
+
+<h2>Café Event</h2>
+<p>On <strong>Friday 18th September 2015 at 20h30</strong>, people are welcome to come and
+share a few good drinks.</p>
+</div>
+
+<h1>Registration</h1>
+<p>Registration will open shortly.</p>
+<!--<p>Please fill out the <b><a href="http://goo.gl/mUEER">registration form</a></b>.</p>-->
+
+<h1><a name="location">Location</a> </h1>
+<p>To be announced.</p>
+
+<!--<h2>Schedule</h2> -->
+<!-- You can find the full final schedule <a href="/videolan/events/vdd09.pdf">here</a>. -->
+
+<?php footer('$Id: index.php 5400 2009-07-19 15:37:21Z jb $'); ?>

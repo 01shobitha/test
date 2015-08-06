8f69034dfbd2c7eec119902d7e0693bd61e95142
Felix Paul Kühne <fkuehne@videolan.org>
2015-08-06 09:58:03 +0000

    iOS 2.6.4

--- www.videolan.org/vlc/download-ios.php
+++ www.videolan.org/vlc/download-ios.php
@@ -8,7 +8,7 @@
 
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 
-   $version = "2.6.3";
+   $version = "2.6.4";
 ?>
 
     <h1 class="bigtitle"> VLC for iOS <?php echo $version; ?></h1>

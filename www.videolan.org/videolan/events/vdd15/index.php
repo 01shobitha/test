d1e7c0bfec5da3a7d377a505f5961ee9c8e9ca80
Felix Paul Kühne <fkuehne@videolan.org>
2015-09-16 10:27:13 +0000

    VDD: fix schedule colors

--- www.videolan.org/videolan/events/vdd15/index.php
+++ www.videolan.org/videolan/events/vdd15/index.php
@@ -70,64 +70,64 @@ share a few good drinks. The location will be announced shortly.</p>
 <p>
 <table style="width:100%">
   <tr>
-    <td style="width:20%"><b>08:45 - 09:30</b></td>
-    <td>Registration and Breakfast</td>
+    <td style="width:20%; color: #4D4D4D"><b>08:45 - 09:30</b></td>
+    <td style="color: #4D4D4D">Registration and Breakfast</td>
   </tr>
   <tr>
     <td style="width:20%">&#8193;</td>
     <td>&#8193; </td>
   </tr>
   <tr>
-    <td style="width:20%"><b>09:30 - 09:45</b></td>
-    <td><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>09:30 - 09:45</b></td>
+    <td style="color: #4D4D4D"><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>09:45 - 10:20</b></td>
-    <td><b>Daala</b> by Timothy Terriberry <i>(Mozilla)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>09:45 - 10:20</b></td>
+    <td style="color: #4D4D4D"><b>Daala</b> by Timothy Terriberry <i>(Mozilla)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>10:20 - 10:55</b></td>
-    <td><b>A new AAC free and open-source encoder</b> by Rostislav Pehlivanov <i>(FFmpeg)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>10:20 - 10:55</b></td>
+    <td style="color: #4D4D4D"><b>A new AAC free and open-source encoder</b> by Rostislav Pehlivanov <i>(FFmpeg)</i></td>
   </tr>
   <tr>
     <td style="width:20%">&#8193;</td>
     <td>&#8193;</td>
   </tr>
   <tr>
-    <td style="width:20%"><b>11:05 - 11:40</b></td>
-    <td><b>New video compression techniques under consideration for VP10</b> by Alex Converse <i>(Google)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>11:05 - 11:40</b></td>
+    <td style="color: #4D4D4D"><b>New video compression techniques under consideration for VP10</b> by Alex Converse <i>(Google)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>11:40 - 12:10</b></td>
-    <td><b>An update to x265</b> by Deepthi Nandakumar <i>(MCW)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>11:40 - 12:10</b></td>
+    <td style="color: #4D4D4D"><b>An update to x265</b> by Deepthi Nandakumar <i>(MCW)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>12:10 - 12:40</b></td>
-    <td><b>VLC 3.0</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>12:10 - 12:40</b></td>
+    <td style="color: #4D4D4D"><b>VLC 3.0</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>12:40 - 12:45</b></td>
-    <td><b>Spice</b> by Julien Navas <i>(Vodkaster)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>12:40 - 12:45</b></td>
+    <td style="color: #4D4D4D"><b>Spice</b> by Julien Navas <i>(Vodkaster)</i></td>
   </tr>
   <tr>
     <td style="width:20%">&#8193;</td>
     <td>&#8193; </td>
   </tr>
   <tr>
-    <td style="width:20%"><b>14:00 - 14:40</b></td>
-    <td><b>The Thor Codec</b> by Thomas Davies <i>(Cisco)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>14:00 - 14:40</b></td>
+    <td style="color: #4D4D4D"><b>The Thor Codec</b> by Thomas Davies <i>(Cisco)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>14:40 - 15:00</b></td>
-    <td><b>How to optimize press coverage for FLOSS projects</b> by Sebastian Grüner <i>(golem.de)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>14:40 - 15:00</b></td>
+    <td style="color: #4D4D4D"><b>How to optimize press coverage for FLOSS projects</b> by Sebastian Grüner <i>(golem.de)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>15:00 - 15:20</b></td>
-    <td><b>A faster VP9 decoder</b> by Ronald Bultje</td>
+    <td style="width:20%; color: #4D4D4D"><b>15:00 - 15:20</b></td>
+    <td style="color: #4D4D4D"><b>A faster VP9 decoder</b> by Ronald Bultje</td>
   </tr>
   <tr>
-    <td style="width:20%"><b>15:30 - 18:00</b></td>
-    <td><b>Break-out sessions</b></td>
+    <td style="width:20%; color: #4D4D4D"><b>15:30 - 18:00</b></td>
+    <td style="color: #4D4D4D"><b>Break-out sessions</b></td>
   </tr>
 </table>
 </p>
@@ -136,40 +136,40 @@ share a few good drinks. The location will be announced shortly.</p>
 <p>
 <table style="width:100%">
   <tr>
-    <td style="width:20%"><b>09:30 - 10:00</b></td>
-    <td>Breakfast</td>
+    <td style="width:20%; color: #4D4D4D"><b>09:30 - 10:00</b></td>
+    <td style="color: #4D4D4D">Breakfast</td>
   </tr>
   <tr>
     <td style="width:20%">&#8193;</td>
     <td>&#8193; </td>
   </tr>
   <tr>
-    <td style="width:20%"><b>10:00 - 10:10</b></td>
-    <td><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>10:00 - 10:10</b></td>
+    <td style="color: #4D4D4D"><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>10:15 - 10:30</b></td>
-    <td><b>FFmpeg and libav metrics</b> by Ronald Bultje</td>
+    <td style="width:20%; color: #4D4D4D"><b>10:15 - 10:30</b></td>
+    <td style="color: #4D4D4D"><b>FFmpeg and libav metrics</b> by Ronald Bultje</td>
   </tr>
   <tr>
-    <td style="width:20%"><b>10:30 - 12:30</b></td>
-    <td><b>Break-out sessions</b></td>
+    <td style="width:20%; color: #4D4D4D"><b>10:30 - 12:30</b></td>
+    <td style="color: #4D4D4D"><b>Break-out sessions</b></td>
   </tr>
   <tr>
     <td style="width:20%">&#8193;</td>
     <td>&#8193;</td>
   </tr>
   <tr>
-    <td style="width:20%"><b>14:00 - 16:00</b></td>
-    <td><b>Break-out sessions</b></td>
+    <td style="width:20%; color: #4D4D4D"><b>14:00 - 16:00</b></td>
+    <td style="color: #4D4D4D"><b>Break-out sessions</b></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>16:00 - 16:15</b></td>
-    <td><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
+    <td style="width:20%; color: #4D4D4D"><b>16:00 - 16:15</b></td>
+    <td style="color: #4D4D4D"><b>Opening remarks</b> by Jean-Baptiste Kempf <i>(VideoLAN)</i></td>
   </tr>
   <tr>
-    <td style="width:20%"><b>16:15 - 18:00</b></td>
-    <td><b>Break-out sessions and after-hours</b></td>
+    <td style="width:20%; color: #4D4D4D"><b>16:15 - 18:00</b></td>
+    <td style="color: #4D4D4D"><b>Break-out sessions and after-hours</b></td>
   </tr>
 </table>
 </p>

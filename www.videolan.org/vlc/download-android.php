d11ab142d7f960fc24e02416a64b012a7f9ae067
Jean-Baptiste Kempf <jb@videolan.org>
2015-10-09 07:17:38 +0000

    VLC for Android 1.6.0

--- www.videolan.org/vlc/download-android.php
+++ www.videolan.org/vlc/download-android.php
@@ -4,7 +4,7 @@
    $menu = array( "vlc", "download" );
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 
-   $android_version = "1.4.1";
+   $android_version = "1.6.0";
 ?>
 
 <h1 class="bigtitle"> VLC media player for <a href="http://android.com/">Android</a>&trade;</h1>

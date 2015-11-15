9f7979b7fd4f0470d3bff11f975637c14e8532be
Felix Paul Kühne <fkuehne@videolan.org>
2015-11-15 14:25:17 +0000

    Updated privacy policy for mobile and embedded apps

--- www.videolan.org/vlc/privacy.php
+++ www.videolan.org/vlc/privacy.php
@@ -9,16 +9,14 @@
 
 <h2>VLC Mobile</h2>
 <p>
-This privacy policy cover the use of the 'VLC for Android', 'VLC for WinRT' and 'VLC for iOS' applications.<br />
+This privacy policy covers the use of the 'VLC for Android', 'VLC for WinRT' and 'VLC for iOS' / 'VLC for tvOS' applications.<br />
 It may not be applicable to other software produced or released by VideoLAN.
 </p>
 <p>
-VideoLAN does not collect any statistics, personal information, or analytics from our users,
-other than built in mechanisms that are present for all the mobile applications in the main distribution channels.
+VideoLAN does not collect any statistics, personal information, or analytics from our users, other than built in mechanisms that are present for all the mobile or embedded applications in their respective main distribution channels.
 </p>
 <p>
-The mobile versions of 'VLC' do allow for videos to be played via various network
-transports.  Cookies and authentication credentials are not stored at any point.
+The mobile or embedded versions of 'VLC' do allow for videos to be played via various network transports. Cookies are not stored at any point. Authentication credentials can be stored optionally on the user's local device upon the user's explicit request. On iOS and tvOS, authentication credentials can be shared across devices using proprietary encryption mechanisms provided by the host operating system, dubbed iCloud Keychain. VideoLAN does not have access to any information stored within.
 </p>
 
 </div>

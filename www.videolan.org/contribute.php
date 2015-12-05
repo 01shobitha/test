3b62a0c217ce7e5a2a352e587ab185c348a82795
Christoph Miebach <christoph.miebach@web.de>
2015-12-05 15:06:11 +0000

    Don't list vlmc for translators

--- www.videolan.org/contribute.php
+++ www.videolan.org/contribute.php
@@ -32,7 +32,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
                             join the <a href="/support/">IRC channel</a> to get help on how to start coding.</li>
                         <li ><strong>Writers</strong>, to help improve the documentation.</li>
                         <li ><strong>Translators</strong>, to <a href="/developers/i18n/">translate</a>
-                         <a href="/vlc">VLC</a>, <a href="/vlmc/">VLMC</a> or other projects.</li>
+                         <a href="/vlc">VLC</a> or other projects.</li>
                         <li ><strong>Moderators &amp; Support</strong>, to answer on the <a href="http://forum.videolan.org">forums</a> and mailing lists.</li>
                         <li ><strong>Designers</strong>, for this website and VLC.</li>
                     </ul>

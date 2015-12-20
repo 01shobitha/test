046af59e7f3b55f3b0210e0c5ab56356005bd069
Felix Paul Kühne <fkuehne@videolan.org>
2015-12-20 16:40:52 +0000

    Holiday season has begun

--- www.videolan.org/vlc/index.php
+++ www.videolan.org/vlc/index.php
@@ -13,7 +13,8 @@
 ?>
 
         <div class="clearme" style='padding-top: 26px;'>
-            <?php image( 'largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'projectImg' ); ?>
+            <?php /*image( 'goodies/cone-eT5x.png', 'Large Orange VLC media player Traffic Cone Logo', 'projectImg' );*/ ?>
+            <img src='//images.videolan.org/images/goodies/cone-eT5x.png' alt='Large Orange VLC media player Traffic Cone Logo' class='projectImg' width='300' />
             <div class="ltrFloatLeft" style='width: 500px;'>
                 <h1 class='bigtitle'>VLC media player</h1>
                 <div class='projectDescription'>

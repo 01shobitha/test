f6e55b4b00c501174b7dd7d9716e5f9cef410711
Jean-Baptiste Kempf <jb@videolan.org>
2016-01-10 15:10:56 +0100

    Move 403 to the new design

--- www.videolan.org/403.php
+++ www.videolan.org/403.php
@@ -2,9 +2,10 @@
    $title = "403 forbidden";
    $menu = array( "vlc", "" );
    $body_color = "red";
+   $new_design = true;
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
-
+<div class="container">
 <h1 class="error">403 error: forbidden</h1>
 
 <p>Nice try :-)</p>
@@ -12,4 +13,5 @@
 <center>
   <?php image( "largeVLC.png", "VLC cone" ); ?>
 </center>
+</div>
 <?php footer('$Id$'); ?>

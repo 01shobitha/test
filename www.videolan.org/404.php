e1217c18cf68ee7591278fd3bd6505151021668d
David Loiret <loiret.d@gmail.com>
2016-01-11 20:52:47 +0100

    Move 404 to the new design
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- www.videolan.org/404.php
+++ www.videolan.org/404.php
@@ -2,24 +2,27 @@
    $title = "404 not found";
    $menu = array( "vlc", "" );
    $body_color = "red";
+   $new_design = true;
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<h1 class="error">404 error: page not found</h1>
+<div class="container">
+    <h1 class="error">404 error: page not found</h1>
 
-<p>Please check your links, or contact
- <a href="/contact.html">the VideoLAN team</a> if you think
-there should be something here. </p>
+    <p>Please check your links, or contact
+    <a href="/contact.html">the VideoLAN team</a> if you think
+    there should be something here. </p>
 
-<script type="text/javascript">
-<!--
-    if (document.referrer) {
-        document.write( "You've come from\n" + document.referrer);
-    }
--->
-</script>
+    <script type="text/javascript">
+    <!--
+        if (document.referrer) {
+            document.write( "You've come from\n" + document.referrer);
+        }
+    -->
+    </script>
 
-<center>
-  <?php image( "largeVLC.png", "VLC cone" ); ?>
-</center>
+    <center>
+    <?php image( "largeVLC.png", "VLC cone" ); ?>
+    </center>
+</div>
 <?php footer('$Id$'); ?>

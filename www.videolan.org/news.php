3a9fed01662b3488c8daff5cec14898e0e38b80a
David Loiret <loiret.d@gmail.com>
2016-01-13 21:10:57 +0100

    Move news page to the new design
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- www.videolan.org/news.php
+++ www.videolan.org/news.php
@@ -1,19 +1,21 @@
 <?php
    $title = "News";
    $lang = "en";
+   $new_design = true;
    $body_color = "green";
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
+<div class="container">
+    <h1 class="bigtitle"> News archive </h1>
 
-<h1 class="bigtitle"> News archive </h1>
+    <div class='longtext'>
 
-<div class='longtext'>
+    <?php
+    include($_SERVER["DOCUMENT_ROOT"]."/include/news.php");
+    shownews("full",0);
+    ?>
 
-<?php
-   include($_SERVER["DOCUMENT_ROOT"]."/include/news.php");
-   shownews("full",0);
-?>
-
+    </div>
 </div>
 <?php footer('$Id$'); ?>

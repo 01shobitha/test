b982dcbc720b65b95d0e1625167b359f7ea9fb0f
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-17 22:13:09 +0100

    Move Fedora to new design

--- www.videolan.org/vlc/download-fedora.php
+++ www.videolan.org/vlc/download-fedora.php
@@ -1,15 +1,58 @@
 <?php
    $title = "VLC media player for Fedora";
+   $new_design = true;
    $lang = "en";
    $menu = array( "vlc", "download" );
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css", "/style/panels.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
-   require($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
+<div class="container">
+	<?php
+	$screenshots = getScreenshots("linux");
+	$defaultDetail = getOS("linux");
+	?>
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <?php
+                echo '<div class="v-align carousel-padding-xs col-xs-12 col-sm-5 col-sm-offset-1 col-sm-pull-1"><div id="header-carousel">';
+                $first = true;
+                foreach($screenshots as $screenshot) {
+                    $src = $first ? $screenshot['src'] : '';
+                    $srcLazy = $first ? '' : $screenshot['src'];
+                    $dom  = '<div class="screenshot2">';
+                    $dom .= '<img src="'.$src.'" alt="'.$screenshot['name'].'" data-lazy="'.$srcLazy.'">';
+                    $dom .= '</div>';
+                    echo $dom;
+                    $first = false;
+                }
+                echo '</div></div>';
+            ?>
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLC media player for <a href="http://fedoraproject.org/">Fedora</a>
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <div id="fullwidth">
-
-
-<h1 class="bigtitle"> VLC media player for <a href="http://fedoraproject.org/">Fedora</a></h1>
 
 <p>Most of our packages for fedora can be found on RPM Fusion <a href="http://rpmfusion.org/">RPM Fusion</a><br />
 You can also use this repository from distribution derived from <a href="http://fedoraproject.org/">Fedora</a></p>
@@ -97,6 +140,7 @@ for FC 4 and 5 on <a href="http://freshrpms.net">FreshRPMs</a>.</p>
 <h3>Other versions - Search</h3>
 <p>Use <a href="http://rpm.pbone.net/">PBone.net</a> to search a lot of other versions of VLC for fedora or fedora compatible distributions.</p>
 
+</div>
 </div>
 
 <?php footer('$Id$'); ?>

345995f5960055726ffa3f5208d2bf2231a10851
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-17 16:10:56 +0100

    shuffle screenshots to have the list start with something less croweded and impossible to differenciate

--- www.videolan.org/include/os-specific.php
+++ www.videolan.org/include/os-specific.php
@@ -266,10 +266,6 @@ function getScreenshots($os) {
     $screenshots = array(
         "All"  => array(
             array(
-                "name"  => "VLC media player - Linux - Gnome",
-                "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/VLC_Gnome.png"
-            ),
-            array(
                 "name"  => "VLC media player - Windows 7 - Qt Interface",
                 "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/vlc_101_w7_2.jpg"
             ),
@@ -286,12 +282,20 @@ function getScreenshots($os) {
                 "src"   => "//images1.videolan.org/vlc/screenshots/ios2/ipadwhitelibrary-medium.jpg"
             ),
             array(
-                "name"  => "VLC media player - Windows Vista - Skins Interface",
-                "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/VLC_Goldneye.jpg"
+                "name"  => "VLC media player - Linux - Gnome",
+                "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/VLC_Gnome.png"
             ),
             array(
                 "name"  => "VLC on Apple TV",
                 "src"   => "//images1.videolan.org/vlc/screenshots/appletv/Apple-TV-device-playback-medium.jpg"
+            ),
+            array(
+                "name"  => "VLC on OS X",
+                "src"   => "//images1.videolan.org/vlc/screenshots/2.2.0/vlc-2.2-macosx-playback-dark.jpg"
+            ),
+            array(
+                "name"  => "VLC media player - Windows Vista - Skins Interface",
+                "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/VLC_Goldneye.jpg"
             ),
             array(
                 "name"  => "VLC media player - Windows 7 - Qt Interface",
@@ -299,15 +303,11 @@ function getScreenshots($os) {
             ),
             array(
                 "name"  => "VLC on OS X",
-                "src"   => "//images1.videolan.org/vlc/screenshots/2.2.0/vlc-2.2-macosx-playback-dark.jpg"
+                "src"   => "//images1.videolan.org/vlc/screenshots/2.2.0/vlc-2.2-macosx-audio-playback-dark.jpg"
             ),
             array(
                 "name"  => "VLC media player - Windows Vista - Qt Interface",
                 "src"   => "//images1.videolan.org/vlc/screenshots/1.0.0/VLC_Qt4.jpg"
-            ),
-            array(
-                "name"  => "VLC on OS X",
-                "src"   => "//images1.videolan.org/vlc/screenshots/2.2.0/vlc-2.2-macosx-audio-playback-dark.jpg"
             )
         ),
         "windows" => array(

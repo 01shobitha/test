6a6e1af642d106324c55795c36bfc76415ba1c24
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-18 01:31:38 +0100

    Move SuSE to new design

--- www.videolan.org/vlc/download-suse.php
+++ www.videolan.org/vlc/download-suse.php
@@ -1,61 +1,98 @@
 <?php
    $title = "VLC media player for openSUSE";
+   $new_design = true;
    $lang = "en";
-   $body_color = "green";
    $menu = array( "vlc", "download" );
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
+
+   $body_color = "green";
 ?>
 
-<?php function pkgitem($version,$name,$comment) { ?><li><a href="http://download.videolan.org/pub/videolan/<?php echo $version."/".$name; ?>"><code><?php echo $name; ?></code></a> <?php echo $comment; ?> </li><?php } ?>
+<div class="container">
+	<?php
+	$screenshots = getScreenshots("linux");
+	$defaultDetail = getOS("linux");
+	?>
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <?php
+                echo '<div class="v-align carousel-padding-xs col-xs-12 col-sm-5 col-sm-offset-1 col-sm-pull-1"><div id="header-carousel">';
+                $first = true;
+                foreach($screenshots as $screenshot) {
+                    $src = $first ? $screenshot['src'] : '';
+                    $srcLazy = $first ? '' : $screenshot['src'];
+                    $dom  = '<div class="screenshot2">';
+                    $dom .= '<img src="'.$src.'" alt="'.$screenshot['name'].'" data-lazy="'.$srcLazy.'">';
+                    $dom .= '</div>';
+                    echo $dom;
+                    $first = false;
+                }
+                echo '</div></div>';
+            ?>
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLC media player for <a href="http://www.opensuse.org/">openSUSE</a>
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
 
-<h1 class="bigtitle">VLC media player for <a href="http://www.opensuse.org/">openSUSE</a></h1>
+			    <div class="inner center-xs">
+			        <div class="btn-group">
+                        <div class="installbtn" style="float: left;">
+                          <a href="http://download.videolan.org/pub/vlc/SuSE/Leap_42.1/vlc.ymp">VLC for Leap 42.1</a>
+                        </div>
+                        <div class="installbtn" style="float: left;">
+                          <a href="http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/vlc.ymp">VLC for Tumbleweed</a>
+                        </div>
+                        <div class="installbtn" style="float: left;">
+                          <a href="http://download.videolan.org/pub/vlc/SuSE/13.2/vlc.ymp">VLC for 13.2</a>
+                        </div>
+                        <div class="installbtn" style="float: left;">
+                          <a href="http://download.videolan.org/pub/vlc/SuSE/13.1/vlc.ymp">VLC for 13.1</a>
+                        </div>
+			        </div>
+			        <div id="downloadDetails">
+                        Install VLC using <a href="http://en.opensuse.org/One_Click_Install/UserFAQ">1-click install</a><br />
+			            <span id='downloadOS'><?php echo $defaultDetail["name"]; ?></span>
+			        </div>
+			    </div>
+            </div>
+        </div>
+    </section>
+
 <img src="/images/icons/openSUSE.jpg" style="float: right; border: 1px solid green;" alt="openSuSE logo" />
 
 <h2>Download VLC for openSUSE</h2>
 
-<p>In order to install <a href="/vlc/">VLC</a> on an openSUSE, you have several
-options:</p>
-<ul>
-<li><a href="#1click">Using 1-click install</a></li>
-<li><a href="#yast">Using YaST interface</a></li>
-<li><a href="#cli">Using Command Line</a></li>
-</ul>
-<br />
-
-<h2>Install VLC using
- <a href="http://en.opensuse.org/One_Click_Install/UserFAQ">1-click install</a></h2>
-<div>
-<div class="installbtn" style="float: left;">
-  <a href="http://download.videolan.org/pub/vlc/SuSE/Leap_42.1/vlc.ymp">VLC for Leap 42.1</a>
-</div>
-<div class="installbtn" style="float: left;">
-  <a href="http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/vlc.ymp">VLC for Tumbleweed</a>
-</div>
-<div class="installbtn" style="float: left;">
-  <a href="http://download.videolan.org/pub/vlc/SuSE/13.2/vlc.ymp">VLC for 13.2</a>
-</div>
-<div class="installbtn" style="float: left;">
-  <a href="http://download.videolan.org/pub/vlc/SuSE/13.1/vlc.ymp">VLC for 13.1</a>
-</div>
-</div>
-
-<br />
-<br />
-
 <h2 class='clearme'><a name="yast">Install VLC using YaST</a></h2>
-<h3>Add VideoLAN's repository</h3>
-<p>
+<p class="projectDescription">
+<b>Add VideoLAN's repository</b><br />
 First, add a new repository (installation source) in YaST (YaST2 ->
 Software -> Installation Sources)</p>
 
-<p>Then, add the following Repository:</p>
+<p class="projectDescription">Then, add the following Repository:</p>
 <pre>
 Type: HTTP / Servername: download.videolan.org /
 Directory: /pub/videolan/vlc/SuSE/&lt;SUSE Version&gt;
 </pre>
-<p>Replace &lt;SUSE Version&gt; in the Directory above with your installed SuSE
+<p class="projectDescription">Replace &lt;SUSE Version&gt; in the Directory above with your installed SuSE
 Version. Supported at this time are :</p>
-<p>
+<p class="projectDescription">
 <ul>
 <li>Leap_42.1</li>
 <li>Tumbleweed</li>
@@ -63,11 +100,10 @@ Version. Supported at this time are :</p>
 <li>13.1</li>
 </ul>
 </p>
-<p>Finally, confirm the addition.<br />
-</p>
+<p class="projectDescription">Finally, confirm the addition.</p>
 
 <h3>Security</h3>
-The repository is signed with a GPG Key 
+<p class="projectDescription">The repository is signed with a GPG Key 
 (gpg key C8DA93D2 fingerprint <code>BC8D 780D E330 8581 B2E0  7085 33DE 8FB7 C8DA 93D2</code>).
 <br />Accept this key in YaST and it will warn you in case there is something
 suspect with the repository.<br />
@@ -75,27 +111,29 @@ suspect with the repository.<br />
 Accept everything by clicking <b>Finish</b>.</p>
 
 <h3>Installation</h3>
-<p>
+<p class="projectDescription">
 Go to <em>Software Management</em> and enter <em>VLC</em> in the searchbox.<br />
 Select it and click <b>Accept</b> to install it with all its dependencies.<br />
-This might require the SUSE CD or DVD.</p>
-
-<p><b>NB:</b>If you want VLC's Mozilla Plugin, you have to select the
-<code>vlc-npapi</code>. It does not get installed by default.</p>
-
-<p>
+This might require the SUSE CD or DVD.
+<br />
+<b>NB:</b>If you want VLC's Mozilla Plugin, you have to select the
+<code>vlc-npapi</code>. It does not get installed by default.
+<br />
 You're done with the installation.
 Have a lot of Fun.
 </p>
 
 <h2><a name="cli">Install using command line</a></h2>
+<p class="projectDescription">
 <pre>
 sudo zypper ar http://download.videolan.org/pub/vlc/SuSE/&lt;SUSE version&gt; VLC
 sudo zypper mr -r VLC
 sudo zypper in vlc
 </pre>
+</p>
 
-<p>&lt;SUSE version&gt; can be 11.4, 12.3 or 13.1, 13.2, Tumbleweed, Factory</p>
+<p class="projectDescription">&lt;SUSE version&gt; can be 11.4, 12.3 or 13.1, 13.2, Tumbleweed, Factory</p>
 
+</div>
 
 <?php footer('$Id$'); ?>

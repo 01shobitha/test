3c13094f6d0c02f1b91599f6430f1b0dd2052c43
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-18 01:15:36 +0100

    Move Sources to new design

--- www.videolan.org/vlc/download-sources.php
+++ www.videolan.org/vlc/download-sources.php
@@ -1,13 +1,41 @@
 <?php
    $title = "VLC media player source code";
+   $new_design = true;
    $lang = "en";
    $menu = array( "vlc", "download" );
-   $additional_css = array("/style/panels.css");
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css", "/style/panels.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
-<h1 class="bigtitle"> VLC media player source code </h1>
+<div class="container">
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <div class="hidden-xs v-align col-sm-5">
+                <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive'); ?>
+            </div>
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLC media player source code
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <div id="left">
 <h2>Tarballs</h2>

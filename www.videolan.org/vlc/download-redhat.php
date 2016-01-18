3b812e32a67b062d7e37a8a4d5b120b2f2bfda3b
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-18 01:07:44 +0100

    Move RedHat to new design

--- www.videolan.org/vlc/download-redhat.php
+++ www.videolan.org/vlc/download-redhat.php
@@ -1,12 +1,56 @@
 <?php
    $title = "VLC media player for Red Hat Linux";
+   $new_design = true;
    $lang = "en";
    $menu = array( "vlc", "download" );
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css", "/style/panels.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
-<h1 class="bigtitle">VLC media player for <a href="http://www.redhat.com/rhel/">Red Hat Enterprise Linux</a></h1>
+<div class="container">
+	<?php
+	$screenshots = getScreenshots("linux");
+	$defaultDetail = getOS("linux");
+	?>
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <?php
+                echo '<div class="v-align carousel-padding-xs col-xs-12 col-sm-5 col-sm-offset-1 col-sm-pull-1"><div id="header-carousel">';
+                $first = true;
+                foreach($screenshots as $screenshot) {
+                    $src = $first ? $screenshot['src'] : '';
+                    $srcLazy = $first ? '' : $screenshot['src'];
+                    $dom  = '<div class="screenshot2">';
+                    $dom .= '<img src="'.$src.'" alt="'.$screenshot['name'].'" data-lazy="'.$srcLazy.'">';
+                    $dom .= '</div>';
+                    echo $dom;
+                    $first = false;
+                }
+                echo '</div></div>';
+            ?>
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLC media player for <a href="http://www.redhat.com/rhel/">Red Hat Enterprise Linux</a>
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <div id="fullwidth">
 <h2> 0.9.9 Packages for RHEL 5, x86 and x86_64 on RPMforge</h2>
@@ -21,6 +65,7 @@
 <h2>Older versions</h2>
 <?php browse_old( "vlc" ); ?>
 
+</div>
 </div>
 
 <?php footer('$Id$'); ?>

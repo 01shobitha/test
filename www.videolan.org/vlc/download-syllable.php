f73c55fcfaab8d13cf9787517fa07d4b5ffdbc66
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-18 01:12:15 +0100

    Move Syllable to new design

--- www.videolan.org/vlc/download-syllable.php
+++ www.videolan.org/vlc/download-syllable.php
@@ -1,14 +1,43 @@
 <?php
    $title = "VLC media player for Syllable";
+   $new_design = true;
    $lang = "en";
    $menu = array( "vlc", "download" );
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css", "/style/panels.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+   include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");
 ?>
 
-<div id="fullwidth">
+<div class="container">
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <div class="hidden-xs v-align col-sm-5">
+                <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive'); ?>
+            </div>
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('largeVLC.png', 'Large Orange VLC media player Traffic Cone Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLC media player for <a href="http://www.syllable.org/">Syllable</a>
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<h1 class="bigtitle">VLC media player for <a href="http://www.syllable.org/">Syllable</a></h1>
+<div id="fullwidth">
 
 <h2>Packages</h2>
 
@@ -35,5 +64,6 @@ build install vlc-0.8.6f
 </pre>
 </blockquote>
 
+</div>
 </div>
 <?php footer('$Id$'); ?>

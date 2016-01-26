e25477db5da321068a5d05554b0b7585ef6b41d4
Felix Paul Kühne <fkuehne@videolan.org>
2016-01-26 10:42:55 -0800

    VLC for Apple TV, v1.0.2

--- www.videolan.org/include/os-specific.php
+++ www.videolan.org/include/os-specific.php
@@ -4,7 +4,7 @@ $macosxversion = '2.2.1';
 $oldmacosxversion = '2.0.10';
 $version = '2.2.1';
 $iosversion = '2.7.2';
-$tvosversion = '1.0.1';
+$tvosversion = '1.0.2';
 $androidversion = '1.7.5';
 $winrtversion = '1.8.5';
 

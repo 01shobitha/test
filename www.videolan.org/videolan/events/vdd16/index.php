e827f97023db89c0aa8efaa779eb1608eb8959b8
Felix Paul Kühne <fkuehne@videolan.org>
2016-02-18 14:34:28 +0100

    Iterate on VDD16

--- www.videolan.org/videolan/events/vdd16/index.php
+++ www.videolan.org/videolan/events/vdd16/index.php
@@ -16,8 +16,8 @@
 <a href="/videolan/"><?php image( "logoGrey.png" , "VideoLAN association"); ?></a>
 <br />
 <a href="http://videolabs.io"><?php image( "partners/videolabs-transparent.png" , "Videolabs SAS", "", 180); ?></a>
-<!--><h2>Host</h2>
-<a href="http://www.criteo.fr/"><?php image( "partners/criteo.png" , "Criteo", "", 180); ?></a>-->
+<h2>Host</h2>
+<a href="http://bcc-berlin.de/"><?php image( "partners/bcc_logo" , "BCC", "", 180); ?></a>
 <?php panel_end(); ?>
 
 <?php panel_start( "blue" ); ?>
@@ -31,6 +31,8 @@
 invite you to the multimedia open-source event of the summer!</b></p>
 
 <p>2016 is a special year for many FLOSS projects: <a href="\\videolan">VideoLAN</a> as open-source project and <a href="https://fsfe.org">Free Software Foundation Europe</a> both have their 15th birthday while <a href="https://www.kde.org">KDE</a> has its 20th birthday. All these call for celebrations! This year VideoLAN has come together with <a href="http://www.qt.io">Qt</a>, <a href="https://fsfe.org">FSFE</a>, <a href="https://www.kde.org">KDE</a> and <a href="https://www.kdab.com">KDAB</a> to bring you <b><a href="https://qtcon.org">QtCon</a></b>, where attendees can meet, collaborate and get the latest news of all these projects.<br />For its eigths edition, <b>VideoLAN Dev Days 2016</b> will be organised as part of <b>QtCon</b> in Berlin. The event will start on Friday the 2nd of September with 3 shared days of talks, workshops, meetups and coding sessions.</p>
+
+<p><?php image( "events/vdd16/QtConInfo_v4_wee.jpg" , "QtCon Info v4"); ?></p>
 
 <h1>Where? </h1>
 <p>The venue is in the center of Berlin!<br />

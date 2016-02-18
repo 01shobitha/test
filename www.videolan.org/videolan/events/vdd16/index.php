aa39feb872295fdf937c53ad73dafd44686cd0d4
Felix Paul Kühne <fkuehne@videolan.org>
2016-02-18 14:39:45 +0100

    VDD16: move to modern design

--- www.videolan.org/videolan/events/vdd16/index.php
+++ www.videolan.org/videolan/events/vdd16/index.php
@@ -2,10 +2,18 @@
    $title = "VideoLAN Dev Days 2016, September 2 - 4, 2016";
    $additional_css = array("/style/panels.css");
    $body_color = "green";
+
+   $new_design = true;
+   $lang = "en";
+   $menu = array( "videolan", "events" );
+
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css");
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<p><?php image( "events/vdd14.jpg" , "VDD14 Group picture"); ?></p>
+<p><?php image( 'events/vdd14.jpg' , 'VDD14 Group picture', 'announce-img'); ?></p>
 
 <h1>VideoLAN Dev Days 2016</h1>
 <h3 style="color: #4D4D4D">The Open Multimedia Conference that frees the cone in you!</h3>
@@ -17,12 +25,7 @@
 <br />
 <a href="http://videolabs.io"><?php image( "partners/videolabs-transparent.png" , "Videolabs SAS", "", 180); ?></a>
 <h2>Host</h2>
-<a href="http://bcc-berlin.de/"><?php image( "partners/bcc_logo" , "BCC", "", 180); ?></a>
-<?php panel_end(); ?>
-
-<?php panel_start( "blue" ); ?>
-<h2>Register!</h2>
-<b><a href="http://goo.gl/forms/Kdi5LSu1u9">Register <strong>now!</strong></a></b>
+<a href="http://bcc-berlin.de/"><?php image( "partners/bcc_logo.png" , "BCC", "", 180); ?></a>
 <?php panel_end(); ?>
 
 </div>
@@ -32,7 +35,7 @@ invite you to the multimedia open-source event of the summer!</b></p>
 
 <p>2016 is a special year for many FLOSS projects: <a href="\\videolan">VideoLAN</a> as open-source project and <a href="https://fsfe.org">Free Software Foundation Europe</a> both have their 15th birthday while <a href="https://www.kde.org">KDE</a> has its 20th birthday. All these call for celebrations! This year VideoLAN has come together with <a href="http://www.qt.io">Qt</a>, <a href="https://fsfe.org">FSFE</a>, <a href="https://www.kde.org">KDE</a> and <a href="https://www.kdab.com">KDAB</a> to bring you <b><a href="https://qtcon.org">QtCon</a></b>, where attendees can meet, collaborate and get the latest news of all these projects.<br />For its eigths edition, <b>VideoLAN Dev Days 2016</b> will be organised as part of <b>QtCon</b> in Berlin. The event will start on Friday the 2nd of September with 3 shared days of talks, workshops, meetups and coding sessions.</p>
 
-<p><?php image( "events/vdd16/QtConInfo_v4_wee.jpg" , "QtCon Info v4"); ?></p>
+<p><?php image( 'events/vdd16/QtConInfo_v4_wee.jpg' , 'QtCon Info v4', 'announce-img'); ?></p>
 
 <h1>Where? </h1>
 <p>The venue is in the center of Berlin!<br />

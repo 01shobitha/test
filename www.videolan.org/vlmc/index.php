94ecb7d8f22a60d0ae897e17335235ed87f11431
Felix Paul Kühne <fkuehne@videolan.org>
2016-03-01 12:00:21 +0100

    Re-write VLMC page

--- www.videolan.org/vlmc/index.php
+++ www.videolan.org/vlmc/index.php
@@ -1,80 +1,100 @@
 <?php
-   $title = "VLMC, open source video editor";
+   $language="";
+   $title = _("VLMC, open source video editor");
+   $new_design = true;
+   
+   $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
+   $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css");
+
+   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+   
+   function vlmcscreenshot($img, $thumb, $desc) {
+        echo"<div class='screenshot-4-3-responsive screenshot-small'><a href='http://images.videolan.org/vlmc/screenshots/$img' rel='lightbox-vlc'><img
+        src='//images.videolan.org/vlmc/screenshots/$thumb' alt='VLMC - $desc' /></a>
+        </div>";
+    }
 ?>
 
-   <div>
-       <?php image( 'VLMC-Icon.png', 'Large Orange VLMC Logo', 'projectImg' ); ?>
-       <div style='float: left; width: 500px;'>
-           <h1 class='bigtitle'>VLMC</h1>
-           <div class='projectDescription'>
-               VideoLAN Movie Creator is a non-linear editing software for video creation based on libVLC and running on Windows, Linux and Mac OS X!
-               It is a free software distributed under the terms of the GPL v2.
-           </div>
-           <a class='extraInfoLink' href='#features'>Features</a>
-           <a class='extraInfoLink' href='#screenshots'>Screenshots</a>
-<!--                <a class='extraInfoLink' href='#'>Other Systems</a> -->
-            <div class="clearme"> </div>
-            <h1>We are <a href="http://trac.videolan.org/vlmc/wiki/Contributing">looking for contributors!</a></h1>
-            <h2>Stable downloads are not available, for now! Sorry for the issue!</a></h1>
+<div class="container">
+    <section class="download-wrapper">
+        <div class="row reorder-xs">
+            <div class="v-align col-sm-5">
+                <div class="center-font-xs">
+                    <?php image('VLMC-Icon.png', 'Large Orange VLMC Logo', 'big-vlc-img img-responsive visible-xs-inline-block v-align'); ?>
+                    <h1 class="v-align bigtitle">
+                        VLMC
+                    </h1>
+                </div>
+                <div class="projectDescription hidden-sm hidden-xs">
+                    <?php echo
+                    _("VideoLAN Movie Creator is a non-linear editing software for video creation based on libVLC and running on Windows, Linux and Mac OS X! It is a free software distributed under the terms of the GPLv2."); ?>
+                </div>
+                <div class="projectDescription visible-xs visible-sm center-font-xs">
+                    <?php echo
+                    _("VideoLAN Movie Creator is a non-linear editing software for video creation based on libVLC and running on Windows, Linux and Mac OS X! It is a free software distributed under the terms of the GPLv2."); ?>
+                </div>
+
+                <div class="inner center-xs">
+                    <div id="downloadDetails">
+                        Downloads will be available later this year<br>
+                        Stay tuned!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<section class="features bg-gray">
+    <div class="container">
+        <h2 class="visible-xs visible-sm center-font-xs center-font-sm"><?php echo _("Features"); ?></h2>
+        <div class="row reorder-sm reorder-xs">
+            <div class="items col-md-7 col-md-offset-1 col-md-pull-1 v-align-md v-align-lg">
+                <h2 class="hidden-xs hidden-sm"><?php echo _("Features"); ?></h2>
+                <ul>
+                    <li>
+                        <?php echo "<b>"._("Cross-platform")."</b> - "._("Runs on Windows, Linux and Mac OS X."); ?>
+                    </li>
+                    <li>
+                        <?php echo "<b>"._("Based on the proven VLC framework")."</b>"; ?>
+                    </li>
+                    <li>
+                        <?php echo "<b>"._("Reads everything and can export to most formats")."</b>"; ?>
+                    </li>
+                    <li>
+                        <?php echo "<b>"._("Simple to use, yet powerful")."</b>"; ?>
+                    </li>
+                    <li>
+                        <?php echo "<b>"._("Completely Free")."</b> - "._("no spyware, no ads and no user tracking."); ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 v-align-md v-align-lg">
+                <img src="//images.videolan.org/vlmc/screenshots/snapshot12.png" class="center-sm center-xs img-responsive">
+            </div>
         </div>
     </div>
-    <div style='clear: both; padding-top: 90px;'>
-        <div style='float: left; width: 400px; color: #333333;'>
-            <a name="features"></a>
-            <h1 style='margin-bottom: 25px;' class='red'>Features</h1>
-            Cross Platform program<br />
-            <br />
-            Based on VLC<br />
-            <br />
-            Read most file formats and can export to most formats<br />
-            <br />
-            Youtube upload integration<br />
-            <br />
-            Simple to use, yet powerful<br />
-        </div>
-<?php
-function vlmcscreenshot($img,$thumb, $desc){
-    echo"<div class='screenshot' style='height: 70px;'><a href='http://images.videolan.org/vlmc/screenshots/$img'><img
-    src='http://images.videolan.org/vlmc/screenshots/$thumb' alt='VLMC - $desc' /></a>
-    </div>";
-}
-?>
-        <div style='float: right; width: 450px; padding-left: 70px;'>
-                <a name="screenshots"></a>
-                <h1 style='padding-left: 5px; margin-bottom: 25px;'>Screenshots</h1>
-<?php
-                vlmcscreenshot('snapshot2.png', 'tm_snapshot2.png', "Screenshot" );
-                vlmcscreenshot('snapshot10.png', 'tm_snapshot10.png', "Screenshot" );
-                vlmcscreenshot('snapshot12.png', 'tm_snapshot12.png', "Screenshot" );
-                vlmcscreenshot('snapshot2.png', 'tm_snapshot2.png', "Screenshot" );
-?>
+</section>
+<section class="bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding-bottom-24">
+                <h1 style='margin-bottom: 25px;' class='red'><?php echo _("Screenshots"); ?></h1>
+                <div id="content-carousel">
+                    <?php
+                    vlmcscreenshot('snapshot2.png', 'tm_snapshot2.png', "Screenshot" );
+                    vlmcscreenshot('snapshot10.png', 'tm_snapshot10.png', "Screenshot" );
+                    vlmcscreenshot('snapshot12.png', 'tm_snapshot12.png', "Screenshot" );
+                    vlmcscreenshot('snapshot2.png', 'tm_snapshot2.png', "Screenshot" );
+                    ?>
+                </div>
+                <div class="ltrClearLeft">
+                </div>
+            </div>
         </div>
     </div>
-<!--        <div class='tricolumn'>
-            <div class="onecolumn">
-                <h1>Heading One</h1>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod commodo suscipit.<br />
-                <br />
-                Donec tempor accumsan massa in iaculis. Donec volutpat rutrum ipsum nec volutpat. Nullam egestas dignissim lorem, eu euismod ante gravida vitae. Sed feugiat porta libero, nec sagittis felis pulvinar ut. Integer ut tristique ante. In hendrerit, risus quis iaculis posuere, ipsum sapien congue nulla, in tristique neque urna sed nibh.<br />
-                <br />
-                Nulla ultrices nisl id turpis egestas fermentum. Nunc egestas lacus sit amet metus tincidunt imperdiet. Donec euismod scelerisque diam, a egestas quam pretium non. Phasellus auctor elit non diam facilisis luctus. Sed id justo quis felis aliquam ullamcorper.<br />
+</section>
 
-            </div>
-            <div class="onecolumn">
-                <h1>Heading Two</h1>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod commodo suscipit.<br />
-                <br />
-                Nulla ultrices nisl id turpis egestas fermentum. Nunc egestas lacus sit amet metus tincidunt imperdiet. Donec euismod scelerisque diam, a egestas quam pretium non. Phasellus auctor elit non diam facilisis luctus. Sed id justo quis felis aliquam ullamcorper.<br />
-                <br />Donec tempor accumsan massa in iaculis. Donec volutpat rutrum ipsum nec volutpat. Nunc ac urna a risus scelerisque commodo non in neque. Donec blandit eleifend placerat. Proin viverra semper nisi sit amet cursus. Suspendisse potenti. Nullam egestas dignissim lorem, eu euismod ante gravida vitae. Sed feugiat porta libero, nec sagittis felis pulvinar ut. Integer ut tristique ante. In hendrerit, risus quis iaculis posuere, ipsum sapien congue nulla, in tristique neque urna sed nibh.
-            </div>
-            <div class='lastcolumn'>
-                <h1>Heading Three</h1>
-                Nulla ultrices nisl id turpis egestas fermentum. Nunc egestas lacus sit amet metus tincidunt imperdiet. Donec euismod scelerisque diam, a egestas quam pretium non. Phasellus auctor elit non diam facilisis luctus. Sed id justo quis felis aliquam ullamcorper.<br />
-                <br />Donec blandit eleifend placerat. Proin viverra semper nisi sit amet cursus. Suspendisse potenti. Nullam egestas dignissim lorem, eu euismod ante gravida vitae. Sed feugiat porta libero, nec sagittis felis pulvinar ut. Integer ut tristique ante. In hendrerit, risus quis iaculis posuere, ipsum sapien congue nulla, in tristique neque urna sed nibh.
-            </div>
-        </div>
--->
-        <div style='clear: both;'></div>
-
-<?php footer(); ?>
+<?php
+  footer('$Id$');
+?>

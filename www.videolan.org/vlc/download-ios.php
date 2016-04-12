b26e601d4fb988642a4ca8b6e92eb54fce62f575
Felix Paul Kühne <fkuehne@videolan.org>
2016-04-12 17:35:06 +0200

    VLC-iOS 2.7.4

--- www.videolan.org/vlc/download-ios.php
+++ www.videolan.org/vlc/download-ios.php
@@ -82,8 +82,8 @@
 
 <?php
    echo "<code><a href='http://get.videolan.org/vlc-iOS/$iosversion/vlc-ios-$iosversion.tar.gz'>VLC for iOS $iosversion source code</a></code><br />"; ?>
-<code><a href="http://get.videolan.org/vlc-iOS/2.7.1/MediaLibraryKit-2.6.1.tar.gz">MediaLibraryKit 2.6.1 source code</a></code><br />
-<code><a href="http://get.videolan.org/vlc-iOS/2.7.1/VLCKit-3.0.0-pre2.tar.gz">MobileVLCKit 3.0.0-pre2 source code</a></code><br />
+<code><a href="http://get.videolan.org/vlc-iOS/2.7.4/MediaLibraryKit-2.6.2.tar.gz">MediaLibraryKit 2.6.2 source code</a></code><br />
+<code><a href="http://get.videolan.org/vlc-iOS/2.7.4/VLCKit-3.0.0-pre3.tar.gz">MobileVLCKit 3.0.0-pre3 source code</a></code><br />
 
 <h2> Binary </h2>
 <p class="projectDescription">You can also get the binary from our servers instead of the App Store:

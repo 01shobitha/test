48afa922fa7781fdac4e59135962d983021f9051
Jean-Baptiste Kempf <jb@videolan.org>
2016-06-05 10:49:50 +0200

    Changes to the videolan pages as suggested by Lydia

--- www.videolan.org/videolan/index.php
+++ www.videolan.org/videolan/index.php
@@ -17,25 +17,25 @@
                 The VideoLAN project is lead and
                 composed of a team of volunteers,
                 that believes in the power of open source
-                when dealing with multimedia.
+                to rock the multimedia world.
             </p>
         </div>
     </div>
-    
+
     <div class="row" style='clear: both; margin-top: 40px;'>
         <div class="col-md-12">
             <h2>The VideoLAN non-profit organization</h2>
-            <p>The VideoLAN project is backed up by the VideoLAN organization, based in France.</p>
+            <p>The VideoLAN project is supported by the VideoLAN organization, based in France.</p>
             <p>The VideoLAN non-profit organization has a committee and members that are part of the VideoLAN project.</p>
-            <p>The current board of VideoLAN is composed by:</p>
+            <p>The current board of VideoLAN organization is composed by:</p>
             <ul class="bullets">
                 <li>President: Jean-Baptiste Kempf</li>
                 <li>Treasurer: Felix Paul Kühne</li>
                 <li>Secretary: Hugo Beauzée-Luyssen</li>
             </ul>
         </div>
-    </div>        
-    
+    </div>
+
     <div class="row">
         <div class="col-sm-6">
             <h2>Open Source</h2>
@@ -45,11 +45,11 @@
         </div>
         <div class="col-sm-6">
             <h2>Hosted projects</h2>
-            <p>VideoLAN hosts many open source projects, related or not to <a href="/vlc/">VLC</a>.<br />
+            <p>VideoLAN hosts many open source projects, related to multimedia, including the famous <a href="/vlc/">VLC</a>.<br />
             Check the list of <a href="/projects/">projects</a>.</p>
         </div>
      </div>
-     <div class="row">   
+     <div class="row">
         <div class="col-sm-6">
             <h2>Contribute</h2>
             <p>VideoLAN welcomes all contributions to the project! You can contribute
@@ -62,25 +62,25 @@
             <p>We regularly attend computing events, like FOSDEM (Free-software
             and Open Source Developers European Meeting). We generally come back
             with some <a href="/videolan/events">photos and stories</a>.</p>
-        </div> 
+        </div>
      </div>
-     <div class="row">      
-        <div class="col-sm-6">            
+     <div class="row">
+        <div class="col-sm-6">
             <h2>History</h2>
             <p>The project started as a student project at the French <a
             href="http://www.ecp.fr/">École Centrale Paris</a>, in 1996.
             After a complete rewrite in 1998, it became Open Source, thanks to the agreement
             of the École Centrale Paris, in 2001.</p>
             <p>The project started to open up to developers outside of the École.
-            It is now a worldwide project with <a href="/videolan/team/">developers</a> from 20 countries.</p>
+            It is now a worldwide project with <a href="/videolan/team/">developers</a> from 40 countries.</p>
             <p>Since 2009, the project is completely separated from École Centrale Paris,
-            and is backed up by an autonomous non-profit organization.</p>
-        </div>    
+            and is driven by an autonomous non-profit organization.</p>
+        </div>
         <div class="col-sm-6">
             <h2>Contact</h2>
-            <p>Please use and read the <a href="/contact.html">contact page</a> for
-            any query, question and remark.</p>
-        </div>    
+            <p>Please see <a href="/contact.html">contact page</a> for
+            important queries, questions and remarks.</p>
+        </div>
     </div>
 </div>
 

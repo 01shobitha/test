e438525db2070b4590e4c3596921428ff071c84a
Jean-Baptiste Kempf <jb@videolan.org>
2016-06-07 18:02:29 +0200

    Show booxmedia sponsor on the main page

--- www.videolan.org/include/sponsors.php
+++ www.videolan.org/include/sponsors.php
@@ -1,61 +1,9 @@
 <?php
 
 $SPONSORS = array(
-    array(
-        "imgSrc"    => "ab.png",
-        "link"      => "https://www.hpe.com"
-    ),
-    array(
-        "imgSrc"    => "anevia.jpg",
-        "link"      => "http://www.anevia-group.com"
-    ),
     array (
-        "imgSrc"    => "april_logo.png",
-        "link"      => "https://www.april.org/"
-    ),
-    array (
-        "imgSrc"    => "att.gif",
-        "link"      => "https://www.att.com/"
-    ),
-    array (
-        "imgSrc"    => "criteo.png",
-        "link"      => "http://www.criteo.com"
-    ),
-    array (
-        "imgSrc"    => "epitech.jpg",
-        "link"      => "http://www.epitech.eu"
-    ),
-    array (
-        "imgSrc"    => "free.gif",
-        "link"      => "https://www.free.fr/"
-    ),
-    array (
-        "imgSrc"    => "gandi.png",
-        "link"      => "https://gandi.net"
-    ),
-    array (
-        "imgSrc"    => "google-2015.png",
-        "link"      => "https://google.com"
-    ),
-    array (
-        "imgSrc"    => "hpinvent.gif",
-        "link"      => "http://www.hp.com/"
-    ),
-    array (
-        "imgSrc"    => "puget.jpg",
-        "link"      => "https://www.pugetsystems.com"
-    ),
-    array (
-        "imgSrc"    => "videolabs.png",
-        "link"      => "https://videolabs.io"
-    ),
-    array (
-        "imgSrc"    => "wdlogo.jpg",
-        "link"      => "http://www.wdc.com/"
-    ),
-    array (
-        "imgSrc"    => "zf.jpg",
-        "link"      => "http://www.zf.com/"
+        "imgSrc"    => "booxmedia.png",
+        "link"      => "http://www.booxmedia.com/"
     )
 );
 

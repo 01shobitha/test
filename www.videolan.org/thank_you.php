9c47d6921c99ed73c4ef347e4ecd0c77d83d285e
epirat07@gmail.com <epirat07@gmail.com>
2016-07-01 10:49:04 +0200

    Update Thankyou page to responsive design
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- www.videolan.org/thank_you.php
+++ www.videolan.org/thank_you.php
@@ -1,17 +1,17 @@
 <?php
 $title = "Thank you";
 $menu = array( "project", "contribute" );
+$new_design = true;
 require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<div id="fullwidth">
+<div class="container">
     <h1 class="bigtitle"> Thank You! </h1>
     <h2>You are awesome!</h2>
     <p>Every donation matters a lot to us!</p>
     <p>As we are all volunteers, we really mean it! <br />
-       We cannot express enough our thank you, so here is an <b>awesome</b> cone for you:</p>
-    <?php image( 'goodies/Cone-Video-small.png', 'Large Orange VLC media player Traffic Cone Logo', 'projectImg' ); ?>
+    We cannot express enough our thank you, so here is an <b>awesome</b> cone for you:</p>
+    <?php image( 'goodies/Cone-Video-small.png', 'Orange VLC media player Traffic Cone with popcorn and glasses Logo' ); ?>
 </div>
-
 
 <?php footer('$Id: contribute.php 6882 2011-04-25 12:17:24Z jb $'); ?>

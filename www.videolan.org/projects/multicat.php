087664423b04562b6c5f47fb21ae069c9163f4a6
Marvin Scholz <epirat07@gmail.com>
2017-01-12 20:33:16 +0100

    videolan.org: Update Multicat page to new design

--- www.videolan.org/projects/multicat.php
+++ www.videolan.org/projects/multicat.php
@@ -3,71 +3,69 @@
    $lang = "en";
    $menu = array( "projects", "Multicat" );
    $body_color = "pink";
+   $new_design = true;
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
-<h1 class="bigtitle">Multicat</h1>
+<div class="container">
+	<h1 class="bigtitle">Multicat</h1>
+	<h2>Simple and efficient multicast and transport stream manipulation</h2>
 
-<h2>Simple and efficient multicast and transport stream manipulation</h2>
+	<p>The multicat package contains a set of tools designed to easily and
+	efficiently manipulate multicast streams in general, and MPEG-2
+	Transport Streams (ISO/IEC 13818-1) in particular.</p>
 
-<p>The multicat package contains a set of tools designed to easily and
-efficiently manipulate multicast streams in general, and MPEG-2
-Transport Streams (ISO/IEC 13818-1) in particular.</p>
+	<p>The multicat suite of applications is very lightweight and designed to
+	operate in tight environments. Memory and CPU usages are kept to a minimum,
+	and they feature only one thread of execution. It is designed to run on
+	Linux, FreeBSD and Mac OS X.</p>
 
-<p>The multicat suite of applications is very lightweight and designed to
-operate in tight environments. Memory and CPU usages are kept to a minimum,
-and they feature only one thread of execution. It is designed to run on
-Linux, FreeBSD and Mac OS X.</p>
+	<h2>The <code>multicat</code> program</h2>
 
-<h2>The <code>multicat</code> program</h2>
+	<p><code>multicat</code> itself is a 1 input/1 output application. Inputs and
+	outputs can be network streams (unicast and multicast), files, directories, character
+	devices or FIFOs. It is thought to be a multicast equivalent of the
+	popular netcat tool. Typical applications are recording
+	live transport streams, or playing out TS files without modification.
+	Also it is able to record a continuous stream into a directory, rotate
+	the files periodically, and make seamless extracts from it.</p>
 
-<p><code>multicat</code> itself is a 1 input/1 output application. Inputs and
-outputs can be network streams (unicast and multicast), files, directories, character
-devices or FIFOs. It is thought to be a multicast equivalent of the
-popular netcat tool. Typical applications are recording
-live transport streams, or playing out TS files without modification.
-Also it is able to record a continuous stream into a directory, rotate
-the files periodically, and make seamless extracts from it.</p>
+	<p>Multicat tries to rebuild the internal clock of the input stream; but
+	it wants to remain agnostic of what is transported, so in case of files
+	the said clock is stored to an auxiliary file (example.aux accompanies
+	example.ts) while recording. Other inputs are considered "live", and
+	the input clock is simply derived from the reception time of the packets.</p>
 
-<p>Multicat tries to rebuild the internal clock of the input stream; but
-it wants to remain agnostic of what is transported, so in case of files
-the said clock is stored to an auxiliary file (example.aux accompanies
-example.ts) while recording. Other inputs are considered "live", and
-the input clock is simply derived from the reception time of the packets.</p>
+	<h2>The <code>ingests</code> program</h2>
 
-<h2>The <code>ingests</code> program</h2>
+	<p><code>ingests</code> is a companion application designed to manipulate TS files. It
+	reads the PCR values of the file, and builds the auxiliary file that is
+	necessary for multicat.</p>
 
-<p><code>ingests</code> is a companion application designed to manipulate TS files. It
-reads the PCR values of the file, and builds the auxiliary file that is
-necessary for multicat.</p>
+	<p> The combination of <code>ingests</code> and <code>multicat</code> makes
+	a simple and efficient TS file streamer.</p>
 
-<p> The combination of <code>ingests</code> and <code>multicat</code> makes
-a simple and efficient TS file streamer.</p>
+	<h2>The <code>aggregartp</code> and <code>reordertp</code> programs</h2>
 
-<h2>The <code>aggregartp</code> and <code>reordertp</code> programs</h2>
+	<p><code>aggregartp</code> splits a single RTP stream (for instance a high-bitrate signal) to several contribution links with load balancing.</p>
 
-<p><code>aggregartp</code> splits a single RTP stream (for instance a high-bitrate signal) to several contribution links with load balancing.</p>
+	<p><code>reordertp</code> takes one or multiple inputs (from <code>aggregartp</code> or another source known to reorder and/or add jitter to packets) and smoothes them out to output the original stream in correct order.</p>
 
-<p><code>reordertp</code> takes one or multiple inputs (from <code>aggregartp</code> or another source known to reorder and/or add jitter to packets) and smoothes them out to output the original stream in correct order.</p>
+	<h2>Get Multicat</h2>
 
-<h2>Get Multicat</h2>
+	<p> The latest official version of <code>Multicat</code>
+	is numbered 2.1, and is available <a
+	href="https://get.videolan.org/multicat/2.1/multicat-2.1.tar.bz2">here</a>.</p>
 
-<p> The latest official version of <code>Multicat</code>
-is numbered 2.1, and is available <a
-href="https://get.videolan.org/multicat/2.1/multicat-2.1.tar.bz2">here</a>.</p>
+	<p><a href="/developers/bitstream.html">biTStream</a> needs to be installed at build-time.</p>
 
-<p><a href="/developers/bitstream.html">biTStream</a> needs to be installed at build-time.</p>
+	<p> Hackers can participate to the development using <a
+	href="http://wiki.videolan.org/Subversion">Subversion</a>.
+	The code can be found at <code>svn://svn.videolan.org/multicat</code> and you can
+	<a href="http://svn.videolan.org/listing.php?repname=Multicat&amp;path=%2F&amp;sc=0">browse the source online</a>.</p>
 
-<p> Hackers can participate to the development using <a
-href="http://wiki.videolan.org/Subversion">Subversion</a>.
-The code can be found at <code>svn://svn.videolan.org/multicat</code> and you can
-<a href="http://svn.videolan.org/listing.php?repname=Multicat&amp;path=%2F&amp;sc=0">browse the source online</a>.</p>
-
-<p>User support and development is handled in the <a
-href="/developers/lists.html">multicat-devel mailing-list</a>.</p>
-
-<br />
-<br />
-<br />
+	<p>User support and development is handled in the <a
+	href="/developers/lists.html">multicat-devel mailing-list</a>.</p>
+</div>
 
 <?php footer('$Id: multicat.php 5717 2009-11-24 08:46:34Z md $'); ?>

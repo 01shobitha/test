e7bd2b5eab2077048f38e528691641edbfb5c8af
Marvin Scholz <epirat07@gmail.com>
2017-01-14 17:12:22 +0100

    videolan.org: Change doctype to HTML5, remove XHTML namespace/language

--- www.videolan.org/include/header.php
+++ www.videolan.org/include/header.php
@@ -127,15 +127,9 @@ function start_head( $title,
         bindtextdomain("website", $_SERVER["DOCUMENT_ROOT"]."/locale");
         textdomain("website");
     }
-
-    /* We love xml, right? */
-    echo '<?xml version="1.0" encoding="utf-8" ?>'."\n"; ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
-      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language?>" lang="<?php echo $language?>"
-        <?php if ( isRTL( $language ) ) { echo ' dir="rtl"'; } ?>
-      >
+?>
+    <!DOCTYPE html>
+    <html lang="<?php echo $language?>" <?php if ( isRTL( $language ) ) { echo ' dir="rtl"'; } ?>>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <?php

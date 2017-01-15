a8c54366e901f32f2abe066c14c338ed481ea3fd
Christoph Miebach <christoph.miebach@web.de>
2017-01-15 06:30:09 -0500

    Remove the links to non existing localised version

--- www.videolan.org/vlc/download-suse.php
+++ www.videolan.org/vlc/download-suse.php
@@ -7,7 +7,6 @@
 
    $additional_js = array("/js/slimbox2.js", "/js/slick-init.js", "/js/slick.min.js");
    $additional_css = array("/js/css/slimbox2.css", "/style/slick.min.css", "/style/panels.css");
-   require($_SERVER["DOCUMENT_ROOT"]."/include/language.php");
    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/os-specific.php");
    include($_SERVER["DOCUMENT_ROOT"]."/include/package.php");

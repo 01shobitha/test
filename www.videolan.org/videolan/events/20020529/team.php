440394cda331156983e402cb9221e34d5ce97a36
Marvin Scholz <epirat07@gmail.com>
2017-01-16 20:37:12 +0100

    videolan.org: Update Launching the test campaign image gallery and team pages (20020529)

---
+++ www.videolan.org/videolan/events/20020529/team.php
@@ -0,0 +1,28 @@
+<?php
+    $title = "VideoLAN - Part of the VideoLAN team";
+    $lang = "en";
+    $date = "6 June 2002";
+    $new_design = true;
+    require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
+?>
+
+<div class="container">
+    <h1 class="bigtitle">The VideoLAN team</h1>
+
+    <p>It is Wednesday, May 29th, very late at night, the VideoLAN
+    show is over and the team is watching "Toy Story" !</p>
+
+    <a href="117-1750_IMG.JPG"><img src="team.jpg" alt="The VideoLAN team" class="img-responsive center-block" /></a>
+
+    <p>From left to right:</p>
+    <table>
+        <tr>
+            <td>First row</td><td><b>Tooney</b>, <b>Ali</b> and <b>Sam</b></td>
+        </tr>
+        <tr>
+            <td>Second row</td><td><b>Jobi</b>, <b>AsMaX</b>, <b>Loïc Berthe</b>, <b>Marcari</b>, <b>Tang</b> (a <a href="http://www.cnedra.org">C'Nedra</a> guy) and <b>Ipkiss</b></td>
+        </tr>
+    </table>
+</div>
+
+<?php footer(); ?>

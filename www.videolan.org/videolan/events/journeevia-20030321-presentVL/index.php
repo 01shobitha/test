5fc723bd6dc4e581bc7b15fd36c66bc4d10a4022
Marvin Scholz <epirat07@gmail.com>
2017-01-16 14:39:43 +0100

    videolan.org: Simplify/Automate journeevia-20030321-presentVL image gallery

--- www.videolan.org/videolan/events/journeevia-20030321-presentVL/index.php
+++ www.videolan.org/videolan/events/journeevia-20030321-presentVL/index.php
@@ -6,149 +6,28 @@
     require($_SERVER["DOCUMENT_ROOT"]."/include/header.php");
 ?>
 
+<?php function printImageThumb($filename) { ?>
+    <div class="col-xs-4 col-md-2">
+        <a href="<?= $filename ?>" class="thumbnail">
+            <img src="thumbs/tn_<?= $filename ?>.png" alt="<?= $filename ?>">
+        </a>
+    </div>
+<?php } ?>
+
 <div class="container">
     <h2>Index of /home/jb/journeevia-20030321-presentVL</h2>
     <div class="row">
-        <div class="col-xs-4 col-md-2">
-            <a href="Claude,_emballe.JPG" class="thumbnail">
-                <img src="thumbs/tn_Claude,_emballe.JPG.png" alt="Claude,_emballe.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Preparation.JPG" class="thumbnail">
-                <img src="thumbs/tn_Preparation.JPG.png" alt="Preparation.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Garf_5.JPG" class="thumbnail">
-                <img src="thumbs/tn_Garf_5.JPG.png" alt="Garf_5.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="John_3.JPG" class="thumbnail">
-                <img src="thumbs/tn_John_3.JPG.png" alt="John_3.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="VIA_men.JPG" class="thumbnail">
-                <img src="thumbs/tn_VIA_men.JPG.png" alt="VIA_men.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Garf_4.JPG" class="thumbnail">
-                <img src="thumbs/tn_Garf_4.JPG.png" alt="Garf_4.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Alexis_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_Alexis_2.JPG.png" alt="Alexis_2.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Julien_4.JPG" class="thumbnail">
-                <img src="thumbs/tn_Julien_4.JPG.png" alt="Julien_4.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Stress.JPG" class="thumbnail">
-                <img src="thumbs/tn_Stress.JPG.png" alt="Stress.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="John_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_John_2.JPG.png" alt="John_2.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="John_4.JPG" class="thumbnail">
-                <img src="thumbs/tn_John_4.JPG.png" alt="John_4.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Garf.JPG" class="thumbnail">
-                <img src="thumbs/tn_Garf.JPG.png" alt="Garf.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Julien.JPG" class="thumbnail">
-                <img src="thumbs/tn_Julien.JPG.png" alt="Julien.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Preparation_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_Preparation_2.JPG.png" alt="Preparation_2.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Adn_Kiel.JPG" class="thumbnail">
-                <img src="thumbs/tn_Adn_Kiel.JPG.png" alt="Adn_Kiel.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Alexis.JPG" class="thumbnail">
-                <img src="thumbs/tn_Alexis.JPG.png" alt="Alexis.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Garf_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_Garf_2.JPG.png" alt="Garf_2.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Preparation_3.JPG" class="thumbnail">
-                <img src="thumbs/tn_Preparation_3.JPG.png" alt="Preparation_3.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Garf_3.JPG" class="thumbnail">
-                <img src="thumbs/tn_Garf_3.JPG.png" alt="Garf_3.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="VIA_men_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_VIA_men_2.JPG.png" alt="VIA_men_2.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Question.JPG" class="thumbnail">
-                <img src="thumbs/tn_Question.JPG.png" alt="Question.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Nitrox.JPG" class="thumbnail">
-                <img src="thumbs/tn_Nitrox.JPG.png" alt="Nitrox.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Public.JPG" class="thumbnail">
-                <img src="thumbs/tn_Public.JPG.png" alt="Public.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Adn_Keelize,_public.JPG" class="thumbnail">
-                <img src="thumbs/tn_Adn_Keelize,_public.JPG.png" alt="Adn_Keelize,_public.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Julien_3.JPG" class="thumbnail">
-                <img src="thumbs/tn_Julien_3.JPG.png" alt="Julien_3.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Claude.JPG" class="thumbnail">
-                <img src="thumbs/tn_Claude.JPG.png" alt="Claude.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="John.JPG" class="thumbnail">
-                <img src="thumbs/tn_John.JPG.png" alt="John.JPG (JPEG)">
-            </a>
-        </div>
-        <div class="col-xs-4 col-md-2">
-            <a href="Julien_2.JPG" class="thumbnail">
-                <img src="thumbs/tn_Julien_2.JPG.png" alt="Julien_2.JPG (JPEG)">
-            </a>
-        </div>
+        <?php
+            $folder = realpath(dirname(__FILE__));
+            $files = scandir($folder);
+
+            foreach ($files as $file) {
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                if (strcasecmp($ext, "JPG") === 0) {
+                    printImageThumb($file);
+                }
+            }
+        ?>
     </div>
 </div>
 

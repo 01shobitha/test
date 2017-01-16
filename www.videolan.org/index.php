5ff53fc421e9ec0eb38b58a7bc08f65a949810f9
Christoph Miebach <christoph.miebach@web.de>
2017-01-16 13:09:36 -0500

    don't use single quotes

--- www.videolan.org/index.php
+++ www.videolan.org/index.php
@@ -231,7 +231,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/include/magpierss/rss_fetch.inc");
                     if (isset($item['link'])) {
                         echo "<p dir='ltr' style='text-align:left;'>";
                         echo "<b><a href=\"" . $item['link'] . '" style="color: black;">' . htmlentities($item['title'], ENT_COMPAT, "UTF-8") . "</a></b></p>";
-                        echo '<p dir='ltr' style="border-bottom: 1px dashed #CCC;padding-bottom:5px;margin-bottom:5px; text-align:left;">';
+                        echo '<p dir="ltr" style="border-bottom: 1px dashed #CCC;padding-bottom:5px;margin-bottom:5px; text-align:left;">';
                         echo substr(strip_tags(html_entity_decode($item['content']['encoded'], ENT_COMPAT, "UTF-8")), 0, $chars_per_entry);
                         echo "<a href=\"" . $item['link'] . "\">[...]</a><br/>";
                         echo "</p>";

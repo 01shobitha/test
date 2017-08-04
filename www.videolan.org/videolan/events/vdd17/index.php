57650c2a82130212b1db3802f3a723695e74a1af
Jean-Baptiste Kempf <jb@videolan.org>
2017-08-04 16:38:42 +0200

    VDD17: update a bit the page

--- www.videolan.org/videolan/events/vdd17/index.php
+++ www.videolan.org/videolan/events/vdd17/index.php
@@ -34,9 +34,6 @@
       <strong>22nd and 24th of September 2017</strong>, to gather, work, discuss and meet, in: <strong>Paris, France</strong>!</p>
       <p>This event is focused on technical discussions and decisions about <strong>multimedia</strong>.</p>
 
-      <p>This year <b>VideoLAN Dev Days</b> will be colocated within a larger conference called <b><a href="https://qtcon.org">QtCon</a></b>!<br />The conference will see <a href="http://www.qt.io">Qt</a>, <a href="https://fsfe.org">FSFE</a>, <a href="https://www.kde.org">KDE</a> and <a href="https://www.kdab.com">KDAB</a> having talks in the same place.
-
-      <p><?php image( 'events/vdd16/QtConInfo_v4_wee.jpg' , 'QtCon Info v4', 'announce-img'); ?></p>
     </div>
   </div>
 </div>
@@ -57,14 +54,13 @@
     <div class="row">
       <div class="text-center">
         <h2 class="uppercase">Schedule</h2>
-        <p>VDD is part of QtCon. Please see the <a href="https://conf.qtcon.org/en/qtcon/public/schedule">official program</a>.</p>
       </div>
       <div class="col-lg-10 col-lg-offset-1">
       <!-- Nav tabs -->
       <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation" class="active"><a href="#friday" aria-controls="friday" role="tab" data-toggle="tab">Friday 2</a></li>
-        <li role="presentation"><a href="#saturday" aria-controls="saturday" role="tab" data-toggle="tab">Saturday 3</a></li>
-        <li role="presentation"><a href="#sunday" aria-controls="sunday" role="tab" data-toggle="tab">Sunday 4</a></li>
+        <li role="presentation" class="active"><a href="#friday" aria-controls="friday" role="tab" data-toggle="tab">Friday 22</a></li>
+        <li role="presentation"><a href="#saturday" aria-controls="saturday" role="tab" data-toggle="tab">Saturday 23</a></li>
+        <li role="presentation"><a href="#sunday" aria-controls="sunday" role="tab" data-toggle="tab">Sunday 24</a></li>
       </ul>
 
       <!-- Tab panes -->
@@ -73,24 +69,13 @@
             <div class="event">
               <div class="event-inner">
                 <div class="event-time">
-                  10:00 - 18:00
-                </div>
-                <div class="event-description">
-                  <h3>General conference</h3>
-                  <p>On Friday, KDE, Qt and FSFE start QtCon with their talks and conferences. You are welcome to attend their <a href="https://conf.qtcon.org/en/qtcon/public/schedule/2016-09-02">sessions through-out the day</a> and the <a href="https://conf.qtcon.org/en/qtcon/public/events/518">keynote</a> on Friday morning.</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  13:00 - 18:30
+                  09:30 - 18:30
                 </div>
                 <div class="event-description">
                   <h3>Community Bonding Day</h3>
-                  <p>This year we'll do an <b>Escape room competition</b>!<br/>
+                  <p>This year we'll do an <b>tbd...>!<br/>
                   The VideoLAN organization will pay for the entrance tickets.<br/>
-                  <b>To participate you'll need to be in downtown Berlin, on Friday at 13h00!</b><br/>Please note, that VideoLAN will not pay for a hotel on Thursday night, except very special cases.</p>
+                  <b>To participate you'll need to be in downtown Berlin, on Friday morning at 9:00!</b><br/>Please note, that VideoLAN will not pay for a hotel on Thursday night, except very special cases.</p>
                 </div>
               </div>
             </div>
@@ -102,7 +87,7 @@
                 <div class="event-description">
                   <h3>Evening reception</h3>
                   <p>On <strong>Friday at 19h00</strong>, people are welcome to come and
-                  share a few good drinks, with all <b>QtCon</b> attendees. Location is the BCC. Registration for QtCon required.</p>
+                  share a few good drinks, with all attendees, somewhere near the Hotel.</p>
                 </div>
               </div>
             </div>
@@ -111,7 +96,7 @@
             <div class="event">
               <div class="event-inner">
                 <div class="event-time">
-                  09:00 - 10:00
+                  09:00 - 09:30
                 </div>
                 <div class="event-description">
                   <h3>Breakfast</h3>
@@ -121,174 +106,11 @@
             <div class="event">
               <div class="event-inner">
                 <div class="event-time">
-                  10:00 - 11:00
+                  10:00 - 13:00
                 </div>
                 <div class="event-description">
-                  <h3>VideoLAN general assembly and VideoLAN quarterly meeting</h3>
-                  <h5>Room A06</h5>
-                  <p><b>This is for non-profit members only.</b></p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  11:00 - 11:15
-                </div>
-                <div class="event-description">
-                  <h3>Welcome to the VideoLAN track!</h3>
-                  <h5>Room B09</h5>
-                  <p>Jean-Baptiste Kempf</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  11:15 - 11:50
-                </div>
-                <div class="event-description">
-                  <h3>Update on VPX</h3>
-                  <h5>Room B09</h5>
-                  <p>Alex Converse</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  11:50 - 12:15
-                </div>
-                <div class="event-description">
-                  <h3>A VC-2 encoder</h3>
-                  <h5>Room B09</h5>
-                  <p>Rostislav Pehlivanov</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  12:15 - 12:30
-                </div>
-                <div class="event-description">
-                  <h3>Group photo</h3>
-                  <h5>Room B02</h5>
-                  <p>Everyone!</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  12:30 - 13:30
-                </div>
-                <div class="event-description">
-                  <h3>Lunch</h3>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  13:30 - 14:00
-                </div>
-                <div class="event-description">
-                  <h3>A VP9 encoder</h3>
-                  <h5>Room B09</h5>
-                  <p>Ronald Bultje</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  14:00 - 14:45
-                </div>
-                <div class="event-description">
-                  <h3>VLC: State of the union</h3>
-                  <h5>Room B09</h5>
-                  <ul><li>Felix Paul Kühne</li><li>Jean-Baptiste Kempf</li><li>Hugo Beauzée-Luyssen</li><li>Thomas Guillem</li></ul>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  15:00 - 15:25
-                </div>
-                <div class="event-description">
-                  <h3>Daala</h3>
-                  <h5>Room B09</h5>
-                  <p>Timothy Terriberry</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  15:25 - 15:50
-                </div>
-                <div class="event-description">
-                  <h3>Update on x265</h3>
-                  <h5>Room B09</h5>
-                  <p>Pradeep Ramachandran</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  15:50 - 16:00
-                </div>
-                <div class="event-description">
-                  <h3>VLMC</h3>
-                  <h5>Room B09</h5>
-                  <p>Hugo Beauzée-Luyssen</p>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  16:30 - 17:30
-                </div>
-                <div class="event-description">
-                  <h3>VLC technical discussion</h3>
-                  <h5>Room A05</h5>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  16:30 - 17:30
-                </div>
-                <div class="event-description">
-                  <h3>FFmpeg technical discussion</h3>
-                  <h5>Room A06</h5>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  16:30 - 17:30
-                </div>
-                <div class="event-description">
-                  <h3>LibAV technical discussion</h3>
-                  <h5>Room B04</h5>
-                </div>
-              </div>
-            </div>
-            <div class="event">
-              <div class="event-inner">
-                <div class="event-time">
-                  16:30 - 17:30
-                </div>
-                <div class="event-description">
-                  <h3>upipe technical discussion</h3>
-                  <h5>Room B95</h5>
+                  <h3>Talks</h3>
+                  <h5>Room ...</h5>
                 </div>
               </div>
             </div>
@@ -311,14 +133,14 @@
                 </div>
                 <div class="event-description">
                   <h3>Unconference</h3>
-                  <h5>Rooms A05 and A06 as well as B04 and B95 (1 to 2PM only)</h5>
+                  <h5>Room ...</h5>
                 </div>
               </div>
             </div>
             <div class="event">
               <div class="event-inner">
                 <div class="event-description">
-                    <p>The actual content of the unconference track is being decided on Saturday evening. For the live schedule, check the <a href="https://wiki.videolan.org/VDD16#Unconference_schedule">designated page on the wiki</a>.</p>
+                    <p>The actual content of the unconference track is being decided on Saturday evening. For the live schedule, check the <a href="https://wiki.videolan.org/VDD17#Unconference_schedule">designated page on the wiki</a>.</p>
                 </div>
               </div>
             </div>

8b97003d3eb670cc7df36a8fa882d00e009bd873
Jean-Baptiste Kempf <jb@videolan.org>
2018-04-21 09:01:44 +0200

    Remove Google+ integration
    G+ is long gone, and it won't work for GDPR

--- www.videolan.org/include/footer.php
+++ www.videolan.org/include/footer.php
@@ -129,14 +129,6 @@ function footer($tag = "", $alternate_lang=array(), $additional_class = "") {
             </div>
         </div>
     </div>
-    <!-- plusone button -->
-    <script type="text/javascript">
-        (function() {
-            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-            po.src = 'https://apis.google.com/js/plusone.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-        })();
-    </script>
     </body>
 </html>
 <?php

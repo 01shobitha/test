2656704b18c99d414bd9e91e8fbd62f2f0e6b5e1
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-05-31 18:00:43 +0200

    Add 3.0.2 and 3.0.3 to the download stats

--- www.videolan.org/vlc/stats/downloads.php
+++ www.videolan.org/vlc/stats/downloads.php
@@ -415,6 +415,16 @@
                                          "Macintosh" => "https://get.videolan.org/vlc/#version#/macosx/vlc-#version#.dmg?stats",
                                          "Source" => "https://get.videolan.org/vlc/#version#/vlc-#version#.tar.xz?stats",
                                   ),
+                     "3.0.2" => array(
+                                         "Windows" => array( "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.exe?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.exe?stats", "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.7z?stats", "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.zip?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.7z?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.zip?stats" ),
+                                         "Macintosh" => "https://get.videolan.org/vlc/#version#/macosx/vlc-#version#.dmg?stats",
+                                         "Source" => "https://get.videolan.org/vlc/#version#/vlc-#version#.tar.xz?stats",
+                                  ),
+                     "3.0.3" => array(
+                                         "Windows" => array( "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.exe?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.exe?stats", "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.7z?stats", "https://get.videolan.org/vlc/#version#/win32/vlc-#version#-win32.zip?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.7z?stats", "https://get.videolan.org/vlc/#version#/win64/vlc-#version#-win64.zip?stats" ),
+                                         "Macintosh" => "https://get.videolan.org/vlc/#version#/macosx/vlc-#version#.dmg?stats",
+                                         "Source" => "https://get.videolan.org/vlc/#version#/vlc-#version#.tar.xz?stats",
+                                  ),
     );
 
     foreach( $table as $key => $t )
